962a75c5163973526e27abc82dfa7cae40bb1ec7
Chris Egerton <fearthecellos@gmail.com>
2017-04-20 10:11:49 -0700

    Fix datagen producers to work with quickstart options

--- kql-examples/src/main/java/io/confluent/kql/datagen/JsonProducer.java
+++ kql-examples/src/main/java/io/confluent/kql/datagen/JsonProducer.java
@@ -5,18 +5,18 @@ package io.confluent.kql.datagen;
 
 import io.confluent.kql.physical.GenericRow;
 import io.confluent.kql.serde.json.KQLJsonPOJOSerializer;
-import io.confluent.kql.util.KQLException;
 
 import org.apache.avro.Schema;
 import org.apache.kafka.common.serialization.Serializer;
-import org.apache.kafka.connect.data.SchemaBuilder;
 
 public class JsonProducer extends DataGenProducer {
 
   @Override
-  protected Serializer<GenericRow> getSerializer(Schema schema, String topicName) {
-    return new KQLJsonPOJOSerializer(getKQLSchema(topicName));
+  protected Serializer<GenericRow> getSerializer(
+      Schema avroSchema,
+      org.apache.kafka.connect.data.Schema kafkaSchema,
+      String topicName
+  ) {
+    return new KQLJsonPOJOSerializer(kafkaSchema);
   }
-
-
 }

50906ec624fede115b705ce2bb80798bb84111b6
Chris Egerton <fearthecellos@gmail.com>
2017-04-24 16:26:28 -0700

    Allow users to terminate streamed queries, add standalone CLI options

--- kql-cli/src/main/java/io/confluent/kql/Cli.java
+++ kql-cli/src/main/java/io/confluent/kql/Cli.java
@@ -5,6 +5,7 @@ package io.confluent.kql;
 
 import io.confluent.kql.rest.client.KQLRestClient;
 import jline.console.ConsoleReader;
+import jline.console.UserInterruptException;
 
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -14,34 +15,85 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
-
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Cli {
 
-  private final ConsoleReader console;
+  private static final String DEFAULT_PROMPT = "kql> ";
+  private static final Pattern QUOTED_PROMPT_PATTERN = Pattern.compile("'(''|[^'])*'");
+
   private final KQLRestClient restClient;
+  private final ConsoleReader console;
 
   public Cli(KQLRestClient restClient) throws IOException {
-    this.console = new ConsoleReader();
     this.restClient = restClient;
+
+    this.console = new ConsoleReader();
+    console.setExpandEvents(false); // Otherwise, things like '!=' will cause things to break
+    console.setPrompt(DEFAULT_PROMPT);
+    console.setHandleUserInterrupt(true);
   }
 
   public void repl() throws IOException {
-    console.println("Entering REPL");
     console.flush();
-    for (String line = console.readLine(); line != null; line = console.readLine()) {
-      if (line.trim().isEmpty()) {
-        continue;
-      } else if (line.trim().startsWith(":")) {
-        handleMetaCommand(line.trim());
-      } else {
-        handleStatements(line);
-      }
+    for (String line = readLine(); line != null && handleLine(line.trim()); line = readLine()) {
       console.flush();
     }
-    console.println("Exiting REPL");
+    console.println();
     console.flush();
     console.close();
+  }
+
+  private String readLine() throws IOException {
+    while (true) {
+      try {
+        return console.readLine();
+      } catch (UserInterruptException exception) {
+        console.println("^C");
+        console.flush();
+      }
+    }
+  }
+
+  // Return value is a hacky way of detecting EOF
+  private boolean handleLine(String trimmedLine) throws IOException {
+    if (trimmedLine.isEmpty()) {
+      return true;
+    }
+
+    try {
+      if (trimmedLine.startsWith(":")) {
+        handleMetaCommand(trimmedLine);
+      } else {
+        StringBuilder multiLineBuffer = new StringBuilder();
+        String currentPrompt = console.getPrompt();
+        while (trimmedLine.endsWith("\\")) {
+          multiLineBuffer.append(trimmedLine.substring(0, trimmedLine.length() - 1));
+          try {
+            trimmedLine = console.readLine("");
+            if (trimmedLine == null) {
+              return false;
+            }
+            trimmedLine = trimmedLine.trim();
+          } catch (UserInterruptException exception) {
+            console.println("^C");
+            return true;
+          }
+        }
+        console.setPrompt(currentPrompt);
+        multiLineBuffer.append(trimmedLine);
+        handleStatements(multiLineBuffer.toString());
+      }
+    } catch (RuntimeException exception) {
+      if (exception.getMessage() != null) {
+        console.println(exception.getMessage());
+      } else {
+        console.println("An unexpected exception with no message was encountered.");
+      }
+    }
+    return true;
   }
 
   private void handleMetaCommand(String trimmedLine) throws IOException {
@@ -69,8 +121,29 @@ public class Cli {
         String query = commandArgs[1];
         handleStreamedQuery(query);
         break;
+      case ":prompt":
+        if (commandArgs.length == 1) {
+          throw new RuntimeException(String.format(
+              "'%s' meta-command must be followed by a new prompt value, optionally enclosed in single quotes",
+              command
+          ));
+        }
+        String prompt = commandArgs[1];
+        if (prompt.startsWith("'")) {
+          Matcher quotedPromptMatcher = QUOTED_PROMPT_PATTERN.matcher(prompt);
+          if (quotedPromptMatcher.matches()) {
+            console.setPrompt(prompt.substring(1, prompt.length() - 1).replace("''", "'"));
+          } else {
+            throw new RuntimeException(
+                "Failed to parse prompt string. All non-enclosing single quotes must be doubled."
+            );
+          }
+        } else {
+          console.setPrompt(prompt);
+        }
+        break;
       case ":help":
-        printCommandList();
+        printHelpMessage();
         break;
       default:
         throw new RuntimeException(String.format("Unknown meta-command: ':%s'", command));
@@ -83,18 +156,47 @@ public class Cli {
   }
 
   private void handleStreamedQuery(String query) throws IOException {
-    try (InputStream queryStream = restClient.makeQueryRequest(query)) {
-      Scanner queryScanner = new Scanner(queryStream);
-      while (queryScanner.hasNextLine()) {
-        String line = queryScanner.nextLine();
-        if (!line.trim().isEmpty()) {
-          InputStream lineInputStream = new ByteArrayInputStream(line.getBytes());
-          printJsonResponse(Json.createReader(lineInputStream).read());
-          console.flush();
+    AtomicBoolean continueStreaming = new AtomicBoolean(true);
+    Thread queryPrintThread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try (Scanner queryScanner = new Scanner(restClient.makeQueryRequest(query))) {
+          while (continueStreaming.get() && queryScanner.hasNextLine()) {
+            String line = queryScanner.nextLine();
+            if (!line.trim().isEmpty()) {
+              InputStream lineInputStream = new ByteArrayInputStream(line.getBytes());
+              try {
+                printJsonResponse(Json.createReader(lineInputStream).read());
+                console.flush();
+              } catch (IOException exception) {
+                throw new RuntimeException(exception);
+              }
+            }
+          }
         }
       }
+    });
+    queryPrintThread.start();
+
+    String currentPrompt = console.getPrompt();
+    // Have to find some way to call console.readLine(), since that's the only way to cause a UserInterruptException to
+    // be thrown; in this case, a separate thread is dispatched to print out the contents of the streamed query, while
+    // the current thread just continuously calls console.readLine() and ignores all user input until they press ^C,
+    // triggering the UserInterruptException and causing everything to go back to normal.
+    try {
+      while (true) {
+        console.readLine("");
+      }
+    } catch (UserInterruptException exception) {
+      continueStreaming.set(false);
+    }
+    try {
+      queryPrintThread.join();
+    } catch (InterruptedException exception) {
+      throw new RuntimeException(exception);
     }
     console.println("Query terminated");
+    console.setPrompt(currentPrompt);
   }
 
   private void printJsonResponse(JsonStructure jsonResponse) throws IOException {
@@ -116,71 +218,46 @@ public class Cli {
     }
   }
 
-  private void printCommandList() throws IOException {
-    console.println("Cli cli commands: ");
-    console.println(
-        "------------------------------------------------------------------------------------ ");
-    console.println(
-        "list topics           .................... Show the list of available topics.");
-    console.println(
-        "list streams          .................... Show the list of available streams.");
-    console.println(
-        "list tables           .................... Show the list of available tables.");
-    console.println(
-        "describe <name>       .................... Show the schema of the given stream/table.");
-    console.println(
-        "show queries          .................... Show the list of running queries.");
-    console.println(
-        "print <topic name>    .................... Print the content of a given topic/stream.");
-    console.println(
-        "terminate <query id>  .................... Terminate the running query with the given id.");
+  private void printHelpMessage() throws IOException {
+    console.println("meta-commands (one per line, must be on their own line):");
     console.println();
-    console.println("For more information refer to www.kql.confluent.io");
+    console.println("    :exit               - Exit the REPL (^D is also fine)");
+    console.println("    :help               - Show this message");
+    console.println("    :prompt <prompt>    - Change the prompt to <prompt>");
+    console.println("                          Example: :prompt 'my ''special'' prompt> '");
+//    console.println("    :output             - Show the current output format");
+//    console.println("                          Example: :output");
+//    console.println("    :output <format>    - Change the output format to <format> (case-insensitive), "
+//        + "currently only 'json' and 'cli' are supported)"
+//    );
+//    console.println("                          Example: :output JSON");
+//    console.println("    :output CLI <width> - Change the output format to CLI, with a minimum column width of <width>");
+//    console.println("                          Example: :output CLI 69");
+//    console.println("    :server             - Show the current server");
+//    console.println("    :server <server>    - Change the current server to <server>");
+//    console.println("                          Example: :server confluent.io:6969'");
+    console.println("    :status             - Check on the statuses of all KQL statements processed by the server");
+    console.println("                          Example: :status");
+    console.println("    :status <id>        - Check on the status of the statement with ID <id>");
+    console.println("                          Example: :status KQL_NODE_69_STATEMENT_69");
+    console.println("    :query <query>      - Stream the results of <query> to the console");
+    console.println("                          Example: :query SELECT * FROM movies WHERE imdb_score = 69;");
     console.println();
+    console.println("default behavior:");
+    console.println();
+    console.println("    Lines are read one at a time and are sent to the server as KQL unless one of "
+        + "the following is true:"
+    );
+    console.println();
+    console.println("    1. The line is empty or entirely whitespace. In this case, no request is made to the server.");
+    console.println();
+    console.println("    2. The line begins with ':'. In this case, the line is parsed as a meta-command "
+        + "(as detailed above)."
+    );
+    console.println();
+    console.println("    3. The line ends with '\\'. In this case, lines are continuously read and stripped of their "
+        + "trailing newline and '\\' until one is encountered that does not end with '\\'; then, the concatenation of "
+        + "all lines read during this time is sent to the server as KQL."
+    );
   }
-
-  public static void main(String[] args)
-      throws Exception {
-
-//    KQL cliOptions = KQL.parse(args);
-//    if (cliOptions == null) {
-//      return;
-//    }
-//
-//    Map<String, Object> kqlProperties = KQL.getPropertiesMap(cliOptions.getPropertiesFile());
-//
-//    MetaStore metaStore;
-//    String catalogFile = cliOptions.getCatalogFile();
-//    if (catalogFile != null) {
-//      metaStore = new MetastoreUtil().loadMetaStoreFromJSONFile(catalogFile);
-//    } else {
-//      metaStore = new MetaStoreImpl();
-//    }
-//
-//    Cli kql = new Cli(metaStore, new KQLConfig(kqlProperties));
-//
-//    String queries = cliOptions.getQueries();
-//    if (queries != null) {
-//      kql.kqlEngine.runMultipleQueries(true, queries);
-//      Long queryTime = cliOptions.getQueryTime();
-//      if (queryTime != null) {
-//        Thread.sleep(queryTime);
-//      }
-//      return;
-//    }
-//
-//    String queryFile = cliOptions.getQueryFile();
-//    if (queryFile != null) {
-//      kql.isCLI = false;
-//      kql.runQueriesFromFile(queryFile);
-//      Long queryTime = cliOptions.getQueryTime();
-//      if (queryTime != null) {
-//        Thread.sleep(queryTime);
-//      }
-//      return;
-//    }
-//
-//    kql.startConsole();
-  }
-
 }
--- kql-cli/src/main/java/io/confluent/kql/KQL.java
+++ kql-cli/src/main/java/io/confluent/kql/KQL.java
@@ -7,15 +7,17 @@ import com.github.rvesse.airline.Cli;
 import com.github.rvesse.airline.annotations.Arguments;
 import com.github.rvesse.airline.annotations.Command;
 import com.github.rvesse.airline.annotations.Option;
-import com.github.rvesse.airline.annotations.restrictions.MutuallyExclusiveWith;
 import com.github.rvesse.airline.annotations.restrictions.Once;
+import com.github.rvesse.airline.annotations.restrictions.Port;
+import com.github.rvesse.airline.annotations.restrictions.PortType;
 import com.github.rvesse.airline.annotations.restrictions.Required;
-import com.github.rvesse.airline.annotations.restrictions.ranges.IntegerRange;
 import com.github.rvesse.airline.help.Help;
 import com.github.rvesse.airline.parser.errors.ParseException;
 import io.confluent.kql.rest.client.KQLRestClient;
 import io.confluent.kql.rest.server.KQLRestApplication;
+import io.confluent.kql.rest.server.KQLRestConfig;
 import io.confluent.rest.RestConfig;
+import org.apache.kafka.streams.StreamsConfig;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -27,54 +29,64 @@ public class KQL {
   @Command(name = "standalone", description = "Run a standalone Cli session")
   public static class Standalone implements Runnable {
 
-//    private static final String PORT_NUMBER_OPTION_NAME = "--port";
     private static final String PROPERTIES_FILE_OPTION_NAME = "--properties-file";
-    private static final String CATALOG_FILE_OPTION_NAME = "--catalog-file";
-    private static final String QUERY_FILE_OPTION_NAME = "--query-file";
-    private static final String QUERIES_OPTION_NAME = "--queries";
-    private static final String QUERY_TIME_OPTION_NAME = "--query-time";
 
-    private static final String NON_INTERACTIVE_QUERIES_TAG = "non-interactive queries";
+    private static final String PORT_NUMBER_OPTION_NAME = "--portNumber";
+    private static final int PORT_NUMBER_OPTION_DEFAULT = 6969;
 
-//    @IntegerRange(min = 0, max = 65535)
-//    @Option(
-//        name = PORT_NUMBER_OPTION_NAME,
-//        description = "The port to use for the connection"
-//    )
-//    private int port = 6969;
+    private static final String KAFKA_BOOTSTRAP_SERVER_OPTION_NAME = "--bootstrap-server";
+    private static final String KAFKA_BOOTSTRAP_SERVER_OPTION_DEFAULT = "localhost:9092";
 
-    @Required
+    private static final String APPLICATION_ID_OPTION_NAME = "--application-id";
+    private static final String APPLICATION_ID_OPTION_DEFAULT = "kql_standalone_cli";
+
+    private static final String NODE_ID_OPTION_NAME = "--node-id";
+    private static final String NODE_ID_OPTION_DEFAULT = "node";
+
+    private static final String COMMAND_TOPIC_SUFFIX_OPTION_NAME = "--command-topic-suffix";
+    private static final String COMMAND_TOPIC_SUFFIX_OPTION_DEFAULT = "commands";
+
+    @Port(acceptablePorts = PortType.ANY)
+    @Option(
+        name = PORT_NUMBER_OPTION_NAME,
+        description = "The portNumber to use for the connection (defaults to " + PORT_NUMBER_OPTION_DEFAULT + ")"
+    )
+    private Integer portNumber;
+
+    @Option(
+        name = KAFKA_BOOTSTRAP_SERVER_OPTION_NAME,
+        description = "The Kafka server to connect to (defaults to " + KAFKA_BOOTSTRAP_SERVER_OPTION_DEFAULT + ")"
+    )
+    private String bootstrapServer;
+
+    @Option(
+        name = APPLICATION_ID_OPTION_NAME,
+        description = "The application ID to use for the created Kafka Streams instance(s) (defaults to "
+            + APPLICATION_ID_OPTION_DEFAULT + ")"
+    )
+    private String applicationId;
+
+    @Option(
+        name = NODE_ID_OPTION_NAME,
+        description = "The node ID to assign to the standalone KQL server instance (defaults to "
+            + NODE_ID_OPTION_DEFAULT + ")"
+    )
+    private String nodeId;
+
+    @Option(
+        name = COMMAND_TOPIC_SUFFIX_OPTION_NAME,
+        description = "The suffix to append to the end of the name of the command topic (defaults to "
+            + COMMAND_TOPIC_SUFFIX_OPTION_DEFAULT + ")"
+    )
+    private String commandTopicSuffix;
+
     @Option(
         name = PROPERTIES_FILE_OPTION_NAME,
-        description = "A file specifying properties for Cli and its underlying Kafka Streams instance(s)"
+        description = "A file specifying properties for KQL and its underlying Kafka Streams instance(s) "
+            + "(can specify port number, bootstrap server, etc. but these options will be overridden if also given via "
+            + "flags)"
     )
     private String propertiesFile;
-
-    @Option(
-        name = CATALOG_FILE_OPTION_NAME,
-        description = "A file to import metastore data from before execution"
-    )
-    private String catalogFile;
-
-    @MutuallyExclusiveWith(tag = NON_INTERACTIVE_QUERIES_TAG)
-    @Option(
-        name = QUERY_FILE_OPTION_NAME,
-        description = "A file to run non-interactive queries from"
-    )
-    private String queryFile;
-
-    @MutuallyExclusiveWith(tag = NON_INTERACTIVE_QUERIES_TAG)
-    @Option(
-        name = QUERIES_OPTION_NAME,
-        description = "One or more non-interactive queries to run"
-    )
-    private String queries;
-
-    @Option(
-        name = QUERY_TIME_OPTION_NAME,
-        description = "How long to run non-interactive queries for (ms)"
-    )
-    private Long queryTime;
 
     @Override
     public void run() {
@@ -118,23 +130,55 @@ public class KQL {
       }
     }
 
-    // TODO: If the properties passed to the KQLRestApplication need to be mucked with, do it here; otherwise, get rid
-    // of this method
     private Properties getStandaloneProperties() throws IOException {
       Properties properties = new Properties();
-      properties.load(new FileInputStream(propertiesFile));
+      addDefaultProperties(properties);
+      addFileProperties(properties);
+      addFlagProperties(properties);
       return properties;
+    }
+
+    private void addDefaultProperties(Properties properties) {
+      properties.put(RestConfig.LISTENERS_CONFIG, String.format("http://localhost:%d", PORT_NUMBER_OPTION_DEFAULT));
+      properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BOOTSTRAP_SERVER_OPTION_DEFAULT);
+      properties.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID_OPTION_DEFAULT);
+      properties.put(KQLRestConfig.NODE_ID_CONFIG, NODE_ID_OPTION_DEFAULT);
+      properties.put(KQLRestConfig.COMMAND_TOPIC_SUFFIX_CONFIG, COMMAND_TOPIC_SUFFIX_OPTION_DEFAULT);
+    }
+
+    private void addFileProperties(Properties properties) throws IOException {
+      if (propertiesFile != null) {
+        properties.load(new FileInputStream(propertiesFile));
+      }
+    }
+
+    private void addFlagProperties(Properties properties) {
+      if (portNumber != null) {
+        properties.put(RestConfig.LISTENERS_CONFIG, String.format("http://localhost:%d", portNumber));
+      }
+      if (bootstrapServer != null) {
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+      }
+      if (applicationId != null) {
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
+      }
+      if (nodeId != null) {
+        properties.put(KQLRestConfig.NODE_ID_CONFIG, nodeId);
+      }
+      if (commandTopicSuffix != null) {
+        properties.put(KQLRestConfig.COMMAND_TOPIC_SUFFIX_CONFIG, commandTopicSuffix);
+      }
     }
   }
 
-  @Command(name = "distributed", description = "Connect to a distributed Cli session")
+  @Command(name = "distributed", description = "Connect to a distributed KQL session")
   public static class Distributed implements Runnable {
 
     @Once
     @Required
     @Arguments(
         title = "server",
-        description = "The address of the Cli server to connect to (ex: http://confluent.io:6969)"
+        description = "The address of the KQL server to connect to (ex: http://confluent.io:6969)"
     )
     private String server;
 

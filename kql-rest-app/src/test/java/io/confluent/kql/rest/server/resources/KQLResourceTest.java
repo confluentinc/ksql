840a26cc08e7b52ee3fae0824b2674b2b94900be
Chris Egerton <fearthecellos@gmail.com>
2017-05-03 16:45:03 -0700

    Remove star import

--- kql-rest-app/src/test/java/io/confluent/kql/rest/server/resources/KQLResourceTest.java
+++ kql-rest-app/src/test/java/io/confluent/kql/rest/server/resources/KQLResourceTest.java
@@ -38,7 +38,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import static org.easymock.EasyMock.*;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.easymock.EasyMock.mock;
+import static org.easymock.EasyMock.replay;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;

2b141d2f22f2306ca00d4bbbe871121c84a80358
junyoung.kim <junyoung.kim@woowahan.com>
2017-09-05 12:57:21 +0900

    Fixed misspelled words

--- ksql-core/src/main/java/io/confluent/ksql/util/StringUtil.java
+++ ksql-core/src/main/java/io/confluent/ksql/util/StringUtil.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class StringUtil {
 
   public static String cleanQuotes(final String stringWithQuotes) {
-    // TODO: move check to grammer
+    // TODO: move check to grammar
     if (stringWithQuotes.startsWith("'") && stringWithQuotes.endsWith("'")) {
       return stringWithQuotes.substring(1, stringWithQuotes.length() - 1);
     } else {

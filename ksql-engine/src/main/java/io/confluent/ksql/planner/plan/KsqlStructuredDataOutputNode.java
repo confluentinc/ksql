863f107ea7655ec704e1407e96779f5df2e8f699
Damian Guy <damian.guy@gmail.com>
2017-10-11 16:43:05 +0100

    extract methods, remove duplication, etc from PhysicalPlanBuilder

--- ksql-engine/src/main/java/io/confluent/ksql/planner/plan/KsqlStructuredDataOutputNode.java
+++ ksql-engine/src/main/java/io/confluent/ksql/planner/plan/KsqlStructuredDataOutputNode.java
@@ -19,6 +19,9 @@ package io.confluent.ksql.planner.plan;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.confluent.ksql.metastore.KsqlTopic;
+import io.confluent.ksql.serde.KsqlTopicSerDe;
+import io.confluent.ksql.util.KsqlConfig;
+
 import org.apache.kafka.connect.data.Field;
 import org.apache.kafka.connect.data.Schema;
 
@@ -72,6 +75,93 @@ public class KsqlStructuredDataOutputNode extends OutputNode {
 
   public Map<String, Object> getOutputProperties() {
     return outputProperties;
+  }
+
+  public KsqlTopicSerDe getTopicSerde() {
+    return ksqlTopic.getKsqlTopicSerDe();
+  }
+
+  public static class Builder {
+    private PlanNodeId id;
+    private PlanNode source;
+    private Schema schema;
+    private Field timestampField;
+    private Field keyField;
+    private KsqlTopic ksqlTopic;
+    private String topicName;
+    private Map<String, Object> outputProperties;
+    private Optional<Integer> limit;
+
+    public KsqlStructuredDataOutputNode build() {
+      return new KsqlStructuredDataOutputNode(id,
+          source,
+          schema,
+          timestampField,
+          keyField,
+          ksqlTopic,
+          topicName,
+          outputProperties,
+          limit);
+    }
+
+    public static Builder from(final KsqlStructuredDataOutputNode original) {
+      return new Builder()
+          .withId(original.getId())
+          .withSource(original.getSource())
+          .withSchema(original.getSchema())
+          .withTimestampField(original.getTimestampField())
+          .withKeyField(original.getKeyField())
+          .withKsqlTopic(original.getKsqlTopic())
+          .withTopicName(original.getKafkaTopicName())
+          .withOutputProperties(original.getOutputProperties())
+          .withLimit(original.getLimit());
+    }
+
+    public Builder withLimit(final Optional<Integer> limit) {
+      this.limit = limit;
+      return this;
+    }
+
+    public Builder withOutputProperties(Map<String, Object> outputProperties) {
+      this.outputProperties = outputProperties;
+      return this;
+    }
+
+    public Builder withTopicName(String topicName) {
+      this.topicName = topicName;
+      return this;
+    }
+
+    public Builder withKsqlTopic(KsqlTopic ksqlTopic) {
+      this.ksqlTopic = ksqlTopic;
+      return this;
+    }
+
+    public Builder withKeyField(Field keyField) {
+      this.keyField = keyField;
+      return this;
+    }
+
+    public Builder withTimestampField(Field timestampField) {
+      this.timestampField = timestampField;
+      return this;
+    }
+
+    public Builder withSchema(Schema schema) {
+      this.schema = schema;
+      return this;
+    }
+
+    public Builder withSource(PlanNode source) {
+      this.source = source;
+      return this;
+    }
+
+    public Builder withId(final PlanNodeId id) {
+      this.id = id;
+      return this;
+    }
+
   }
 
 }

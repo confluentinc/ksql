f9e441621fd5ca2573b01696d21bd88773d85a67
bluemonk3y <neil@confluent.io>
2017-10-12 13:37:00 +0100

    new PR with migrated tests

--- ksql-engine/src/test/java/io/confluent/ksql/StreamsSelectAndProjectIntTest.java
+++ ksql-engine/src/test/java/io/confluent/ksql/StreamsSelectAndProjectIntTest.java
@@ -1,22 +1,25 @@
 package io.confluent.ksql;
 
-import io.confluent.ksql.util.OrderDataProvider;
-import io.confluent.ksql.util.SchemaUtil;
+import io.confluent.ksql.util.*;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.kstream.internals.TimeWindow;
+import org.apache.kafka.streams.kstream.internals.WindowedDeserializer;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 import static io.confluent.ksql.util.MetaStoreFixture.assertExpectedResults;
+import static io.confluent.ksql.util.MetaStoreFixture.assertExpectedWindowedResults;
 
 public class StreamsSelectAndProjectIntTest {
 
@@ -39,6 +42,45 @@ public class StreamsSelectAndProjectIntTest {
     testHarness.stop();
   }
 
+  @Test
+  public void testTimestampColumnSelection() throws Exception {
+
+    publishOrdersTopicData();
+
+    createOrdersStream();
+
+    final String stream1Name = "ORIGINALSTREAM";
+    final String stream2Name = "TIMESTAMPSTREAM";
+    final String query1String =
+            String.format("CREATE STREAM %s WITH (timestamp='RTIME') AS SELECT ROWKEY AS RKEY, "
+                            + "ROWTIME+10000 AS "
+                            + "RTIME, ROWTIME+100 AS RT100, ORDERID, ITEMID "
+                            + "FROM %s WHERE ORDERUNITS > 20 AND ITEMID = 'ITEM_8'; "
+                            + "CREATE STREAM %s AS SELECT ROWKEY AS NEWRKEY, "
+                            + "ROWTIME AS NEWRTIME, RKEY, RTIME, RT100, ORDERID, ITEMID "
+                            + "FROM %s ;", stream1Name,
+                    "ORDERS", stream2Name, stream1Name);
+
+
+    ksqlContext.sql(query1String);
+
+    Map<String, GenericRow> expectedResults = new HashMap<>();
+    expectedResults.put("8", new GenericRow(Arrays.asList(null, null, "8", recordMetadataMap.get("8")
+                    .timestamp() + 10000, "8", recordMetadataMap.get("8").timestamp() + 10000,
+            recordMetadataMap.get("8").timestamp
+                    () + 100, "ORDER_6", "ITEM_8")));
+
+    Schema resultSchema = ksqlContext.getMetaStore().getSource(stream2Name).getSchema();
+
+    Map<String, GenericRow> results1 = testHarness.consumeData(stream1Name, resultSchema , expectedResults.size(), new StringDeserializer());
+    Map<String, GenericRow> results2 = testHarness.consumeData(stream2Name, resultSchema , expectedResults.size(), new StringDeserializer());
+
+    System.out.println("Results:" + results1);
+
+    Assert.assertEquals(expectedResults.size(), results2.size());
+    assertExpectedResults(expectedResults, results2);
+  }
+
 
   @Test
   public void testSelectProjectKeyTimestamp() throws Exception {
@@ -49,15 +91,15 @@ public class StreamsSelectAndProjectIntTest {
 
     ksqlContext.sql("CREATE STREAM PROJECT_KEY_TIMESTAMP AS SELECT ROWKEY AS RKEY, ROWTIME AS RTIME, ITEMID FROM ORDERS WHERE ORDERUNITS > 20 AND ITEMID = 'ITEM_8';");
 
-    Schema resultSchema = SchemaUtil.removeImplicitRowTimeRowKeyFromSchema(ksqlContext.getMetaStore().getSource("PROJECT_KEY_TIMESTAMP").getSchema());
+    Schema resultSchema = ksqlContext.getMetaStore().getSource("PROJECT_KEY_TIMESTAMP").getSchema();
 
     Map<String, GenericRow> results = testHarness.consumeData("PROJECT_KEY_TIMESTAMP", resultSchema , dataProvider.data().size(), new StringDeserializer());
 
     Map<String, GenericRow> expectedResults = new HashMap<>();
-    expectedResults.put("8", new GenericRow(Arrays.asList("8", recordMetadataMap.get("8").timestamp(), "ITEM_8")));
+    expectedResults.put("8", new GenericRow(Arrays.asList(null, null, "8", recordMetadataMap.get("8").timestamp(), "ITEM_8")));
 
     Assert.assertEquals(expectedResults.size(), results.size());
-    assertExpectedResults(results, expectedResults);
+    assertExpectedResults(expectedResults, results);
   }
 
   @Test
@@ -68,12 +110,13 @@ public class StreamsSelectAndProjectIntTest {
 
     ksqlContext.sql("CREATE STREAM PROJECT_STREAM AS SELECT ITEMID, ORDERUNITS, PRICEARRAY FROM ORDERS;");
 
-    Schema resultSchema = SchemaUtil.removeImplicitRowTimeRowKeyFromSchema(ksqlContext.getMetaStore().getSource("PROJECT_STREAM").getSchema());
+    Schema resultSchema = ksqlContext.getMetaStore().getSource("PROJECT_STREAM").getSchema();
 
     Map<String, GenericRow> easyOrdersData = testHarness.consumeData("PROJECT_STREAM", resultSchema, dataProvider.data().size(), new StringDeserializer());
 
     GenericRow value = easyOrdersData.values().iterator().next();
-    Assert.assertEquals( "ITEM_1", value.getColumns().get(0));
+    // skip over first to values (rowKey, rowTime)
+    Assert.assertEquals( "ITEM_1", value.getColumns().get(2));
   }
 
 
@@ -112,7 +155,7 @@ public class StreamsSelectAndProjectIntTest {
   }
 
   private void createOrdersStream() throws Exception {
-    ksqlContext.sql("CREATE STREAM orders (ORDERTIME bigint, ORDERID varchar, ITEMID varchar, ORDERUNITS double, PRICEARRAY array<double>, KEYVALUEMAP map<varchar, double>) WITH (kafka_topic='TestTopic', value_format='JSON');");
+    ksqlContext.sql("CREATE STREAM ORDERS (ORDERTIME bigint, ORDERID varchar, ITEMID varchar, ORDERUNITS double, PRICEARRAY array<double>, KEYVALUEMAP map<varchar, double>) WITH (kafka_topic='TestTopic', value_format='JSON', key='ordertime');");
   }
 
   private OrderDataProvider publishOrdersTopicData() throws InterruptedException, TimeoutException, ExecutionException {

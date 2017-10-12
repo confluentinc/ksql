e111ff800523832df974b3cd920f820c565536c5
bluemonk3y <neil@confluent.io>
2017-10-12 09:54:28 +0100

    passing

--- ksql-engine/src/test/java/io/confluent/ksql/IntegrationTestHarness.java
+++ ksql-engine/src/test/java/io/confluent/ksql/IntegrationTestHarness.java
@@ -38,7 +38,7 @@ public class IntegrationTestHarness {
 
   public static final long TEST_RECORD_FUTURE_TIMEOUT_MS = 5000;
 
-  public static final long RESULTS_POLL_MAX_TIME_MS = 30000;
+  public static final long RESULTS_POLL_MAX_TIME_MS = 10000;
   public static final long RESULTS_EXTRA_POLL_TIME_MS = 250;
 
   public static final String CONSUMER_GROUP_ID_PREFIX = "KSQL_Iintegration_Test_Consumer_";

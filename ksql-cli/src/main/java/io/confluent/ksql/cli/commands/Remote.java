9296fe4e1fbbd7a89d9c98b57c85308ba8a34c15
Magesh Nandakumar <magesh.n.kumar@gmail.com>
2017-11-13 11:32:17 -0800

    Ksql beacon (#429)
    KSQL Version Checker

--- ksql-cli/src/main/java/io/confluent/ksql/cli/commands/Remote.java
+++ ksql-cli/src/main/java/io/confluent/ksql/cli/commands/Remote.java
@@ -28,6 +28,7 @@ import io.confluent.ksql.cli.RemoteCli;
 import io.confluent.ksql.rest.client.KsqlRestClient;
 import io.confluent.ksql.cli.console.Console;
 import io.confluent.ksql.cli.console.JLineTerminal;
+import io.confluent.ksql.version.metrics.collector.KsqlModuleType;
 import io.confluent.ksql.util.KsqlConfig;
 
 import java.io.FileInputStream;
@@ -68,6 +69,7 @@ public class Remote extends AbstractCliCommands {
     KsqlRestClient restClient = new KsqlRestClient(server, propertiesMap);
     Console terminal = new JLineTerminal(parseOutputFormat(), restClient);
 
+    versionCheckerAgent.start(KsqlModuleType.REMOTE_CLI, properties);
     return new RemoteCli(
         streamedQueryRowLimit,
         streamedQueryTimeoutMs,

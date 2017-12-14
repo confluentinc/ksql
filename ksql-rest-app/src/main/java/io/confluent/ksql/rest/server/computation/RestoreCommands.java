e3199b0f74f0abd915f285d50a03fcbdc0aceeec
Damian Guy <damian.guy@gmail.com>
2017-12-14 17:36:21 +0000

    don't execute dropped streams/tables, but update metastore

--- ksql-rest-app/src/main/java/io/confluent/ksql/rest/server/computation/RestoreCommands.java
+++ ksql-rest-app/src/main/java/io/confluent/ksql/rest/server/computation/RestoreCommands.java
@@ -21,12 +21,17 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import io.confluent.ksql.query.QueryId;
+import io.confluent.ksql.util.Pair;
 
 class RestoreCommands {
-  private final Map<CommandId, Command> toRestore = new LinkedHashMap<>();
+
+  private final Map<Pair<Integer, CommandId>, Command> toRestore = new LinkedHashMap<>();
   private final Map<QueryId, CommandId> allTerminatedQueries = new HashMap<>();
+  private final Map<String, CommandId> dropped = new HashMap<>();
   private final List<CommandId> allCommandIds = new ArrayList<>();
 
   void addCommand(final CommandId key, final Command value) {
@@ -35,35 +40,37 @@ class RestoreCommands {
       if (allCommandIds.contains(key)) {
         allCommandIds.remove(key);
       }
-    } else if (!toRestore.containsKey(key)){
-      toRestore.put(key, value);
+    } else {
+      toRestore.put(new Pair<>(allCommandIds.size(), key), value);
+      if (key.getAction() == CommandId.Action.DROP) {
+        dropped.put(key.getEntity(), key);
+      }
     }
     allCommandIds.add(key);
   }
 
-  boolean remove(final CommandId commandId) {
-    if (toRestore.remove(commandId) != null) {
-      allCommandIds.remove(commandId);
-      return true;
-    }
-    return false;
-  }
 
   interface ForEach {
     void apply(final CommandId commandId,
                final Command command,
-               final Map<QueryId, CommandId> terminatedQueries);
+               final Map<QueryId, CommandId> terminatedQueries,
+               final boolean dropped);
   }
 
   void forEach(final ForEach action) {
-    toRestore.forEach((commandId, command) -> {
-      final int commandIdIdx = allCommandIds.indexOf(commandId);
+    toRestore.forEach((commandIdIndexPair, command) -> {
       final Map<QueryId, CommandId> terminatedAfter = new HashMap<>();
       allTerminatedQueries.entrySet().stream()
-          .filter(entry -> allCommandIds.indexOf(entry.getValue()) > commandIdIdx)
+          .filter(entry -> allCommandIds.indexOf(entry.getValue()) > commandIdIndexPair.left)
           .forEach(queryIdCommandIdEntry ->
               terminatedAfter.put(queryIdCommandIdEntry.getKey(), queryIdCommandIdEntry.getValue()));
-      action.apply(commandId, command, terminatedAfter);
+      final Set<String> droppedEntities = this.dropped.entrySet().stream()
+          .filter(entry -> allCommandIds.indexOf(entry.getValue()) > commandIdIndexPair.left)
+          .map(Map.Entry::getKey)
+          .collect(Collectors.toSet());
+      action.apply(commandIdIndexPair.right, command,
+          terminatedAfter,
+          droppedEntities.contains(commandIdIndexPair.right.getEntity()));
     });
   }
 

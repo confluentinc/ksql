fba2975d20cca3c962a8bb64d728e53275b8c41e
Flavio Stutz <flaviostutz@gmail.com>
2018-02-22 18:37:46 -0300

    Updating window tumbling from 10 to 60 seconds
    As the table is "per minute", 60 seconds should be used in window tumbling.

--- ksql-clickstream-demo/demo/clickstream-schema.sql
+++ ksql-clickstream-demo/demo/clickstream-schema.sql
@@ -17,7 +17,7 @@ CREATE STREAM clickstream (_time bigint,time varchar, ip varchar, request varcha
 
  -- number of events per minute - think about key-for-distribution-purpose - shuffling etc - shouldnt use 'userid'
 DROP TABLE events_per_min;
-CREATE table events_per_min AS SELECT userid, count(*) AS events FROM clickstream window  TUMBLING (size 10 second) GROUP BY userid;
+CREATE table events_per_min AS SELECT userid, count(*) AS events FROM clickstream window TUMBLING (size 60 second) GROUP BY userid;
 
 -- VIEW - Enrich with rowTime
 DROP TABLE events_per_min_ts;

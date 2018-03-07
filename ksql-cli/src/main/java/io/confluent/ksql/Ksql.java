79c20d8a76599ddac2c03dd44c4e761832dba5fa
Hojjat Jafarpour <hjafarpour@gmail.com>
2018-03-07 13:12:32 -0800

    Applied review feedback.

--- ksql-cli/src/main/java/io/confluent/ksql/Ksql.java
+++ ksql-cli/src/main/java/io/confluent/ksql/Ksql.java
@@ -35,10 +35,8 @@ import io.confluent.ksql.version.metrics.collector.KsqlModuleType;
 public class Ksql {
 
   public static void main(String[] args) throws IOException {
-    if (args.length == 0) {
-      args = new String[]{"http://localhost:8080"};
-    }
-    final Options options = Options.parse(args);
+    final Options options = args.length == 0 ? Options.parse("http://localhost:8080")
+                                             : Options.parse(args);
     if (options == null) {
       System.exit(-1);
     }

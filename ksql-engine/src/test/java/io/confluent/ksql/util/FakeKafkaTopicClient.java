eea2e46f97b41bef51e37782aad425436d79104f
Hojjat Jafarpour <hjafarpour@users.noreply.github.com>
2018-03-28 17:35:43 -0700

    Tests fixes. (#1070)
    * Tests fixes.
    
    * Made fields private.

--- ksql-engine/src/test/java/io/confluent/ksql/util/FakeKafkaTopicClient.java
+++ ksql-engine/src/test/java/io/confluent/ksql/util/FakeKafkaTopicClient.java
@@ -37,15 +37,15 @@ import java.util.stream.IntStream;
 public class FakeKafkaTopicClient implements KafkaTopicClient {
 
   class FakeTopic {
-    final String topicName;
-    final int numPartitions;
-    final short replicatonFactor;
-    final String cleanupPolicy;
+    private final String topicName;
+    private final int numPartitions;
+    private final short replicatonFactor;
+    private final TopicCleanupPolicy cleanupPolicy;
 
     public FakeTopic(String topicName,
                      int numPartitions,
                      short replicatonFactor,
-                     String cleanupPolicy) {
+                     TopicCleanupPolicy cleanupPolicy) {
       this.topicName = topicName;
       this.numPartitions = numPartitions;
       this.replicatonFactor = replicatonFactor;
@@ -73,7 +73,7 @@ public class FakeKafkaTopicClient implements KafkaTopicClient {
               .collect(Collectors.toList());
       return new TopicDescription(topicName, false, partitionInfoList);
     }
-    public String getCleanupPolicy() {
+    public TopicCleanupPolicy getCleanupPolicy() {
       return cleanupPolicy;
     }
   }
@@ -84,8 +84,8 @@ public class FakeKafkaTopicClient implements KafkaTopicClient {
   public void createTopic(String topic, int numPartitions, short replicatonFactor, boolean isCompacted) {
     if (!topicMap.containsKey(topic)) {
       topicMap.put(topic, new FakeTopic(topic, numPartitions, replicatonFactor, isCompacted?
-                                                                                "compact":
-                                                                                "delete"));
+                                                                                TopicCleanupPolicy.COMPACT:
+                                                                                TopicCleanupPolicy.DELETE));
     }
   }
 
@@ -94,8 +94,8 @@ public class FakeKafkaTopicClient implements KafkaTopicClient {
       String> configs, boolean isCompacted) {
     if (!topicMap.containsKey(topic)) {
       topicMap.put(topic, new FakeTopic(topic, numPartitions, replicatonFactor, isCompacted?
-                                                                                "compact":
-                                                                                "delete"));
+                                                                                TopicCleanupPolicy.COMPACT:
+                                                                                TopicCleanupPolicy.DELETE));
     }
   }
 

12cf452f90ff1037a9dfef4f74e79ea58a265b98
dan norwood <dan.norwood@gmail.com>
2018-04-25 17:28:26 -0700

    Make the websockets endpoints return strings instead of raw bytes (#1198)

--- ksql-rest-app/src/main/java/io/confluent/ksql/rest/server/KsqlRestApplication.java
+++ ksql-rest-app/src/main/java/io/confluent/ksql/rest/server/KsqlRestApplication.java
@@ -276,6 +276,7 @@ public class KsqlRestApplication extends Application<KsqlRestConfig> implements 
                       exec
                   );
                 }
+
               })
               .build()
       );

b31d23afa67d45529476415a8301f35f4fc1efb3
Apurva Mehta <apurva@confluent.io>
2018-06-13 14:22:21 -0700

    Fix failing test

--- ksql-engine/src/test/java/io/confluent/ksql/planner/plan/JoinNodeTest.java
+++ ksql-engine/src/test/java/io/confluent/ksql/planner/plan/JoinNodeTest.java
@@ -219,8 +219,8 @@ public class JoinNodeTest {
       buildJoin("SELECT t1.col0, t2.col0, t2.col1 FROM test1 t1 LEFT JOIN test2 t2 ON t1.col0 = t2.col0;");
     } catch (KsqlException e) {
       Assert.assertThat(e.getMessage(), equalTo(
-          "Can't join STREAM with TABLE since the number of partitions don't match. STREAM "
-          + "partitions = 1; TABLE partitions = 2. Please repartition either one so that the "
+          "Can't join TEST1 with TEST2 since the number of partitions don't match. TEST1 "
+          + "partitions = 1; TEST2 partitions = 2. Please repartition either one so that the "
           + "number of partitions match."
       ));
     }

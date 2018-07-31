078048feaff064ff4daa2ed4f5640ff53b6bc71c
Andy Coates <big-andy-coates@users.noreply.github.com>
2018-07-31 13:26:04 +0100

    Fix import order

--- ksql-engine/src/test/java/io/confluent/ksql/codegen/CodeGenRunnerTest.java
+++ ksql-engine/src/test/java/io/confluent/ksql/codegen/CodeGenRunnerTest.java
@@ -18,6 +18,12 @@ package io.confluent.ksql.codegen;
 
 import com.google.common.collect.ImmutableMap;
 
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.Collections;
@@ -26,11 +32,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import org.apache.kafka.connect.data.Schema;
-import org.apache.kafka.connect.data.SchemaBuilder;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 
 import io.confluent.ksql.analyzer.Analysis;
 import io.confluent.ksql.analyzer.AnalysisContext;

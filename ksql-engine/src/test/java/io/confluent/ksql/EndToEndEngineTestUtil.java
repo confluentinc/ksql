7ad875a7a41b9bade639f1df0ab1676ad4ec02cc
Bill Bejeck <bill@confluent.io>
2018-08-15 12:12:43 -0400

    KSQL-1361: add topology optimization config

--- ksql-engine/src/test/java/io/confluent/ksql/EndToEndEngineTestUtil.java
+++ ksql-engine/src/test/java/io/confluent/ksql/EndToEndEngineTestUtil.java
@@ -686,7 +686,7 @@ final class EndToEndEngineTestUtil {
           put("commit.interval.ms", 0);
           put("cache.max.bytes.buffering", 0);
           put("auto.offset.reset", "earliest");
-          put("", "");
+          put("topology.optimization", "all");
           put(StreamsConfig.STATE_DIR_CONFIG, TestUtils.tempDirectory().getPath());
       }};
       if(additionalConfigs != null){

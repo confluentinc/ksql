c419927e1df367a6eed3370c8f273c452c71660d
Andy Coates <big-andy-coates@users.noreply.github.com>
2018-10-06 17:16:23 +0100

    Display stats timestamps in unambiguous format.
    Fixes #2002

---
+++ ksql-common/src/test/java/io/confluent/ksql/metrics/TopicSensorsTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 Confluent Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.confluent.ksql.metrics;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import io.confluent.ksql.metrics.TopicSensors.Stat;
+import org.junit.Test;
+
+public class TopicSensorsTest {
+
+  @Test
+  public void shouldFormatTimestampInUnambiguousFormatAndUTC() {
+    // Given:
+    final Stat stat = new Stat("stat", 5.0, 1538842403035L);
+
+    // When:
+    final String timestamp = stat.timestamp();
+
+    // Then:
+    assertThat(timestamp, is("2018-10-06T16:13:23.035Z"));
+  }
+}

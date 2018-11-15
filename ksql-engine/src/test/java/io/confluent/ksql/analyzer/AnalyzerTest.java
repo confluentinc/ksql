8105f0d22f395fecaa76e8bec7a9588978b93c94
Hojjat Jafarpour <hjafarpour@users.noreply.github.com>
2018-11-14 16:08:56 -0800

    Fix the build failure. (#2153)

--- ksql-engine/src/test/java/io/confluent/ksql/analyzer/AnalyzerTest.java
+++ ksql-engine/src/test/java/io/confluent/ksql/analyzer/AnalyzerTest.java
@@ -208,7 +208,7 @@ public class AnalyzerTest {
     final String simpleQuery = "CREATE STREAM FOO WITH (KAFKA_TOPIC='TEST_TOPIC1') AS SELECT col0, col2, col3 FROM test1 WHERE col0 > 100;";
     // The following few lines are only needed for this test
     final MetaStore testMetastore = metaStore.clone();
-    final KsqlTopic ksqlTopic = new KsqlTopic("FOO", "TEST_TOPIC1", new KsqlJsonTopicSerDe());
+    final KsqlTopic ksqlTopic = new KsqlTopic("FOO", "TEST_TOPIC1", new KsqlJsonTopicSerDe(), true);
     testMetastore.putTopic(ksqlTopic);
     final List<Statement> statements = getPreparedStatements(simpleQuery, testMetastore)
         .stream()

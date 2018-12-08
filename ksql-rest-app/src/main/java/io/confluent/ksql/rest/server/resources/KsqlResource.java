8104a7f352d08f32eea2aa3cf93d0562a7d7e7f3
Rohan Desai <desai.p.rohan@gmail.com>
2018-12-08 10:34:34 -0800

    Fix broken version checker test

--- ksql-rest-app/src/main/java/io/confluent/ksql/rest/server/resources/KsqlResource.java
+++ ksql-rest-app/src/main/java/io/confluent/ksql/rest/server/resources/KsqlResource.java
@@ -188,8 +188,6 @@ public class KsqlResource {
   @POST
   public Response handleKsqlStatements(final KsqlRequest request) {
     activenessRegistrar.updateLastRequestTime();
-
-    activenessRegistrar.updateLastRequestTime();
     try {
       final List<PreparedStatement<?>> statements = parseStatements(request.getKsql());
 

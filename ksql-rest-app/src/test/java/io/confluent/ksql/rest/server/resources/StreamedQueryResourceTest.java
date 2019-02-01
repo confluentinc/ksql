ae1087762dcc008054d2690a5c0df0ea51ef2081
Victoria Xia <victoria.f.xia281@gmail.com>
2019-01-31 19:07:13 -0800

    Make CLI requests wait for last command sequence number (#2280)

--- ksql-rest-app/src/test/java/io/confluent/ksql/rest/server/resources/StreamedQueryResourceTest.java
+++ ksql-rest-app/src/test/java/io/confluent/ksql/rest/server/resources/StreamedQueryResourceTest.java
@@ -93,6 +93,7 @@ import org.junit.runner.RunWith;
 public class StreamedQueryResourceTest {
 
   private static final Duration DISCONNECT_CHECK_INTERVAL = Duration.ofMillis(1000);
+  private static final Duration COMMAND_QUEUE_CATCHUP_TIMOEUT = Duration.ofMillis(1000);
 
   @Rule
   public final ExpectedException expectedException = ExpectedException.none();
@@ -135,6 +136,7 @@ public class StreamedQueryResourceTest {
         mockStatementParser,
         commandQueue,
         DISCONNECT_CHECK_INTERVAL,
+        COMMAND_QUEUE_CATCHUP_TIMOEUT,
         activenessRegistrar);
   }
 

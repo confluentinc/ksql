8a9cb88c3a348456ff0b3add1183163302443c18
Rohan Desai <desai.p.rohan@gmail.com>
2019-02-15 16:59:35 -0800

    rebase

--- ksql-rest-app/src/main/java/io/confluent/ksql/rest/server/computation/ConfigTopicKey.java
+++ ksql-rest-app/src/main/java/io/confluent/ksql/rest/server/computation/ConfigTopicKey.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2019 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License; you may not use this file
+ * except in compliance with the License.  You may obtain a copy of the License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package io.confluent.ksql.rest.server.computation;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -19,7 +33,7 @@ abstract class ConfigTopicKey {
   public static class StringKey extends ConfigTopicKey {
     private final String value;
 
-    public StringKey(@JsonProperty("value") final String value) {
+    StringKey(@JsonProperty("value") final String value) {
       this.value = value;
     }
 
@@ -35,7 +49,7 @@ abstract class ConfigTopicKey {
       if (o == null || getClass() != o.getClass()) {
         return false;
       }
-      StringKey stringKey = (StringKey) o;
+      final StringKey stringKey = (StringKey) o;
       return Objects.equals(value, stringKey.value);
     }
 

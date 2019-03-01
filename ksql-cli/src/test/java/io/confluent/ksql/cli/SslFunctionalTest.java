63d498a61776484981ce6a060a232489f0b410d2
Andy Coates <big-andy-coates@users.noreply.github.com>
2019-03-01 22:17:52 +0000

    Improve test to actually query Kafka

--- ksql-cli/src/test/java/io/confluent/ksql/cli/SslFunctionalTest.java
+++ ksql-cli/src/test/java/io/confluent/ksql/cli/SslFunctionalTest.java
@@ -17,13 +17,15 @@ package io.confluent.ksql.cli;
 
 import static java.util.Collections.emptyMap;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.both;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.either;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.internal.matchers.ThrowableCauseMatcher.hasCause;
 import static org.junit.internal.matchers.ThrowableMessageMatcher.hasMessage;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.net.UrlEscapers;
 import io.confluent.common.utils.IntegrationTest;
 import io.confluent.ksql.rest.client.KsqlRestClient;
 import io.confluent.ksql.rest.client.RestResponse;
@@ -32,7 +34,10 @@ import io.confluent.ksql.test.util.EmbeddedSingleNodeKafkaCluster;
 import io.confluent.ksql.test.util.TestKsqlRestApp;
 import io.confluent.ksql.test.util.secure.ClientTrustStore;
 import io.confluent.ksql.test.util.secure.ServerKeyStore;
+import io.confluent.ksql.util.OrderDataProvider;
+import io.confluent.ksql.util.TopicProducer;
 import io.confluent.rest.RestConfig;
+import java.io.EOFException;
 import java.net.URI;
 import java.util.Collections;
 import java.util.Map;
@@ -46,13 +51,14 @@ import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.http.HttpStatus.Code;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.websocket.api.Session;
-import org.eclipse.jetty.websocket.api.UpgradeException;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -63,10 +69,12 @@ import org.junit.rules.RuleChain;
 @Category({IntegrationTest.class})
 public class SslFunctionalTest {
 
-  private static final ImmutableMap<String, String> TRUST_STORE_PROPS = ImmutableMap
-      .<String, String>builder()
-      .putAll(ClientTrustStore.trustStoreProps())
-      .build();
+  private static final String TOPIC_NAME = new OrderDataProvider().topicName();
+
+  private static final String JSON_KSQL_REQUEST = UrlEscapers.urlFormParameterEscaper()
+      .escape("{"
+          + " \"ksql\": \"PRINT " + TOPIC_NAME + " FROM BEGINNING;\""
+          + "}");
 
   private static final EmbeddedSingleNodeKafkaCluster CLUSTER = EmbeddedSingleNodeKafkaCluster
       .newBuilder()
@@ -86,6 +94,13 @@ public class SslFunctionalTest {
 
   private Map<String, String> clientProps;
   private static SslContextFactory sslContextFactory;
+
+  @BeforeClass
+  public static void classSetUp() throws Exception {
+    final OrderDataProvider dataProvider = new OrderDataProvider();
+    CLUSTER.createTopic(TOPIC_NAME);
+    new TopicProducer(CLUSTER).produceInputData(dataProvider);
+  }
 
   @Before
   public void setUp() {
@@ -119,9 +134,13 @@ public class SslFunctionalTest {
   @Test
   public void shouldNotBeAbleToUseWssIfClientDoesNotTrustServerCert() throws Exception {
     // Then:
-    expectedException.expect(SSLHandshakeException.class);
-    expectedException.expectCause(hasCause(hasMessage(containsString(
-        "unable to find valid certification path to requested target"))));
+    expectedException.expect(either(
+        both(hasCause(hasCause(hasMessage(
+            containsString("unable to find valid certification path to requested target")))))
+            .and(instanceOf(SSLHandshakeException.class))
+    ).or(
+        instanceOf(EOFException.class)  // Occasionally, get EOF exception.
+    ));
 
     // When:
     makeWsRequest();
@@ -133,15 +152,14 @@ public class SslFunctionalTest {
     givenTrustStoreConfigured();
 
     // When:
-    final Code result = makeWsRequest();
+    makeWsRequest();
 
-    // Then:
-    assertThat(result, is(Code.OK));
+    // Then: did not throw.
   }
 
   private void givenTrustStoreConfigured() {
     // HTTP:
-    clientProps = TRUST_STORE_PROPS;
+    clientProps = ClientTrustStore.trustStoreProps();
 
     // WS:
     sslContextFactory.setTrustStorePath(ClientTrustStore.trustStorePath());
@@ -163,7 +181,7 @@ public class SslFunctionalTest {
     }
   }
 
-  private static Code makeWsRequest() throws Exception {
+  private static void makeWsRequest() throws Exception {
     final HttpClient httpClient = new HttpClient(sslContextFactory);
     httpClient.start();
     final WebSocketClient wsClient = new WebSocketClient(httpClient);
@@ -172,7 +190,7 @@ public class SslFunctionalTest {
     try {
       final ClientUpgradeRequest request = new ClientUpgradeRequest();
       final WebSocketListener listener = new WebSocketListener();
-      final URI wsUri = REST_APP.getWssListener().resolve("/ws/query");
+      final URI wsUri = REST_APP.getWssListener().resolve("/ws/query?request=" + JSON_KSQL_REQUEST);
 
       wsClient.connect(listener, wsUri, request);
 
@@ -180,46 +198,51 @@ public class SslFunctionalTest {
           listener.latch.await(30, TimeUnit.SECONDS), is(true));
 
       final Throwable error = listener.error.get();
-      return error == null ? Code.OK : extractStatusCode(error);
+      if (error != null) {
+        throw (Exception) error;
+      }
     } finally {
       wsClient.stop();
       httpClient.destroy();
     }
   }
 
-  private static Code extractStatusCode(final Throwable t) throws Exception {
-    if (t instanceof UpgradeException) {
-      final Code code = HttpStatus.getCode(((UpgradeException) t).getResponseStatusCode());
-      if (code == null) {
-        throw new RuntimeException(t.getCause());
-      }
-      return code;
-    }
-
-    if (t instanceof Exception) {
-      throw (Exception) t;
-    }
-
-    throw new RuntimeException(t);
-  }
-
-  @SuppressWarnings("unused")
   @WebSocket
   public static class WebSocketListener {
 
+    private Session session;
     final CountDownLatch latch = new CountDownLatch(1);
     final AtomicReference<Throwable> error = new AtomicReference<>();
 
+    @SuppressWarnings("unused") // Invoked via reflection
     @OnWebSocketConnect
     public void onConnect(final Session session) {
-      session.close();
-      latch.countDown();
+      this.session = session;
     }
 
+    @SuppressWarnings("unused") // Invoked via reflection
     @OnWebSocketError
     public void onError(final Throwable t) {
       error.compareAndSet(null, t);
+      closeSilently();
       latch.countDown();
+    }
+
+    @SuppressWarnings("unused") // Invoked via reflection
+    @OnWebSocketMessage
+    public void onMessage(String msg) {
+      closeSilently();
+      latch.countDown();
+    }
+
+    private void closeSilently() {
+      try {
+        if (session != null) {
+          session.close();
+        }
+      } catch (final Exception e) {
+        // meh
+      }
     }
   }
 }

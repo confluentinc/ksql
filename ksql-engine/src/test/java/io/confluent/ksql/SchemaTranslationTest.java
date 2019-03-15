b941b65b6b513b9f0290576105abb51b37936a3d
Andy Coates <big-andy-coates@users.noreply.github.com>
2019-03-15 15:56:36 +0000

    Switch QTT and STT over to using Pojos to deserialize Json.

--- ksql-engine/src/test/java/io/confluent/ksql/SchemaTranslationTest.java
+++ ksql-engine/src/test/java/io/confluent/ksql/SchemaTranslationTest.java
@@ -21,28 +21,28 @@ import static io.confluent.ksql.EndToEndEngineTestUtil.TestCase;
 import static io.confluent.ksql.EndToEndEngineTestUtil.Topic;
 import static io.confluent.ksql.EndToEndEngineTestUtil.ValueSpecAvroSerdeSupplier;
 import static io.confluent.ksql.EndToEndEngineTestUtil.avroToValueSpec;
+import static io.confluent.ksql.EndToEndEngineTestUtil.buildAvroSchema;
+import static io.confluent.ksql.EndToEndEngineTestUtil.buildTestName;
 import static io.confluent.ksql.EndToEndEngineTestUtil.findTestCases;
+import static java.util.Objects.requireNonNull;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
-import com.google.common.io.Files;
 import io.confluent.avro.random.generator.Generator;
-import io.confluent.ksql.EndToEndEngineTestUtil.JsonTestCase;
-import java.io.IOException;
+import io.confluent.ksql.EndToEndEngineTestUtil.MissingFieldException;
+import io.confluent.ksql.EndToEndEngineTestUtil.TestFile;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.apache.avro.Schema;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -51,12 +51,16 @@ import org.junit.runners.Parameterized;
 
 @RunWith(Parameterized.class)
 public class SchemaTranslationTest {
-  private static final ObjectMapper objectMapper = new ObjectMapper();
+
   private static final Path SCHEMA_VALIDATION_TEST_DIR = Paths.get("schema-validation-tests");
   private static final String TOPIC_NAME = "TEST_INPUT";
   private static final String OUTPUT_TOPIC_NAME = "TEST_OUTPUT";
+  private static final String DDL_STATEMENT = "CREATE STREAM " + TOPIC_NAME
+      + " WITH (KAFKA_TOPIC='" + TOPIC_NAME + "', VALUE_FORMAT='AVRO');";
 
   private final TestCase testCase;
+  private static final Topic OUTPUT_TOPIC = new Topic(OUTPUT_TOPIC_NAME, Optional.empty(),
+      new ValueSpecAvroSerdeSupplier(), 4, 1);
 
   @SuppressWarnings("unused")
   public SchemaTranslationTest(final String name, final TestCase testCase) {
@@ -72,13 +76,11 @@ public class SchemaTranslationTest {
   public static Collection<Object[]> data() {
     final List<String> testFiles = EndToEndEngineTestUtil.getTestFilesParam();
 
-    return findTestCases(SCHEMA_VALIDATION_TEST_DIR, testFiles)
-        .map(SchemaTranslationTest::loadTest)
-        .map(q -> new Object[]{q.getName(), q})
+    return findTestCases(SCHEMA_VALIDATION_TEST_DIR, testFiles, SttTestFile.class)
+        .map(test -> new Object[]{test.getName(), test})
         .collect(Collectors.toList());
   }
 
-  @SuppressWarnings("unchecked")
   private static List<Record> generateInputRecords(
       final Topic topic, final org.apache.avro.Schema avroSchema) {
     final Generator generator = new Generator(avroSchema, new Random());
@@ -93,7 +95,6 @@ public class SchemaTranslationTest {
     ).collect(Collectors.toList());
   }
 
-  @SuppressWarnings("unchecked")
   private static List<Record> getOutputRecords(
       final Topic topic, final List<Record> inputRecords,
       final org.apache.avro.Schema avroSchema) {
@@ -110,87 +111,87 @@ public class SchemaTranslationTest {
 
   }
 
-  private static Object loadRecordSpec(final JsonNode node) {
-    try {
-      return objectMapper.readValue(
-          objectMapper.writeValueAsString(node), Map.class);
-    } catch (final IOException e) {
-      throw new RuntimeException(e);
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  static class SttTestFile implements TestFile<TestCase> {
+
+    private final List<TestCaseNode> tests;
+
+    SttTestFile(@JsonProperty("tests") final List<TestCaseNode> tests) {
+      this.tests = ImmutableList.copyOf(requireNonNull(tests, "tests collection missing"));
+    }
+
+    @Override
+    public Stream<TestCase> buildTests(final Path testPath) {
+      if (tests.isEmpty()) {
+        throw new IllegalArgumentException(testPath + ": test file did not contain any tests");
+      }
+
+      try {
+        return tests.stream().flatMap(node -> node.buildTests(testPath));
+      } catch (final Exception e) {
+        throw new IllegalArgumentException(testPath + ": " + e.getMessage(), e);
+      }
     }
   }
 
-  @SuppressWarnings("unchecked")
-  private static List<Record> loadRecords(final Topic topic, final JsonNode node) {
-    final List<Record> records = new LinkedList<>();
-    node.forEach(
-        child -> records.add(
-            new Record(
-                topic,
-                "test-key",
-                loadRecordSpec(child),
-                0,
-                null
-            )
-        )
-    );
-    return records;
-  }
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  static class TestCaseNode {
 
-  private static TestCase loadTest(final JsonTestCase testCase) {
-    final JsonNode node = testCase.getNode();
+    private final String name;
+    private final JsonNode schema;
 
-    final String name = Files.getNameWithoutExtension(testCase.getTestPath().toString())
-        + " - "
-        + node.get("name").asText();
-
-    final JsonNode schemaNode = node.get("schema");
-    final String schemaString;
-    try {
-      schemaString = new ObjectMapper().writeValueAsString(schemaNode);
-    } catch (final JsonProcessingException e) {
-      throw new RuntimeException(e);
+    TestCaseNode(
+        @JsonProperty("name") final String name,
+        @JsonProperty("schema") final JsonNode schema
+    ) {
+      this.name = name == null ? "" : name;
+      this.schema = requireNonNull(schema, "schema");
     }
 
-    final org.apache.avro.Schema.Parser parser = new org.apache.avro.Schema.Parser();
-    final org.apache.avro.Schema avroSchema = parser.parse(schemaString);
+    Stream<TestCase> buildTests(final Path testPath) {
+      if (name.isEmpty()) {
+        throw new MissingFieldException("name");
+      }
 
-    final Topic srcTopic;
-    final Topic outputTopic
-        = new Topic(OUTPUT_TOPIC_NAME, Optional.empty(), new ValueSpecAvroSerdeSupplier(), 4);
-    final List<Record> inputRecords;
-    final List<Record> outputRecords;
-    if (node.has("input_records")) {
-      srcTopic = new Topic(TOPIC_NAME, Optional.of(avroSchema), new ValueSpecAvroSerdeSupplier(), 1);
-      inputRecords = loadRecords(srcTopic, node.get("input_records"));
-      outputRecords = loadRecords(outputTopic, node.get("output_records"));
-    } else {
-      srcTopic = new Topic(TOPIC_NAME, Optional.of(avroSchema), new AvroSerdeSupplier(), 1);
-      inputRecords = generateInputRecords(srcTopic, avroSchema);
-      outputRecords = getOutputRecords(outputTopic, inputRecords, avroSchema);
-    }
+      final String testName = buildTestName(testPath, name, "");
 
-    final String ddlStatement =
-        String.format(
-            "CREATE STREAM %s WITH (KAFKA_TOPIC='%s', VALUE_FORMAT='AVRO');",
-            TOPIC_NAME, TOPIC_NAME);
-    final String csasStatement = avroSchema.getFields()
-        .stream()
-        .map(Schema.Field::name)
-        .collect(
-            Collectors.joining(
-                ", ",
-                "CREATE STREAM " + OUTPUT_TOPIC_NAME + " AS SELECT ",
-                " FROM " + TOPIC_NAME + ";")
+      try {
+        final Schema avroSchema = buildAvroSchema(schema)
+            .orElseThrow(() -> new MissingFieldException("schema"));
+
+        final Topic srcTopic = new Topic(
+            TOPIC_NAME,
+            Optional.of(avroSchema),
+            new AvroSerdeSupplier(),
+            1,
+            1
         );
 
-    return new TestCase(
-        testCase.getTestPath(),
-        name,
-        Collections.emptyMap(),
-        ImmutableList.of(srcTopic, outputTopic),
-        inputRecords,
-        outputRecords,
-        Arrays.asList(ddlStatement, csasStatement),
-        EndToEndEngineTestUtil.ExpectedException.none());
+        final List<Record> inputRecords = generateInputRecords(srcTopic, avroSchema);
+        final List<Record> outputRecords = getOutputRecords(OUTPUT_TOPIC, inputRecords, avroSchema);
+
+        final String csasStatement = avroSchema.getFields()
+            .stream()
+            .map(Schema.Field::name)
+            .collect(
+                Collectors.joining(
+                    ", ",
+                    "CREATE STREAM " + OUTPUT_TOPIC_NAME + " AS SELECT ",
+                    " FROM " + TOPIC_NAME + ";")
+            );
+
+        return Stream.of(new TestCase(
+            testPath,
+            name,
+            Collections.emptyMap(),
+            ImmutableList.of(srcTopic, OUTPUT_TOPIC),
+            inputRecords,
+            outputRecords,
+            ImmutableList.of(DDL_STATEMENT, csasStatement),
+            EndToEndEngineTestUtil.ExpectedException.none()));
+      } catch (final Exception e) {
+        throw new AssertionError(testName + ": Invalid test. " + e.getMessage(), e);
+      }
+    }
   }
 }

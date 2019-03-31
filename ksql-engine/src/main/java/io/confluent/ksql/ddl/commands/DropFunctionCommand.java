5d04df51671aa54c43fa374cd2c723b2ef5f24d6
Mitch Seymour <mitchseymour@gmail.com>
2019-03-30 23:31:28 -0400

    Fix drop function implementation

--- ksql-engine/src/main/java/io/confluent/ksql/ddl/commands/DropFunctionCommand.java
+++ ksql-engine/src/main/java/io/confluent/ksql/ddl/commands/DropFunctionCommand.java
@@ -25,6 +25,7 @@ public class DropFunctionCommand implements DdlCommand {
 
   private final String functionName;
   private final Boolean isExists;
+  private final Object lock = new Object();
 
   public DropFunctionCommand(final DropFunction dropFunction) {
     this.functionName = dropFunction.getName();
@@ -40,17 +41,25 @@ public class DropFunctionCommand implements DdlCommand {
   public DdlCommandResult run(final MutableMetaStore metaStore) {
     final MutableFunctionRegistry functionRegistry = metaStore.getFunctionRegistry();
     try {
-      if (isExists && !functionRegistry.isInline(functionName)) {
-        functionRegistry.dropFunction(functionName);
-        return new DdlCommandResult(true, "Function " + functionName + " does not exist");
+      // make this check + drop operation atomic with a synchonized lock
+      synchronized (lock) {
+        if (!functionRegistry.isInline(functionName)) {
+          if (isExists) {
+            // function doesn't exist, but IF EXISTS clause was specified, so no big deal
+            return new DdlCommandResult(true, functionName + " is not an inline function");
+          }
+          // function doesn't exist, but IF EXISTS clause was NOT specified. throw an exception
+          throw new KsqlException(functionName + " is not an inline function");
+        }
       }
+      // function is inline, do drop it
       functionRegistry.dropFunction(functionName);
       return new DdlCommandResult(true, "Function " + functionName + " was dropped");
     } catch (Exception e) {
       final String errorMessage =
-            String.format("Cannot drop function '%s': %s",
-                functionName, e.getMessage());
+            String.format("Cannot drop function: %s", e.getMessage());
       throw new KsqlException(errorMessage, e);
     }
   }
 }
+

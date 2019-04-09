5f3037d42c8fcf4e30f7037a911df11104c62ab2
Almog Gavra <almog@confluent.io>
2019-04-09 10:03:55 -0700

    update UdfClassLoaderTest

--- ksql-engine/src/test/java/io/confluent/ksql/function/UdfClassLoaderTest.java
+++ ksql-engine/src/test/java/io/confluent/ksql/function/UdfClassLoaderTest.java
@@ -18,6 +18,7 @@ package io.confluent.ksql.function;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
 import java.io.File;
@@ -26,41 +27,53 @@ import org.junit.Test;
 
 public class UdfClassLoaderTest {
 
-  /*
-    The jar contains the class org.damian.ksql.udf.ToString. It also contains the
-    contents the classes from slf4j-log4j12-1.7.25.jar
-   */
-  private final Path udfJar = new File("src/test/resources/udf-example.jar").toPath();
-  private final UdfClassLoader udfClassLoader = UdfClassLoader.newClassLoader(udfJar,
-      getClass().getClassLoader(), resourceName -> false);
+  private static final String ONLY_IN_JAR = "org.damian.ksql.udf.ToString";
+  private static final String ONLY_IN_PARENT = "class.that.exists.only.in.parent";
+  private static final String IN_PARENT_AND_JAR = "org.damian.ksql.udf.ToStruct";
 
+  // The jar contains the class org.damian.ksql.udf.ToString and org.damian.ksql.udf.ToStruct.
+  private final Path udfJar = new File("src/test/resources/udf-example.jar").toPath();
+  private final MyClassLoader parentLoader = new MyClassLoader();
+  private final UdfClassLoader udfClassLoader =
+      UdfClassLoader.newClassLoader(udfJar, parentLoader, resourceName -> false);
 
   @Test
   public void shouldLoadClassesInPath() throws ClassNotFoundException {
     final UdfClassLoader udfClassLoader = UdfClassLoader.newClassLoader(udfJar,
         getClass().getClassLoader(), resourceName -> false);
 
-    assertThat(udfClassLoader.loadClass("org.damian.ksql.udf.ToString", true), not(nullValue()));
+    assertThat(udfClassLoader.loadClass(ONLY_IN_JAR, true), not(nullValue()));
   }
 
   @Test(expected = ClassNotFoundException.class)
   public void shouldThrowClassNotFoundIfClassIsBlacklisted() throws ClassNotFoundException {
     final UdfClassLoader udfClassLoader = UdfClassLoader.newClassLoader(udfJar,
         getClass().getClassLoader(), resourceName -> true);
-    udfClassLoader.loadClass("org.damian.ksql.udf.ToString", true);
+    udfClassLoader.loadClass(ONLY_IN_JAR, true);
   }
 
   @Test
   public void shouldLoadClassesFromParentIfNotFoundInChild() throws ClassNotFoundException {
-    assertThat(udfClassLoader.loadClass("io.confluent.ksql.function.UdfClassLoaderTest", true),
-        equalTo(UdfClassLoaderTest.class));
-
+    assertThat(udfClassLoader.loadClass(ONLY_IN_PARENT, true), equalTo(OnlyInParent.class));
   }
 
   @Test
   public void shouldLoadNonConfluentClassesFromChildFirst() throws ClassNotFoundException {
-    assertThat(udfClassLoader.loadClass("org.slf4j.impl.Log4jLoggerAdapter", true),
-        not(org.slf4j.impl.Log4jLoggerAdapter.class));
+    assertThat(parentLoader.findClass(IN_PARENT_AND_JAR), is(equalTo(InParentAndJar.class)));
+    assertThat(udfClassLoader.loadClass(IN_PARENT_AND_JAR, true), not(InParentAndJar.class));
   }
 
+  private class MyClassLoader extends ClassLoader {
+    @Override
+    public Class<?> findClass(String name) throws ClassNotFoundException {
+      switch (name) {
+        case ONLY_IN_PARENT: return OnlyInParent.class;
+        case IN_PARENT_AND_JAR: return InParentAndJar.class;
+        default: throw new ClassNotFoundException(name);
+      }
+    }
+  }
+
+  private static class OnlyInParent {}
+  private static class InParentAndJar {}
 }

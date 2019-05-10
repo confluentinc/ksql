3f144a208f5a5ae7e7d8aee8c4b41a0677d1c548
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2019-05-10 01:44:55 +0100

    Test JSON top-level maps and add documentation about serialization formats (#2796)
    * Test JSON top-level maps and add documentation about serialization formats.

--- ksql-functional-tests/src/main/java/io/confluent/ksql/test/serde/json/ValueSpecJsonSerdeSupplier.java
+++ ksql-functional-tests/src/main/java/io/confluent/ksql/test/serde/json/ValueSpecJsonSerdeSupplier.java
@@ -15,14 +15,25 @@
 
 package io.confluent.ksql.test.serde.json;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableList;
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import io.confluent.ksql.test.serde.SerdeSupplier;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serializer;
 
 public class ValueSpecJsonSerdeSupplier implements SerdeSupplier<Object> {
+
   @Override
   public Serializer<Object> getSerializer(final SchemaRegistryClient schemaRegistryClient) {
     return new ValueSpecJsonSerializer();
@@ -48,7 +59,8 @@ public class ValueSpecJsonSerdeSupplier implements SerdeSupplier<Object> {
         return null;
       }
       try {
-        return new ObjectMapper().writeValueAsBytes(spec);
+        final Object toSerialize = Converter.toJsonNode(spec);
+        return new ObjectMapper().writeValueAsBytes(toSerialize);
       } catch (final Exception e) {
         throw new RuntimeException(e);
       }
@@ -74,6 +86,94 @@ public class ValueSpecJsonSerdeSupplier implements SerdeSupplier<Object> {
       } catch (final Exception e) {
         throw new RuntimeException(e);
       }
+    }
+  }
+
+  private static final class Converter<T> {
+
+    private static final List<Converter<?>> CONVERTORS = ImmutableList.of(
+        Converter.converter(Boolean.class, JsonNodeFactory.instance::booleanNode),
+        Converter.converter(Integer.class, JsonNodeFactory.instance::numberNode),
+        Converter.converter(Long.class, JsonNodeFactory.instance::numberNode),
+        Converter.converter(Float.class, JsonNodeFactory.instance::numberNode),
+        Converter.converter(Double.class, JsonNodeFactory.instance::numberNode),
+        Converter.converter(String.class, JsonNodeFactory.instance::textNode),
+        Converter.converter(Collection.class, Converter::handleCollection),
+        Converter.converter(Map.class, Converter::handleMap)
+    );
+
+    private final Class<T> type;
+    private final Function<T, JsonNode> mapper;
+
+    static JsonNode toJsonNode(final Object obj) {
+      if (obj == null) {
+        return JsonNodeFactory.instance.nullNode();
+      }
+
+      final List<Converter<?>> candidates = CONVERTORS.stream()
+          .filter(c -> c.handles(obj))
+          .collect(Collectors.toList());
+
+      if (candidates.isEmpty()) {
+        throw new UnsupportedOperationException("Test framework does not current handle " + obj);
+      }
+
+      if (candidates.size() > 1) {
+        throw new RuntimeException("Ambiguous handling of " + obj);
+      }
+
+      return candidates.get(0).convert(obj);
+    }
+
+    private boolean handles(final Object obj) {
+      return type.isAssignableFrom(obj.getClass());
+    }
+
+    private static <T> Converter<T> converter(
+        final Class<T> type,
+        final Function<T, JsonNode> mapper
+    ) {
+      return new Converter<>(type, mapper);
+    }
+
+    private Converter(
+        final Class<T> type,
+        final Function<T, JsonNode> mapper
+    ) {
+      this.type = Objects.requireNonNull(type, "type");
+      this.mapper = Objects.requireNonNull(mapper, "mapper");
+    }
+
+    private JsonNode convert(final Object o) {
+      final T cast = type.cast(o);
+      return mapper.apply(cast);
+    }
+
+    private static JsonNode handleCollection(final Collection<?> collection) {
+      final ArrayNode list = JsonNodeFactory.instance.arrayNode();
+      for (Object element : collection) {
+        list.add(toJsonNode(element));
+      }
+      return list;
+    }
+
+    private static JsonNode handleMap(final Map<?, ?> map) {
+      final ObjectNode node = JsonNodeFactory.instance.objectNode();
+
+      if (map.isEmpty()) {
+        return node;
+      }
+
+      map.forEach((k, v) -> {
+        if (!(k instanceof String)) {
+          throw new UnsupportedOperationException(
+              "Test framework does not yet support maps with non-string keys");
+        }
+
+        node.set((String) k, toJsonNode(v));
+      });
+
+      return node;
     }
   }
 
--- ksql-serde/src/main/java/io/confluent/ksql/serde/util/SerdeUtils.java
+++ ksql-serde/src/main/java/io/confluent/ksql/serde/util/SerdeUtils.java
@@ -16,9 +16,15 @@
 package io.confluent.ksql.serde.util;
 
 import io.confluent.ksql.util.KsqlException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
 
 public final class SerdeUtils {
+
   public static final String DESERIALIZER_LOGGER_NAME = "deserializer";
 
   private SerdeUtils() {
@@ -86,5 +92,90 @@ public final class SerdeUtils {
       }
     }
     throw new IllegalArgumentException("This Object doesn't represent a double");
+  }
+
+  public static boolean isCoercible(final Object object, final Schema targetSchema) {
+    if (object == null) {
+      return true;
+    }
+
+    switch (targetSchema.type()) {
+      case BOOLEAN:
+        return object instanceof Boolean;
+      case INT32:
+      case INT64:
+      case FLOAT64:
+        return isCoercibleToNumber(object);
+      case STRING:
+        return true;
+      case ARRAY:
+        return isCoercibleToArray(object, targetSchema.valueSchema());
+      case MAP:
+        return isCoercibleToMap(object, targetSchema.keySchema(), targetSchema.valueSchema());
+      case STRUCT:
+        return isCoercibleToStruct(object, targetSchema.fields());
+      default:
+        throw new UnsupportedOperationException("Unsupported type: " + targetSchema.type());
+    }
+  }
+
+  private static boolean isCoercibleToNumber(final Object object) {
+    return object instanceof Number || object instanceof String;
+  }
+
+  private static boolean isCoercibleToArray(final Object object, final Schema valueSchema) {
+    if (!(object instanceof Collection)) {
+      return false;
+    }
+
+    for (final Object element : ((Collection<?>) object)) {
+      if (!isCoercible(element, valueSchema)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static boolean isCoercibleToMap(
+      final Object object,
+      final Schema keySchema,
+      final Schema valueSchema) {
+    if (!(object instanceof Map)) {
+      return false;
+    }
+
+    for (final Map.Entry<?, ?> e : ((Map<?, ?>) object).entrySet()) {
+      if (!isCoercible(e.getKey(), keySchema)) {
+        return false;
+      }
+
+      if (!isCoercible(e.getValue(), valueSchema)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static boolean isCoercibleToStruct(final Object object, final List<Field> fields) {
+    if (!(object instanceof Map)) {
+      return false;
+    }
+
+    for (final Map.Entry<?, ?> e : ((Map<?, ?>) object).entrySet()) {
+      final String fieldName = e.getKey().toString();
+      final Field field = fields.stream()
+          .filter(f -> f.name().equalsIgnoreCase(fieldName))
+          .findFirst()
+          .orElse(null);
+
+      if (field == null) {
+        return false;
+      }
+
+      if (!isCoercible(e.getValue(), field.schema())) {
+        return false;
+      }
+    }
+    return true;
   }
 }

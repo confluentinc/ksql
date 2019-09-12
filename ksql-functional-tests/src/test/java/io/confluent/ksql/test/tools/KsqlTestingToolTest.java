413e37047f0160913f78b64c2d5761d31edb3960
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2019-09-12 12:29:28 +0100

    test: output topic and message index when QTT detects mismatch (MINOR) (#3332)
    * test: output topic and message index when QTT detects mismatch
    
    The QTT test used to output both the name of the topic and the message index when it failed due to a mismatch between the expected output and the actual. This was lost in the recent changes. This info really helps to zero in on what is different.

--- ksql-functional-tests/src/test/java/io/confluent/ksql/test/tools/KsqlTestingToolTest.java
+++ ksql-functional-tests/src/test/java/io/confluent/ksql/test/tools/KsqlTestingToolTest.java
@@ -119,7 +119,7 @@ public class KsqlTestingToolTest {
 
     // Then:
     assertThat(errContent.toString(UTF_8),
-        containsString("Test failed: Expected <1001, 101> with timestamp=0 but was <101, 101> with timestamp=0\n"));
+        containsString("Test failed: Topic 'S1', message 0: Expected <1001, 101> with timestamp=0 but was <101, 101> with timestamp=0\n"));
   }
 
   @Test

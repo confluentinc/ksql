3b6050d33c71ee709f1ec54c48a62e67975c7037
Almog Gavra <almog@confluent.io>
2019-09-20 10:20:33 -0700

    refactor: change null handling

--- ksql-cli/src/main/java/io/confluent/ksql/cli/console/Console.java
+++ ksql-cli/src/main/java/io/confluent/ksql/cli/console/Console.java
@@ -648,7 +648,7 @@ public class Console implements Closeable {
     writer().println(String.format("%-20s : %s", "Type", description.getStatus().type()));
     writer().println(String.format("%-20s : %s", "State", status.connector().state()));
     writer().println(String.format("%-20s : %s", "WorkerId", status.connector().workerId()));
-    if (status.connector().trace() != null && !status.connector().trace().isEmpty()) {
+    if (!ObjectUtils.defaultIfNull(status.connector().trace(), "").isEmpty()) {
       writer().println(String.format("%-20s : %s", "Trace", status.connector().trace()));
     }
 

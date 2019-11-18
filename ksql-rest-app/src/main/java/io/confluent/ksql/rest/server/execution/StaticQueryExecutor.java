24523da89bf25f943a6a1610107179a5eb6f6a98
Andy Coates <big-andy-coates@users.noreply.github.com>
2019-11-18 13:41:04 +0000

    fix: wrong source type in pull query error message
    Fixes: #3523
    
    Remove the 'table' in the error message 'Table `foo` is not materialized' as it won't always be a table: it could be a stream.

--- ksql-rest-app/src/main/java/io/confluent/ksql/rest/server/execution/StaticQueryExecutor.java
+++ ksql-rest-app/src/main/java/io/confluent/ksql/rest/server/execution/StaticQueryExecutor.java
@@ -712,7 +712,7 @@ public final class StaticQueryExecutor {
   }
 
   private static KsqlException notMaterializedException(final SourceName sourceTable) {
-    return new KsqlException("Table '"
+    return new KsqlException("'"
         + sourceTable.toString(FormatOptions.noEscape()) + "' is not materialized. "
         + StaticQueryValidator.NEW_QUERY_SYNTAX_SHORT_HELP
         + System.lineSeparator()

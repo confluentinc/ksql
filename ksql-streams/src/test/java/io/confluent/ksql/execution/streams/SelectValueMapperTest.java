0ac1f249edf0c03551d8eb9558c24ae32af3ca6c
Andy Coates <big-andy-coates@users.noreply.github.com>
2019-11-21 13:37:25 +0000

    chore: fix missing copyright

--- ksql-streams/src/test/java/io/confluent/ksql/execution/streams/SelectValueMapperTest.java
+++ ksql-streams/src/test/java/io/confluent/ksql/execution/streams/SelectValueMapperTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package io.confluent.ksql.execution.streams;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -12,13 +27,12 @@ import com.google.common.collect.ImmutableList;
 import io.confluent.ksql.GenericRow;
 import io.confluent.ksql.execution.codegen.ExpressionMetadata;
 import io.confluent.ksql.execution.expression.tree.FunctionCall;
-import io.confluent.ksql.name.ColumnName;
-import io.confluent.ksql.name.FunctionName;
-import io.confluent.ksql.schema.ksql.ColumnRef;
 import io.confluent.ksql.logging.processing.ProcessingLogConfig;
 import io.confluent.ksql.logging.processing.ProcessingLogMessageSchema;
 import io.confluent.ksql.logging.processing.ProcessingLogMessageSchema.MessageType;
 import io.confluent.ksql.logging.processing.ProcessingLogger;
+import io.confluent.ksql.name.ColumnName;
+import io.confluent.ksql.name.FunctionName;
 import java.util.Collections;
 import java.util.function.Function;
 import org.apache.kafka.connect.data.SchemaAndValue;

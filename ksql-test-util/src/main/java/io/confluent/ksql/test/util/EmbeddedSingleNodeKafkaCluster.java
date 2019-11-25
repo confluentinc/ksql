d878ce2b528af1d96f21e2904912bbee82adb0c6
Andy Coates <big-andy-coates@users.noreply.github.com>
2019-11-25 18:01:29 +0000

    test: fix flaky test - caused by async ACL creation (#3972)
    The old way of creating ACLS through the `SimpleAclAuthorizer` was async:
    1. the authorizer writes direct to ZK.
    2. the brokers ZK listener fires.
    3. the broker loads the ACL changes.
    
    This was causing occasional test failures.
    
    This commit switches our tests to use the sync ACL operations available through the Admin client.
    
    It also moves us away from the deprecated `SimpleAclAuthorizer` to the new `AclAuthorizer`.
    
    (cherry picked from 5.4.x)

--- ksql-test-util/src/main/java/io/confluent/ksql/test/util/EmbeddedSingleNodeKafkaCluster.java
+++ ksql-test-util/src/main/java/io/confluent/ksql/test/util/EmbeddedSingleNodeKafkaCluster.java
@@ -16,7 +16,6 @@
 package io.confluent.ksql.test.util;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import io.confluent.ksql.test.util.secure.ClientTrustStore;
 import io.confluent.ksql.test.util.secure.Credentials;
 import io.confluent.ksql.test.util.secure.SecureKafkaHelper;
@@ -26,25 +25,26 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.security.auth.login.Configuration;
-import kafka.security.auth.Acl;
-import kafka.security.auth.Operation$;
-import kafka.security.auth.PermissionType;
-import kafka.security.auth.PermissionType$;
-import kafka.security.auth.ResourceType$;
 import kafka.security.auth.SimpleAclAuthorizer;
 import kafka.server.KafkaConfig;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.acl.AccessControlEntry;
+import org.apache.kafka.common.acl.AclBinding;
+import org.apache.kafka.common.acl.AclBindingFilter;
 import org.apache.kafka.common.acl.AclOperation;
 import org.apache.kafka.common.acl.AclPermissionType;
 import org.apache.kafka.common.config.SslConfigs;
@@ -58,7 +58,6 @@ import org.junit.rules.ExternalResource;
 import org.junit.rules.TemporaryFolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import scala.collection.JavaConversions;
 
 /**
  * Runs an in-memory, "embedded" Kafka cluster with 1 ZooKeeper instance and 1 Kafka broker.
@@ -77,6 +76,8 @@ public final class EmbeddedSingleNodeKafkaCluster extends ExternalResource {
       new Credentials("valid_user_1", "some-password");
   public static final Credentials VALID_USER2 =
       new Credentials("valid_user_2", "some-password");
+  private static final Credentials INTER_BROKER_USER =
+      new Credentials("broker", "brokerPassword");
   private static final List<Credentials> ALL_VALID_USERS =
       ImmutableList.of(VALID_USER1, VALID_USER2);
 
@@ -85,8 +86,7 @@ public final class EmbeddedSingleNodeKafkaCluster extends ExternalResource {
   private final Map<String, Object> brokerConfig = new HashMap<>();
   private final Map<String, Object> clientConfig = new HashMap<>();
   private final TemporaryFolder tmpFolder = new TemporaryFolder();
-  private final SimpleAclAuthorizer authorizer = new SimpleAclAuthorizer();
-  private final Set<kafka.security.auth.Resource> addedAcls = new HashSet<>();
+  private final List<AclBinding> addedAcls = new ArrayList<>();
 
   private ZooKeeperEmbedded zookeeper;
   private KafkaEmbedded broker;
@@ -122,13 +122,6 @@ public final class EmbeddedSingleNodeKafkaCluster extends ExternalResource {
     brokerConfig.put("group.initial.rebalance.delay.ms", 100);
     broker = new KafkaEmbedded(effectiveBrokerConfigFrom());
     clientConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers());
-
-    final ImmutableMap<String, Object> props = ImmutableMap.of(
-        KafkaConfig.ZkConnectProp(), zookeeperConnect(),
-        SimpleAclAuthorizer.ZkConnectionTimeOutProp(), (int) ZK_CONNECT_TIMEOUT.toMillis(),
-        SimpleAclAuthorizer.ZkSessionTimeOutProp(), (int) ZK_SESSION_TIMEOUT.toMillis()
-    );
-    authorizer.configure(props);
   }
 
   @Override
@@ -150,7 +143,7 @@ public final class EmbeddedSingleNodeKafkaCluster extends ExternalResource {
     if (broker != null) {
       broker.stop();
     }
-    authorizer.close();
+
     try {
       if (zookeeper != null) {
         zookeeper.stop();
@@ -240,7 +233,7 @@ public final class EmbeddedSingleNodeKafkaCluster extends ExternalResource {
   }
 
   /**
-   * Writes the supplied ACL information to ZK, where it will be picked up by the brokes authorizer.
+   * Create ACLs via admin client
    *
    * @param username    the who.
    * @param permission  the allow|deny.
@@ -251,35 +244,36 @@ public final class EmbeddedSingleNodeKafkaCluster extends ExternalResource {
       final String username,
       final AclPermissionType permission,
       final ResourcePattern resource,
-      final Set<AclOperation> ops) {
+      final Set<AclOperation> ops
+  ) {
+    try (AdminClient adminClient = adminClient()) {
 
-    final KafkaPrincipal principal = new KafkaPrincipal("User", username);
-    final PermissionType scalaPermission = PermissionType$.MODULE$.fromJava(permission);
+      final KafkaPrincipal principal = new KafkaPrincipal("User", username);
 
-    final Set<Acl> javaAcls = ops.stream()
-        .map(Operation$.MODULE$::fromJava)
-        .map(op -> new Acl(principal, scalaPermission, "*", op))
-        .collect(Collectors.toSet());
+      final Set<AclBinding> acls = ops.stream()
+          .map(op -> new AccessControlEntry(principal.toString(), "*", op, permission))
+          .map(ace -> new AclBinding(resource, ace))
+          .collect(Collectors.toSet());
 
-    final scala.collection.immutable.Set<Acl> scalaAcls =
-        JavaConversions.asScalaSet(javaAcls).toSet();
+      adminClient.createAcls(acls).all().get();
 
-    final kafka.security.auth.ResourceType scalaResType =
-        ResourceType$.MODULE$.fromJava(resource.resourceType());
-
-    final kafka.security.auth.Resource scalaResource =
-        new kafka.security.auth.Resource(scalaResType, resource.name(), resource.patternType());
-
-    authorizer.addAcls(scalaAcls, scalaResource);
-
-    addedAcls.add(scalaResource);
+      addedAcls.addAll(acls);
+    } catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException("Failed to set ACLs", e);
+    }
   }
 
   /**
    * Clear all ACLs from the cluster.
    */
   public void clearAcls() {
-    addedAcls.forEach(authorizer::removeAcls);
+    try (AdminClient adminClient = adminClient()) {
+      final List<AclBindingFilter> filters = addedAcls.stream()
+          .map(AclBinding::toFilter)
+          .collect(Collectors.toList());
+
+      adminClient.deleteAcls(filters);
+    }
   }
 
   public static Builder newBuilder() {
@@ -341,9 +335,9 @@ public final class EmbeddedSingleNodeKafkaCluster extends ExternalResource {
 
   private String createJaasConfigContent() {
     final String prefix = "KafkaServer {\n  " + PlainLoginModule.class.getName() + " required\n"
-                          + "  username=\"broker\"\n"
-                          + "  password=\"brokerPassword\"\n"
-                          + "  user_broker=\"brokerPassword\"\n";
+        + "  username=\"" + INTER_BROKER_USER.username + "\"\n"
+        + "  password=\"" + INTER_BROKER_USER.password + "\"\n"
+        + "  user_broker=\"" + INTER_BROKER_USER.password + "\"\n";
 
     return ALL_VALID_USERS.stream()
         .map(creds -> "  user_" + creds.username + "=\"" + creds.password + "\"")
@@ -363,6 +357,14 @@ public final class EmbeddedSingleNodeKafkaCluster extends ExternalResource {
       System.clearProperty(JaasUtils.JAVA_LOGIN_CONFIG_PARAM);
     }
     Configuration.setConfiguration(null);
+  }
+
+  private AdminClient adminClient() {
+    final Map<String, Object> props = new HashMap<>(getClientProperties());
+    props.put(AdminClientConfig.RETRIES_CONFIG, 5);
+    props.putAll(SecureKafkaHelper.getSecureCredentialsConfig(INTER_BROKER_USER));
+
+    return AdminClient.create(props);
   }
 
   public static final class Builder {

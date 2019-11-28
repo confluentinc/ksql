8e0cdb6caccfdac2fdab1623193cc956d9506269
Andy Coates <big-andy-coates@users.noreply.github.com>
2019-11-28 10:03:57 +0000

    chore: turn on checkstyle rule to detect todo comments
    and fix up / remove any outstanding todos
    
    one new github issue to track outstanding todo:
    
    https://github.com/confluentinc/ksql/issues/3993

--- ksql-engine/src/main/java/io/confluent/ksql/function/udf/string/MaskKudf.java
+++ ksql-engine/src/main/java/io/confluent/ksql/function/udf/string/MaskKudf.java
@@ -17,6 +17,7 @@ package io.confluent.ksql.function.udf.string;
 
 import io.confluent.ksql.function.udf.Udf;
 import io.confluent.ksql.function.udf.UdfDescription;
+import io.confluent.ksql.function.udf.UdfParameter;
 import io.confluent.ksql.util.KsqlConstants;
 
 @UdfDescription(name = "mask", author = KsqlConstants.CONFLUENT_AUTHOR,
@@ -27,24 +28,23 @@ public class MaskKudf {
 
   @Udf(description = "Returns a masked version of the input string. All characters of the input"
       + " will be replaced according to the default masking rules.")
-  public String mask(final String input) {
+  @SuppressWarnings("MethodMayBeStatic") // Invoked via reflection
+  public String mask(
+      @UdfParameter("input STRING to be masked") final String input
+  ) {
     return doMask(new Masker(), input);
   }
 
-  // TODO these descriptions would be much easier and more structured if we had an annotation for
-  // each param (a bit like javadoc) as it's basically the extra params that are getting described
-  // in each variant of the UDF
   @Udf(description = "Returns a masked version of the input string. All characters of the input"
-      + " will be replaced with the specified masking characters: e.g."
-      + " mask(input, upperCaseMask, lowerCaseMask, digitMask, otherMask)."
-      + " Pass NULL for any of the mask characters to prevent masking of that character type.")
-  public String mask(final String input, final String upper, final String lower, final String digit,
-      final String other) {
-    // TODO once KSQL gains Char sql-datatype support we should change the xxMask params to int
-    // (codepoint) instead of String
-
-    // TODO really need a way for UDFs to do one-shot init() stuff instead of repeating all this
-    // literal-param manipulation and validation for every single record
+      + " will be replaced with the specified masking characters")
+  @SuppressWarnings("MethodMayBeStatic") // Invoked via reflection
+  public String mask(
+      @UdfParameter("input STRING to be masked") final String input,
+      @UdfParameter("upper-case mask, or NULL to use default") final String upper,
+      @UdfParameter("lower-case mask, or NULL to use default") final String lower,
+      @UdfParameter("digit mask, or NULL to use default") final String digit,
+      @UdfParameter("mask for other characters, or NULL to use default") final String other
+  ) {
     final int upperMask = Masker.getMaskCharacter(upper);
     final int lowerMask = Masker.getMaskCharacter(lower);
     final int digitMask = Masker.getMaskCharacter(digit);
@@ -53,13 +53,10 @@ public class MaskKudf {
     return doMask(masker, input);
   }
 
-  private String doMask(final Masker masker, final String input) {
+  private static String doMask(final Masker masker, final String input) {
     if (input == null) {
       return null;
     }
-    final StringBuilder output = new StringBuilder(input.length());
-    output.append(masker.mask(input));
-    return output.toString();
+    return masker.mask(input);
   }
-
 }

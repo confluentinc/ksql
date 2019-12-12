da962599c17c47108dce702071ee0b13270e9fc6
Bill Bejeck <bill@confluent.io>
2019-12-12 10:07:35 -0500

    chore: Update KafkaStreams.close to take a duration parameter (#4110)
    Updated QueryMetadata#close method to use KafkaStreams#close(Duration.ofXXX) vs. the no-arg version. When calling KafkaStreams#close() the default timeout is set to Long.MAX_VALUE so the close call will block forever.
    
    Reviewers: Guozhang Wang <wangguoz@gmail.com>, Andy Coates <andy@confluent.io>,
     Almog Gavra <almog@confluent.io>

--- ksql-engine/src/main/java/io/confluent/ksql/util/QueryMetadata.java
+++ ksql-engine/src/main/java/io/confluent/ksql/util/QueryMetadata.java
@@ -20,6 +20,7 @@ import io.confluent.ksql.internal.QueryStateListener;
 import io.confluent.ksql.name.SourceName;
 import io.confluent.ksql.schema.ksql.LogicalSchema;
 import java.lang.Thread.UncaughtExceptionHandler;
+import java.time.Duration;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
@@ -44,6 +45,7 @@ public class QueryMetadata {
   private final Consumer<QueryMetadata> closeCallback;
   private final Set<SourceName> sourceNames;
   private final LogicalSchema logicalSchema;
+  private final Long closeTimeout;
 
   private Optional<QueryStateListener> queryStateListener = Optional.empty();
   private boolean everStarted = false;
@@ -59,8 +61,8 @@ public class QueryMetadata {
       final Topology topology,
       final Map<String, Object> streamsProperties,
       final Map<String, Object> overriddenProperties,
-      final Consumer<QueryMetadata> closeCallback
-  ) {
+      final Consumer<QueryMetadata> closeCallback,
+      final Long closeTimeout) {
     // CHECKSTYLE_RULES.ON: ParameterNumberCheck
     this.statementString = Objects.requireNonNull(statementString, "statementString");
     this.kafkaStreams = Objects.requireNonNull(kafkaStreams, "kafkaStreams");
@@ -76,6 +78,7 @@ public class QueryMetadata {
     this.closeCallback = Objects.requireNonNull(closeCallback, "closeCallback");
     this.sourceNames = Objects.requireNonNull(sourceNames, "sourceNames");
     this.logicalSchema = Objects.requireNonNull(logicalSchema, "logicalSchema");
+    this.closeTimeout = Objects.requireNonNull(closeTimeout, "closeTimeout");
   }
 
   protected QueryMetadata(final QueryMetadata other, final Consumer<QueryMetadata> closeCallback) {
@@ -89,6 +92,7 @@ public class QueryMetadata {
     this.sourceNames = other.sourceNames;
     this.logicalSchema = other.logicalSchema;
     this.closeCallback = Objects.requireNonNull(closeCallback, "closeCallback");
+    this.closeTimeout = other.closeTimeout;
   }
 
   public void registerQueryStateListener(final QueryStateListener queryStateListener) {
@@ -141,7 +145,7 @@ public class QueryMetadata {
   }
 
   public void close() {
-    kafkaStreams.close();
+    kafkaStreams.close(Duration.ofMillis(closeTimeout));
 
     kafkaStreams.cleanUp();
 

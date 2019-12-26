78b46ec5152b78d80bce1bc7b95aa415feacb230
Alberto Santini <albertosantini@pm.me>
2019-12-26 09:09:59 +0100

    chore: remove double semi-colon

--- ksql-functional-tests/src/test/java/io/confluent/ksql/test/planned/TestCasePlanLoader.java
+++ ksql-functional-tests/src/test/java/io/confluent/ksql/test/planned/TestCasePlanLoader.java
@@ -42,7 +42,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
 import javax.xml.parsers.DocumentBuilder;

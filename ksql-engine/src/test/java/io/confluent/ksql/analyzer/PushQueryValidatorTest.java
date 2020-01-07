1f110d9f425fb08bef0b52d93daa05f10fd8295e
rodesai <desai.p.rohan@gmail.com>
2020-01-07 13:19:04 -0800

    minor: don't allow persistent push query of windowed table
    Disallows persistent push queries of windowed tables. We can revert
    when streams adds support for loading these with a windowed state
    store. Note that we still allow transient queries on windowed tables.

--- ksql-engine/src/test/java/io/confluent/ksql/analyzer/PushQueryValidatorTest.java
+++ ksql-engine/src/test/java/io/confluent/ksql/analyzer/PushQueryValidatorTest.java
@@ -15,9 +15,19 @@
 
 package io.confluent.ksql.analyzer;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.google.common.collect.ImmutableList;
+import io.confluent.ksql.analyzer.Analysis.AliasedDataSource;
+import io.confluent.ksql.analyzer.Analysis.Into;
+import io.confluent.ksql.execution.ddl.commands.KsqlTopic;
+import io.confluent.ksql.metastore.model.DataSource;
+import io.confluent.ksql.metastore.model.DataSource.DataSourceType;
 import io.confluent.ksql.parser.tree.ResultMaterialization;
+import io.confluent.ksql.serde.KeyFormat;
+import io.confluent.ksql.util.KsqlException;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -34,6 +44,14 @@ public class PushQueryValidatorTest {
 
   @Mock
   private Analysis analysis;
+  @Mock
+  private AliasedDataSource aliasedSource;
+  @Mock
+  private DataSource source;
+  @Mock
+  private KsqlTopic topic;
+  @Mock
+  private KeyFormat keyFormat;
 
   private QueryValidator validator;
 
@@ -53,5 +71,96 @@ public class PushQueryValidatorTest {
 
     // When:
     validator.validate(analysis);
+  }
+
+  @Test
+  public void shouldThrowOnPersistentPushQueryOnWindowedTable() {
+    // Given:
+    givenPushQuery();
+    givenPersistentQuery();
+    givenSourceTable();
+    givenWindowedSource();
+
+    // Then:
+    expectedException.expect(KsqlException.class);
+    expectedException.expectMessage(
+        "KSQL does not support persistent push queries on windowed tables.");
+
+    // When:
+    validator.validate(analysis);
+  }
+
+  @Test
+  public void shouldNotThrowOnTransientPushQueryOnWindowedTable() {
+    // Given:
+    givenPushQuery();
+    givenTransientQuery();
+    givenSourceTable();
+    givenWindowedSource();
+
+    // When/Then:
+    validator.validate(analysis);
+  }
+
+  @Test
+  public void shouldNotThrowOnPersistentPushQueryOnWindowedStream() {
+    // Given:
+    givenPushQuery();
+    givenPersistentQuery();
+    givenSourceStream();
+    givenWindowedSource();
+
+    // When/Then:
+    validator.validate(analysis);
+  }
+
+  @Test
+  public void shouldNotThrowOnPersistentPushQueryOnUnwindowedTable() {
+    // Given:
+    givenPushQuery();
+    givenPersistentQuery();
+    givenSourceTable();
+    givenUnwindowedSource();
+
+    // When/Then(don't throw):
+    validator.validate(analysis);
+  }
+
+  private void givenPushQuery() {
+    when(analysis.getResultMaterialization()).thenReturn(ResultMaterialization.CHANGES);
+  }
+
+  private void givenPersistentQuery() {
+    when(analysis.getInto()).thenReturn(Optional.of(mock(Into.class)));
+  }
+
+  private void givenTransientQuery() {
+    when(analysis.getInto()).thenReturn(Optional.empty());
+  }
+
+  @SuppressWarnings("unchecked")
+  private void givenSourceTable() {
+    when(analysis.getFromDataSources()).thenReturn(ImmutableList.of(aliasedSource));
+    when(aliasedSource.getDataSource()).thenReturn(source);
+    when(source.getDataSourceType()).thenReturn(DataSourceType.KTABLE);
+  }
+
+  @SuppressWarnings("unchecked")
+  private void givenSourceStream() {
+    when(analysis.getFromDataSources()).thenReturn(ImmutableList.of(aliasedSource));
+    when(aliasedSource.getDataSource()).thenReturn(source);
+    when(source.getDataSourceType()).thenReturn(DataSourceType.KSTREAM);
+  }
+
+  private void givenWindowedSource() {
+    when(source.getKsqlTopic()).thenReturn(topic);
+    when(topic.getKeyFormat()).thenReturn(keyFormat);
+    when(keyFormat.isWindowed()).thenReturn(true);
+  }
+
+  private void givenUnwindowedSource() {
+    when(source.getKsqlTopic()).thenReturn(topic);
+    when(topic.getKeyFormat()).thenReturn(keyFormat);
+    when(keyFormat.isWindowed()).thenReturn(false);
   }
 }

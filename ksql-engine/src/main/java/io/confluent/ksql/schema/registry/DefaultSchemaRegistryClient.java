e045f7cbd69f026b6d05f4e473824a155739eca7
Steven Zhang <35498506+stevenpyzhang@users.noreply.github.com>
2020-01-16 14:54:01 -0800

    feat: add a new default SchemaRegistryClient and remove default for SR url (#4325)

---
+++ ksql-engine/src/main/java/io/confluent/ksql/schema/registry/DefaultSchemaRegistryClient.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.confluent.ksql.schema.registry;
+
+import io.confluent.kafka.schemaregistry.ParsedSchema;
+import io.confluent.kafka.schemaregistry.client.SchemaMetadata;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaReference;
+
+import io.confluent.ksql.rest.ErrorMessages;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.apache.kafka.common.config.ConfigException;
+
+/** 
+ * Implements the SchemaRegistryClient interface. Used as default when the Schema Registry URL isn't
+ * specified in {@link io.confluent.ksql.util.KsqlConfig}
+ */
+public class DefaultSchemaRegistryClient implements SchemaRegistryClient {
+
+  private final ConfigException configException;
+
+  public DefaultSchemaRegistryClient(final ErrorMessages errorMessages) {
+    configException = new ConfigException(errorMessages.schemaRegistryUnconfiguredErrorMessage());
+  }
+
+  @Override
+  public Optional<ParsedSchema> parseSchema(
+      final String var1, final String var2,
+      final List<SchemaReference> var3
+  ) {
+    throw configException;
+  }
+
+  @Override
+  public int register(final String s, final ParsedSchema parsedSchema) {
+    throw configException;
+  }
+
+  @Override
+  public int register(final String s, final ParsedSchema parsedSchema, final int i, final int i1) {
+    throw configException;
+  }
+
+  @Override
+  public ParsedSchema getSchemaById(final int i) {
+    throw configException;
+  }
+
+  @Override
+  public ParsedSchema getSchemaBySubjectAndId(final String s, final int i) {
+    throw configException;
+  }
+
+  @Override
+  public Collection<String> getAllSubjectsById(final int i) {
+    throw configException;
+  }
+
+  @Override
+  public SchemaMetadata getLatestSchemaMetadata(final String s) {
+    throw configException;
+  }
+
+  @Override
+  public SchemaMetadata getSchemaMetadata(final String s, final int i) {
+    throw configException;
+  }
+
+  @Override
+  public int getVersion(final String s, final ParsedSchema parsedSchema) {
+    throw configException;
+  }
+
+  @Override
+  public List<Integer> getAllVersions(final String s) {
+    throw configException;
+  }
+
+  @Override
+  public boolean testCompatibility(final String s, final ParsedSchema parsedSchema) {
+    throw configException;
+  }
+
+  @Override
+  public String updateCompatibility(final String s, final String s1) {
+    throw configException;
+  }
+
+  @Override
+  public String getCompatibility(final String s) {
+    throw configException;
+  }
+
+  @Override
+  public String setMode(final String s) {
+    throw configException;
+  }
+
+  @Override
+  public String setMode(final String s, final String s1) {
+    throw configException;
+  }
+
+  @Override
+  public String getMode() {
+    throw configException;
+  }
+
+  @Override
+  public String getMode(final String s) {
+    throw configException;
+  }
+
+  @Override
+  public Collection<String> getAllSubjects() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public int getId(final String s, final ParsedSchema parsedSchema) {
+    throw configException;
+  }
+
+  @Override
+  public List<Integer> deleteSubject(final String s) {
+    throw configException;
+  }
+
+  @Override
+  public List<Integer> deleteSubject(final Map<String, String> map, final String s) {
+    throw configException;
+  }
+
+  @Override
+  public Integer deleteSchemaVersion(final String s, final String s1) {
+    throw configException;
+  }
+
+  @Override
+  public Integer deleteSchemaVersion(
+      final Map<String, String> map, 
+      final String s, 
+      final String s1
+  ) {
+    throw configException;
+  }
+
+  @Override
+  public void reset() {
+    throw configException;
+  }
+}

3946f7317705cdb01201b1317fd90d6d6e7875c8
Rohan <desai.p.rohan@gmail.com>
2020-01-16 09:51:13 -0800

    chore: clean up LogicalSchemaWithMetaAndKeyFields (#4188)

--- ksql-engine/src/main/java/io/confluent/ksql/planner/plan/DataSourceNode.java
+++ ksql-engine/src/main/java/io/confluent/ksql/planner/plan/DataSourceNode.java
@@ -22,9 +22,7 @@ import com.google.errorprone.annotations.Immutable;
 import io.confluent.ksql.execution.builder.KsqlQueryBuilder;
 import io.confluent.ksql.execution.context.QueryContext;
 import io.confluent.ksql.execution.context.QueryContext.Stacker;
-import io.confluent.ksql.execution.plan.LogicalSchemaWithMetaAndKeyFields;
 import io.confluent.ksql.execution.plan.SelectExpression;
-import io.confluent.ksql.execution.plan.StreamSource;
 import io.confluent.ksql.metastore.model.DataSource;
 import io.confluent.ksql.metastore.model.DataSource.DataSourceType;
 import io.confluent.ksql.metastore.model.KeyField;
@@ -42,7 +40,7 @@ public class DataSourceNode extends PlanNode {
 
   private final DataSource<?> dataSource;
   private final SourceName alias;
-  private final LogicalSchemaWithMetaAndKeyFields schema;
+  private final LogicalSchema schema;
   private final KeyField keyField;
   private final SchemaKStreamFactory schemaKStreamFactory;
   private final ImmutableList<SelectExpression> selectExpressions;
@@ -72,18 +70,18 @@ public class DataSourceNode extends PlanNode {
     // DataSourceNode copies implicit and key fields into the value schema
     // It users a KS valueMapper to add the key fields
     // and a KS transformValues to add the implicit fields
-    this.schema = StreamSource.getSchemaWithMetaAndKeyFields(alias, dataSource.getSchema());
+    this.schema = dataSource.getSchema().withAlias(alias).withMetaAndKeyColsInValue();
 
     this.keyField = dataSource.getKeyField()
         .withAlias(alias)
-        .validateKeyExistsIn(schema.getSchema());
+        .validateKeyExistsIn(schema);
 
     this.schemaKStreamFactory = requireNonNull(schemaKStreamFactory, "schemaKStreamFactory");
   }
 
   @Override
   public LogicalSchema getSchema() {
-    return schema.getSchema();
+    return schema;
   }
 
   @Override

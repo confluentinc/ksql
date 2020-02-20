0808dc84341bdeb2b4e41f4157e910d4f1103777
Andy Coates <big-andy-coates@users.noreply.github.com>
2020-02-20 12:56:54 +0000

    chore: api config validation tweak
    move the validation of the client auth config into the config class.

--- ksql-api/src/test/java/io/confluent/ksql/api/ApiServerConfigTest.java
+++ ksql-api/src/test/java/io/confluent/ksql/api/ApiServerConfigTest.java
@@ -18,9 +18,12 @@ package io.confluent.ksql.api;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import com.google.common.collect.ImmutableMap;
 import io.confluent.ksql.api.server.ApiServerConfig;
+import io.vertx.core.http.ClientAuth;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.kafka.common.config.ConfigException;
 import org.junit.Test;
 
 public class ApiServerConfigTest {
@@ -75,5 +78,23 @@ public class ApiServerConfigTest {
     assertThat(config.getString(ApiServerConfig.TLS_TRUST_STORE_PATH), is(""));
     assertThat(config.getString(ApiServerConfig.TLS_TRUST_STORE_PASSWORD), is(""));
     assertThat(config.getString(ApiServerConfig.TLS_CLIENT_AUTH_REQUIRED), is("none"));
+  }
+
+  @Test(expected = ConfigException.class)
+  public void shouldThrowOnUnknownClientAuth() {
+    new ApiServerConfig(ImmutableMap.of(
+        ApiServerConfig.TLS_CLIENT_AUTH_REQUIRED, "blah"
+    ));
+  }
+
+  @Test
+  public void shouldGetValidClientAuth() {
+    // Given:
+    final ApiServerConfig config = new ApiServerConfig(ImmutableMap.of(
+        ApiServerConfig.TLS_CLIENT_AUTH_REQUIRED, "ReQuIrEd"
+    ));
+
+    // Then:
+    assertThat(config.getClientAuth(), is(ClientAuth.REQUIRED));
   }
 }

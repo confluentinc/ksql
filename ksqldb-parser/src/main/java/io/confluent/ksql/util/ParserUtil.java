5eb3565700102cab5b906f254087b0812c08953d
Haji <>
2020-04-01 23:43:27 +0300

    added dot dymbol to valid source names pattern

--- ksqldb-parser/src/main/java/io/confluent/ksql/util/ParserUtil.java
+++ ksqldb-parser/src/main/java/io/confluent/ksql/util/ParserUtil.java
@@ -48,7 +48,7 @@ public final class ParserUtil {
    *
    * @see org.apache.kafka.streams.state.StoreBuilder#name
    */
-  private static final Pattern VALID_SOURCE_NAMES = Pattern.compile("[a-zA-Z0-9_-]*");
+  private static final Pattern VALID_SOURCE_NAMES = Pattern.compile("[a-zA-Z0-9_\.-]*");
 
   private ParserUtil() {
   }

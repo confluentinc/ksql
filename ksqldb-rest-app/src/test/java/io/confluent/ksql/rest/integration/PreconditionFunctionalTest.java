dbd4a83ca606911db76777b0a77b2e7ddeeded7d
Victoria Xia <victoria.xia@confluent.io>
2020-04-15 09:37:32 -0700

    chore: style

--- ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/integration/PreconditionFunctionalTest.java
+++ ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/integration/PreconditionFunctionalTest.java
@@ -36,7 +36,6 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.rules.RuleChain;
 
-@SuppressWarnings("unchecked")
 @Category({IntegrationTest.class})
 public class PreconditionFunctionalTest {
 

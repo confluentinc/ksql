63e66caa98f3ec3db8d7dd456acbcb03476ae0fc
Vicky Papavasileiou <vpapavasileiou@confluent.io>
2020-04-14 17:33:20 -0700

    addressed apurva's comments

--- bin/commandTopicConsumer.py
+++ bin/commandTopicConsumer.py
@@ -2,25 +2,30 @@
 #
 # Copyright 2020 Confluent Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Confluent Community License (the "License"); you may not use
+# this file except in compliance with the License.  You may obtain a copy of the
+# License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+# http://www.confluent.io/confluent-community-license
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations under the License.
 #
+#
+# Usage: commandTopicConsumer.py [-h] [-f CONFFILE] [-b BROKERS][-k KSQLSERVICEID]
+# Command topic consumer that dumps CREATE, DROP and TERMINATE queries to
+# stdout. If no arguments are provided, default values are used. Default broker
+# is 'localhost:9092'. Default ksqlServiceId is 'default_'. You may optionally
+# provide a configuration file with broker specific configuration parameters.
+# Every run of this script will consume the topic from the beginning.
+# optional arguments:
+#   -h, --help        show this help message and exit
+#   -b BROKERS        Bootstrap servers
+#   -f CONFFILE       Configuration file (configProp=value format)
+#   -k KSQLSERVICEID  KsqlDB service ID
 
-#
-# Dump ksqlDB persistent queries to stdout.
-#
-# Usage:
-#  commandTopicConsumer.py -f <client-conf-file>
-#
 
 from confluent_kafka import KafkaError, KafkaException, version
 from confluent_kafka import Producer, Consumer
@@ -51,6 +56,7 @@ def consumer_latest_offsets(consumer, partitions):
         high_water = consumer.get_watermark_offsets(p)[1]
         if high_water >= max_offset:
             max_offset = high_water
+    print("Max offset in command topic = ", max_offset)
 
 class CommandConsumer(object):
     def __init__(self, ksqlServiceId, conf):
@@ -60,7 +66,6 @@ class CommandConsumer(object):
     def consumer_run(self):
         global max_offset
         self.consumer.subscribe([self.topic], on_assign=consumer_latest_offsets)
-        #print("max offset = ", max_offset)
         self.msg_cnt = 0
         self.msg_err_cnt = 0
         stmts = {}
@@ -79,6 +84,9 @@ class CommandConsumer(object):
                     #print("Read msg with offset ", msg.offset())
                     self.msg_cnt += 1
                     record = CommandRecord.deserialize(msg.value())
+                    #print(record)
+
+                    # match statements CREATE/DROP STREAM, CREATE/DROP TABLE
                     match = re.search(r'(?:create|drop) (?:stream|table) ([a-zA-z0-9-]+?)(:?\(|AS|\s|;)', record.stmt, re.I)
                     if match:
                         name = match.group(1).upper()
@@ -87,15 +95,32 @@ class CommandConsumer(object):
                         if name not in stmts:
                             stmts[name] = []
                         stmts[name].append(record.stmt)
-                    else:
-                        match2 = re.search(r'(?:terminate) (?:ctas|csas)_(.+?)_', record.stmt, re.I)
-                        if match2:
-                            name = match2.group(1).upper()
-                            stmts[name].append(record.stmt)
-                        else:
-                            print("No match %s",record)
-                    # High watermark is +2 from last offset
-                    if msg.offset() >= max_offset-2:
+
+                    # match statements TERMINATE query
+                    match2 = re.search(r'(?:terminate) (?:ctas|csas)_(.+?)_', record.stmt, re.I)
+                    if match2:
+                        name = match2.group(1).upper()
+                        stmts[name].append(record.stmt)
+
+                    # match statements INSERT INTO stream or table
+                    match3 = re.search(r'(?:insert into) ([a-zA-z0-9-]+?)(:?\(|\s|\()', record.stmt, re.I)
+                    if match3:
+                        name = match3.group(1).upper()
+                        stmts[name].append(record.stmt)
+
+                    #match statements CREATE TYPE
+                    match4 = re.search(r'(?:create|drop) type ([a-zA-z0-9-]+?)(:?AS|\s|;)', record.stmt, re.I)
+                    if match4:
+                        name = match4.group(1).upper()
+                        if name not in stmts:
+                            stmts[name] = []
+                        stmts[name].append(record.stmt)
+
+                    if match is None and match2 is None and match3 is None and match4 is None:
+                        print("No match for record %s",record)
+
+                    # High watermark is +1 from last offset
+                    if msg.offset() >= max_offset-1:
                         break
 
                 except ValueError as ex:
@@ -117,10 +142,14 @@ class CommandConsumer(object):
                 print("-------------------------------------------------\n")
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Command Topic consumer client')
-    parser.add_argument('-b', dest='brokers', type=str, default=None, help='Bootstrap servers')
+    parser = argparse.ArgumentParser(description='Command topic consumer that dumps CREATE, DROP and TERMINATE queries to '+
+        'stdout. If no arguments are provided, default values are used. Default broker is '
+        '\'localhost:9092\'. Default ksqlServiceId is \'default_\'. You may optionally provide a configuration file with '+
+        'broker specific configuration parameters. Every run of this script will consume the topic from the beginning. ')
+
     parser.add_argument('-f', dest='confFile', type=argparse.FileType('r'),
                         help='Configuration file (configProp=value format)')
+    parser.add_argument('-b', dest='brokers', type=str, default=None, help='Bootstrap servers')
     parser.add_argument('-k', dest='ksqlServiceId', type=str, default=None, help='KsqlDB service ID')
 
     args = parser.parse_args()

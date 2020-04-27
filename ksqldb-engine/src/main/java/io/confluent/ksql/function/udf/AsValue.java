a3c4f30afded7be385f198d663b33c0c7fb10189
Andy Coates <big-andy-coates@users.noreply.github.com>
2020-04-27 12:17:01 +0100

    chore: add AS_VALUE Udf
    Part of [klip-24](https://github.com/confluentinc/ksql/pull/5115).
    
    A Udf that indicates that a key column in a projection should be copied into a value column, for example:
    
    ```sql
    -- Given:
    CREATE STREAM INPUT (ID INT KEY, V0 INT, V1 INT) WITH (kafka_topic='input', value_format='JSON');
    
    -- When:
    CREATE STREAM OUTPUT AS SELECT ID, AS_VALUE(ID) AS ID_COPY, V1 FROM INPUT;
    
    -- Then:
    -- resulting schema: ID INT KEY, ID_COPY INT, V1 INT
    ```
    
    Note, the UDF doesn't actually _do_ anything as yet. It requires the request of klip-24 to enable its true purpose.

---
+++ ksqldb-engine/src/main/java/io/confluent/ksql/function/udf/AsValue.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.confluent.ksql.function.udf;
+
+@SuppressWarnings("MethodMayBeStatic")
+@UdfDescription(name = "AS_VALUE", description = AsValue.DESCRIPTION)
+public class AsValue {
+
+  static final String DESCRIPTION = "Used exclusively in the projection of a query to allow a key"
+      + "column to be copied into the value schema. Has no affect if used anywhere else.";
+
+  @Udf
+  public Boolean asValue(@UdfParameter final Boolean val) {
+    return val;
+  }
+
+  @Udf
+  public Integer asValue(@UdfParameter final Integer val) {
+    return val;
+  }
+
+  @Udf
+  public Long asValue(@UdfParameter final Long val) {
+    return val;
+  }
+
+  @Udf
+  public Double asValue(@UdfParameter final Double val) {
+    return val;
+  }
+
+  @Udf
+  public String asValue(@UdfParameter final String val) {
+    return val;
+  }
+}
+

319ea6797d22236c1ee99fbf7a6b4c2e6a834199
Almog Gavra <almog@confluent.io>
2020-04-28 15:16:18 -0700

    chore: fix checkstyle

--- ksqldb-execution/src/main/java/io/confluent/ksql/execution/ddl/commands/RegisterTypeCommand.java
+++ ksqldb-execution/src/main/java/io/confluent/ksql/execution/ddl/commands/RegisterTypeCommand.java
@@ -47,8 +47,14 @@ public class RegisterTypeCommand implements DdlCommand {
 
   @Override
   public boolean equals(final Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (this == o) {
+      return true;
+    }
+
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
     final RegisterTypeCommand that = (RegisterTypeCommand) o;
     return Objects.equals(type, that.type)
         && Objects.equals(typeName, that.typeName);

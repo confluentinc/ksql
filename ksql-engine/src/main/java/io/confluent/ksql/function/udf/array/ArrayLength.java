b5f9a02f15428e9fe4a23a7b8df7226da5f422c1
Jim Galasyn <jim.galasyn@confluent.io>
2020-05-04 15:40:25 -0700

    docs: cherry-pick: backport array length UDF to 0.7.1-ksqldb (DOCS-4247) (#5264)
    Co-authored-by: Andy Coates <8012398+big-andy-coates@users.noreply.github.com>

---
+++ ksql-engine/src/main/java/io/confluent/ksql/function/udf/array/ArrayLength.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"; you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.confluent.ksql.function.udf.array;
+
+import io.confluent.ksql.function.udf.Udf;
+import io.confluent.ksql.function.udf.UdfDescription;
+import io.confluent.ksql.function.udf.UdfParameter;
+import java.util.List;
+
+/**
+ * Returns the length of an array
+ */
+@SuppressWarnings("MethodMayBeStatic") // UDF methods can not be static.
+@UdfDescription(name = "ARRAY_LENGTH", description = "Returns the length on an array")
+public class ArrayLength {
+
+  @Udf
+  public <T> Integer calcArrayLength(
+      @UdfParameter(description = "The array") final List<T> array
+  ) {
+    if (array == null) {
+      return null;
+    }
+    return array.size();
+  }
+}

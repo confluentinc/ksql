123fa34b6e176b22ccdc8f52b583b2b4264e7a79
Mateusz Mrozewski <matixo@gmail.com>
2020-05-18 20:48:55 +1000

    feat: INSTR function #881
    Adds implementation of INSTR function

---
+++ ksqldb-engine/src/main/java/io/confluent/ksql/function/udf/string/Instr.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2018 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.confluent.ksql.function.udf.string;
+
+import static org.apache.commons.lang3.StringUtils.ordinalIndexOf;
+import static org.apache.commons.lang3.StringUtils.substring;
+
+import io.confluent.ksql.function.udf.Udf;
+import io.confluent.ksql.function.udf.UdfDescription;
+import io.confluent.ksql.util.KsqlConstants;
+import org.apache.commons.lang3.StringUtils;
+
+@UdfDescription(name = "instr",
+    author = KsqlConstants.CONFLUENT_AUTHOR,
+    description = "PROPER DESCRIPTION")
+public class Instr {
+
+  @Udf
+  public int instr(final String str, final String substring) {
+    return instr(str, substring, 1);
+  }
+
+  @Udf
+  public int instr(final String str, final String substring, final int position) {
+    return instr(str, substring, position, 1);
+  }
+
+  @Udf
+  public int instr(final String str, final String substring, final int position,
+      final int occurrence) {
+    if (str == null || substring == null) {
+      return 0;
+    }
+    if (Math.abs(position) > str.length()) {
+      return 0;
+    }
+    if (position < 0) {
+      final String reversedStr = StringUtils.reverse(str);
+      final String reversedSubstring = StringUtils.reverse(substring);
+      return find(reversedStr, reversedSubstring, position * -1, occurrence, true);
+    }
+    return find(str, substring, position, occurrence, false);
+  }
+
+  private int find(final String str, final String substring, final int position,
+      final int occurence, final boolean reversed) {
+    final int i = ordinalIndexOf(substring(str, position - 1), substring, occurence);
+    return reversed ? str.length() - position - i : i + position;
+  }
+}

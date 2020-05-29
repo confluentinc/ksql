16747bd2cd9c2dd0df8f3c930de897d93efd2694
Natea Beshada <nbeshada@MacBook-Pro.local>
2020-05-29 12:03:41 -0500

    added ignore back to PlannedTestGenerator

--- ksqldb-functional-tests/src/test/java/io/confluent/ksql/test/planned/PlannedTestGeneratorTest.java
+++ ksqldb-functional-tests/src/test/java/io/confluent/ksql/test/planned/PlannedTestGeneratorTest.java
@@ -29,7 +29,7 @@ public class PlannedTestGeneratorTest {
    * or changed query plans.
    */
   @Test
-  //@Ignore
+  @Ignore
   public void manuallyGeneratePlans() {
     PlannedTestGenerator.generatePlans(QueryTranslationTest.findTestCases()
         .filter(PlannedTestUtils::isNotExcluded));

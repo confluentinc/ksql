30309bf07fbc706fb5200996ce33318d379cd97f
Alan Sheinberg <57688982+AlanConfluent@users.noreply.github.com>
2020-06-02 12:49:34 -0700

    feat: Adds UDF regexp_replace (#5504)
    * feat: Adds UDF regexp_replace

--- docs/developer-guide/ksqldb-reference/scalar-functions.md
+++ docs/developer-guide/ksqldb-reference/scalar-functions.md
@@ -481,6 +481,16 @@ the entire substring is returned by default.
 For example, `REGEXP_EXTRACT("(\\w+) (\\w+)", 'hello there nice day', 2)`
 returns `['there', 'day']`.
 
+### `REGEXP_REPLACE`
+
+```sql
+REGEXP_REPLACE(col1, 'a.b+', 'bar')
+```
+
+Replace all matches of a regex in an input string with a new string.
+If either the input string, regular expression, or new string is null,
+the result is null.
+
 ### `REGEXP_SPLIT_TO_ARRAY`
 
 ```sql
---
+++ ksqldb-engine/src/test/java/io/confluent/ksql/function/udf/string/RegexpReplaceTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.confluent.ksql.function.udf.string;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+
+import io.confluent.ksql.function.KsqlFunctionException;
+import org.junit.Test;
+
+public class RegexpReplaceTest {
+
+  private RegexpReplace udf = new RegexpReplace();
+
+  @Test
+  public void shouldHandleNull() {
+    assertThat(udf.regexpReplace(null, "foo", "bar"), isEmptyOrNullString());
+    assertThat(udf.regexpReplace("foo", null, "bar"), isEmptyOrNullString());
+    assertThat(udf.regexpReplace("foo", "bar", null), isEmptyOrNullString());
+  }
+
+  @Test
+  public void shouldReplace() {
+    assertThat(udf.regexpReplace("foobar", "foo", "bar"), is("barbar"));
+    assertThat(udf.regexpReplace("foobar", "fooo", "bar"), is("foobar"));
+    assertThat(udf.regexpReplace("foobar", "o", ""), is("fbar"));
+    assertThat(udf.regexpReplace("abc", "", "n"), is("nanbncn"));
+
+    assertThat(udf.regexpReplace("foobar", "(foo|bar)", "cat"), is("catcat"));
+    assertThat(udf.regexpReplace("foobar", "^foo", "cat"), is("catbar"));
+    assertThat(udf.regexpReplace("foobar", "^bar", "cat"), is("foobar"));
+    assertThat(udf.regexpReplace("barbar", "bar$", "cat"), is("barcat"));
+    assertThat(udf.regexpReplace("aababa", "ab", "xy"), is("axyxya"));
+    assertThat(udf.regexpReplace("aababa", "(ab)+", "xy"), is("axya"));
+  }
+
+  @Test(expected = KsqlFunctionException.class)
+  public void shouldThrowExceptionOnBadPattern() {
+    udf.regexpReplace("foobar", "(()", "bar");
+  }
+}

b45b3020864aa7832837a84cf0e7397098e91d5f
blueedgenick <nick@confluent.io>
2020-06-03 22:45:57 -0700

    initial version

---
+++ ksqldb-engine/src/main/java/io/confluent/ksql/function/udf/array/ArrayUnion.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License; you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the
+ * License.
+ */
+
+package io.confluent.ksql.function.udf.array;
+
+import com.google.common.collect.Sets;
+import io.confluent.ksql.function.udf.Udf;
+import io.confluent.ksql.function.udf.UdfDescription;
+import io.confluent.ksql.function.udf.UdfParameter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+@UdfDescription(
+    name = "array_union",
+    description = "Returns an array of all the distinct elements from both input arrays, "
+        + "or NULL if either array is NULL.")
+public class ArrayUnion {
+
+  @SuppressWarnings("unchecked")
+  @Udf
+  public <T> List<T> union(
+      @UdfParameter(description = "First array of values") final List<T> left,
+      @UdfParameter(description = "Second array of values") final List<T> right) {
+    if (left == null || right == null) {
+      return null;
+    }
+    final Set<T> combined = Sets.newLinkedHashSet(left);
+    combined.addAll(right);
+    return (List<T>) Arrays.asList(combined.toArray());
+  }
+
+}
---
+++ ksqldb-engine/src/test/java/io/confluent/ksql/function/udf/array/ArrayExceptTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License; you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the
+ * License.
+ */
+
+package io.confluent.ksql.function.udf.array;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class ArrayExceptTest {
+  private final ArrayExcept udf = new ArrayExcept();
+
+  @Test
+  public void shouldRemoveExceptions() {
+    final List<String> input1 = Arrays.asList("foo", "bar", "baz");
+    final List<String> input2 = Arrays.asList("foo", "bar");
+    final List<String> result = udf.except(input1, input2);
+    assertThat(result, is(Arrays.asList("baz")));
+  }
+
+  @Test
+  public void shouldRetainOnlyDistinctValues() {
+    final List<String> input1 = Arrays.asList("foo", " ", "foo", "bar");
+    final List<String> input2 = Arrays.asList("bar");
+    final List<String> result = udf.except(input1, input2);
+    assertThat(result, contains("foo", " "));
+  }
+
+  @Test
+  public void shouldReturnEmptyArrayIfAllExcepted() {
+    final List<String> input1 = Arrays.asList("foo", " ", "foo", "bar");
+    final List<String> input2 = Arrays.asList("bar", " ", "foo", "extra");
+    final List<String> result = udf.except(input1, input2);
+    assertThat(result.isEmpty(), is(true));
+  }
+
+  @Test
+  public void shouldExceptEmptyArray() {
+    final List<String> input1 = Arrays.asList();
+    final List<String> input2 = Arrays.asList("foo");
+    final List<String> result = udf.except(input1, input2);
+    assertThat(result.isEmpty(), is(true));
+  }
+
+  @Test
+  public void shouldDistinctValuesForEmptyExceptionArray() {
+    final List<String> input1 = Arrays.asList("foo", "foo", "bar", "foo");
+    final List<String> input2 = Arrays.asList();
+    final List<String> result = udf.except(input1, input2);
+    assertThat(result, contains("foo", "bar"));
+  }
+
+  @Test
+  public void shouldReturnNullForNullLeftInput() {
+    final List<String> input1 = Arrays.asList("foo");
+    final List<String> result = udf.except(input1, null);
+    assertThat(result, is(nullValue()));
+  }
+
+  @Test
+  public void shouldReturnNullForNullExceptionArray() {
+    final List<String> input2 = Arrays.asList("foo");
+    final List<String> result = udf.except(null, input2);
+    assertThat(result, is(nullValue()));
+  }
+
+  @Test
+  public void shouldReturnNullForNullInputs() {
+    @SuppressWarnings("rawtypes")
+    final List result = udf.except(null, null);
+    assertThat(result, is(nullValue()));
+  }
+
+}

082b5d243b9c5bac48ba6406a91619046cea77fa
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2020-08-07 12:47:24 +0100

    fix import

--- ksqldb-rest-model/src/main/java/io/confluent/ksql/rest/entity/SourceDescription.java
+++ ksqldb-rest-model/src/main/java/io/confluent/ksql/rest/entity/SourceDescription.java
@@ -22,7 +22,6 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.google.common.collect.ImmutableList;
 import io.confluent.ksql.model.WindowType;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;

6fae7bd11c4ebdc00c542d772caeb9f9fa6f07e1
Sarwar Bhuiyan <sarwar@confluent.io>
2020-08-07 18:42:15 +0100

    Merged code for Latest N

--- ksqldb-engine/src/main/java/io/confluent/ksql/function/udaf/offset/LatestByOffset.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/function/udaf/offset/LatestByOffset.java
@@ -28,7 +28,12 @@ import io.confluent.ksql.function.udaf.Udaf;
 import io.confluent.ksql.function.udaf.UdafDescription;
 import io.confluent.ksql.function.udaf.UdafFactory;
 import io.confluent.ksql.util.KsqlConstants;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.Struct;
 
@@ -40,17 +45,25 @@ import org.apache.kafka.connect.data.Struct;
 public final class LatestByOffset {
 
   static final String DESCRIPTION =
-      "This function returns the most recent value for the column, computed by offset.";
+      "This function returns the most recent n values for the column, computed by offset.";
 
   private LatestByOffset() {
   }
 
   static AtomicLong sequence = new AtomicLong();
+  
+  static final Comparator<Struct> structComparator  = new LatestStructComparator();
 
   @UdafFactory(description = "return the latest value of an integer column",
       aggregateSchema = "STRUCT<SEQ BIGINT, VAL INT>")
   public static Udaf<Integer, Struct, Integer> latestInteger() {
     return latest(STRUCT_INTEGER);
+  }
+  
+  @UdafFactory(description = "return the latest value of an integer column",
+      aggregateSchema = "ARRAY<STRUCT<SEQ BIGINT, VAL INT>>")
+  public static Udaf<Integer, List<Struct>, List<Integer>> latestInteger(final int topNSize) {
+    return latestN(STRUCT_INTEGER, topNSize);
   }
 
   @UdafFactory(description = "return the latest value of an big integer column",
@@ -58,11 +71,23 @@ public final class LatestByOffset {
   public static Udaf<Long, Struct, Long> latestLong() {
     return latest(STRUCT_LONG);
   }
+  
+  @UdafFactory(description = "return the latest value of an big integer column",
+      aggregateSchema = "ARRAY<STRUCT<SEQ BIGINT, VAL BIGINT>>")
+  public static Udaf<Long, List<Struct>, List<Long>> latestLong(final int topNSize) {
+    return latestN(STRUCT_LONG, topNSize);
+  }
 
   @UdafFactory(description = "return the latest value of a double column",
       aggregateSchema = "STRUCT<SEQ BIGINT, VAL DOUBLE>")
   public static Udaf<Double, Struct, Double> latestDouble() {
     return latest(STRUCT_DOUBLE);
+  }
+  
+  @UdafFactory(description = "return the latest value of a double column",
+      aggregateSchema = "ARRAY<STRUCT<SEQ BIGINT, VAL DOUBLE>>")
+  public static Udaf<Double, List<Struct>, List<Double>> latestDouble(final int topNSize) {
+    return latestN(STRUCT_DOUBLE, topNSize);
   }
 
   @UdafFactory(description = "return the latest value of a boolean column",
@@ -70,13 +95,25 @@ public final class LatestByOffset {
   public static Udaf<Boolean, Struct, Boolean> latestBoolean() {
     return latest(STRUCT_BOOLEAN);
   }
+  
+  @UdafFactory(description = "return the latest value of a boolean column",
+      aggregateSchema = "ARRAY<STRUCT<SEQ BIGINT, VAL BOOLEAN>>")
+  public static Udaf<Boolean, List<Struct>, List<Boolean>> latestBoolean(final int topNSize) {
+    return latestN(STRUCT_BOOLEAN, topNSize);
+  }
 
   @UdafFactory(description = "return the latest value of a string column",
       aggregateSchema = "STRUCT<SEQ BIGINT, VAL STRING>")
   public static Udaf<String, Struct, String> latestString() {
     return latest(STRUCT_STRING);
   }
-
+  
+  @UdafFactory(description = "return the latest value of a string column",
+      aggregateSchema = "ARRAY<STRUCT<SEQ BIGINT, VAL STRING>>")
+  public static Udaf<String, List<Struct>, List<String>> latestString(final int topNSize) {
+    return latestN(STRUCT_STRING, topNSize);
+  }
+  
   static <T> Struct createStruct(final Schema schema, final T val) {
     final Struct struct = new Struct(schema);
     struct.put(SEQ_FIELD, generateSequence());
@@ -121,6 +158,51 @@ public final class LatestByOffset {
       @SuppressWarnings("unchecked")
       public T map(final Struct agg) {
         return (T) agg.get(VAL_FIELD);
+      }
+    };
+  }
+  
+  @UdafFactory(description = "Latest N by offset")
+  static <T> Udaf<T, List<Struct>, List<T>> latestN(final Schema structSchema, final int topNSize) {
+    return new Udaf<T, List<Struct>, List<T>>() {
+      @Override
+      public List<Struct> initialize() {
+        return new ArrayList<Struct>();
+      }
+
+      @Override
+      public List<Struct> aggregate(final T current, final List<Struct> aggregate) {
+        if (current == null) {
+          return aggregate;
+        }
+
+        aggregate.add(createStruct(structSchema, current));
+        final int currentSize = aggregate.size();
+        if (currentSize <= topNSize) {
+          return aggregate;
+        } else {
+          return aggregate.subList(currentSize - topNSize, currentSize);
+        }
+      }
+
+      @Override
+      public List<Struct> merge(final List<Struct> aggOne, final List<Struct> aggTwo) {
+        final List<Struct> merged = new ArrayList<>();
+        merged.addAll(aggOne);
+        merged.addAll(aggTwo);
+        final int currentSize = merged.size();
+        Collections.sort(merged, structComparator);
+        if (currentSize <= topNSize) {
+          return merged;
+        } else {
+          return merged.subList(currentSize - topNSize, currentSize);
+        }
+      }
+
+      @Override
+      @SuppressWarnings("unchecked")
+      public List<T> map(final List<Struct> agg) {
+        return (List<T>) agg.stream().map(s -> s.get(VAL_FIELD)).collect(Collectors.toList());
       }
     };
   }
---
+++ ksqldb-engine/src/main/java/io/confluent/ksql/function/udaf/offset/LatestStructComparator.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"; you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.confluent.ksql.function.udaf.offset;
+
+import static io.confluent.ksql.function.udaf.KudafByOffsetUtils.SEQ_FIELD;
+
+import java.io.Serializable;
+import java.util.Comparator;
+import org.apache.kafka.connect.data.Struct;
+
+public class LatestStructComparator implements Comparator<Struct>, Serializable {
+  
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public int compare(final Struct struct1, final Struct struct2) {
+    final long sequence1 = struct1.getInt64(SEQ_FIELD);
+    final long sequence2 = struct2.getInt64(SEQ_FIELD);
+    if (sequence1 < 0 && sequence2 >= 0) {
+      return 1;
+    } else if (sequence2 < 0 && sequence1 >= 0) {
+      return -1;
+    } else {
+      return Long.compare(sequence1, sequence2);
+    }
+  }
+}

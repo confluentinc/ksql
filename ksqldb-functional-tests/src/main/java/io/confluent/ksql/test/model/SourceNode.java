4901db68f0a5efd2d657fd75278be79171deaff0
Victoria Xia <victoria.xia@confluent.io>
2020-08-13 10:32:04 -0400

    chore: switch back to NON_EMPTY

--- ksqldb-functional-tests/src/main/java/io/confluent/ksql/test/model/SourceNode.java
+++ ksqldb-functional-tests/src/main/java/io/confluent/ksql/test/model/SourceNode.java
@@ -89,7 +89,7 @@ public final class SourceNode {
     return schema;
   }
 
-  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @JsonInclude(JsonInclude.Include.NON_EMPTY)
   public Optional<Set<SerdeOption>> getSerdeOptions() {
     return serdeOptions;
   }

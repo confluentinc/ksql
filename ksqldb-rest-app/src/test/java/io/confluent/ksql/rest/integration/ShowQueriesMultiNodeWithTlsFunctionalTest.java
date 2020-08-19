c4b199cdb8298c2adef39ce7fc7101673e9611ca
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2020-08-19 22:58:35 +0100

    test: fix flaky test due to static state (#6053)
    The `TlsTest` tended to fail locally when running other tests in parallel. Investigation showed this is due to shared state in `ServerKeyStore`: the path to the key store.  The test needed to write a bad cert and then assert requests failed, but if another test ran it would override the bad cert with a good one.
    
    Co-authored-by: Andy Coates <big-andy-coates@users.noreply.github.com>

--- ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/integration/ShowQueriesMultiNodeWithTlsFunctionalTest.java
+++ ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/integration/ShowQueriesMultiNodeWithTlsFunctionalTest.java
@@ -42,6 +42,8 @@ import org.junit.rules.RuleChain;
 @Category({IntegrationTest.class})
 public class ShowQueriesMultiNodeWithTlsFunctionalTest {
 
+  private static final ServerKeyStore SERVER_KEY_STORE = new ServerKeyStore();
+
   private static final PageViewDataProvider PAGE_VIEWS_PROVIDER = new PageViewDataProvider();
   private static final String PAGE_VIEW_TOPIC = PAGE_VIEWS_PROVIDER.topicName();
   private static final String PAGE_VIEW_STREAM = PAGE_VIEWS_PROVIDER.kstreamName();
@@ -51,14 +53,14 @@ public class ShowQueriesMultiNodeWithTlsFunctionalTest {
       .withProperty(KsqlRestConfig.LISTENERS_CONFIG,
           "http://localhost:8088,https://localhost:8189")
       .withProperty(KsqlRestConfig.ADVERTISED_LISTENER_CONFIG, "https://localhost:8189")
-      .withProperties(ServerKeyStore.keyStoreProps())
+      .withProperties(SERVER_KEY_STORE.keyStoreProps())
       .build();
   private static final TestKsqlRestApp REST_APP_1 = TestKsqlRestApp
       .builder(TEST_HARNESS::kafkaBootstrapServers)
       .withProperty(KsqlRestConfig.LISTENERS_CONFIG,
           "http://localhost:8098,https://localhost:8199")
       .withProperty(KsqlRestConfig.ADVERTISED_LISTENER_CONFIG, "https://localhost:8199")
-      .withProperties(ServerKeyStore.keyStoreProps())
+      .withProperties(SERVER_KEY_STORE.keyStoreProps())
       .build();
 
   @ClassRule

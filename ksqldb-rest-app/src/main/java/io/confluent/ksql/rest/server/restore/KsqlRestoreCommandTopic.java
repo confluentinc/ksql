c12aee6504452b7b71273bcae3eee4a57acd8d0a
Andy Coates <big-andy-coates@users.noreply.github.com>
2020-11-02 17:00:32 +0000

    chore: fix spotbugs errors

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/restore/KsqlRestoreCommandTopic.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/restore/KsqlRestoreCommandTopic.java
@@ -31,7 +31,6 @@ import io.confluent.ksql.util.KsqlConfig;
 import io.confluent.ksql.util.KsqlException;
 import io.confluent.ksql.util.Pair;
 import io.confluent.ksql.util.ReservedInternalTopics;
-
 import java.io.Console;
 import java.io.File;
 import java.io.IOException;
@@ -43,7 +42,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier;
-
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -88,8 +86,9 @@ public class KsqlRestoreCommandTopic {
       } catch (final Exception e) {
         throw new KsqlException(String.format(
             "Invalid CommandId string (line %d): %s",
-            n, new String(record.getLeft(), StandardCharsets.UTF_8), e
-        ));
+            n, new String(record.getLeft(), StandardCharsets.UTF_8)),
+            e
+        );
       }
 
       try {
@@ -98,8 +97,9 @@ public class KsqlRestoreCommandTopic {
       } catch (final Exception e) {
         throw new KsqlException(String.format(
             "Invalid Command string (line %d): %s",
-            n, new String(record.getRight(), StandardCharsets.UTF_8), e
-        ));
+            n, new String(record.getRight(), StandardCharsets.UTF_8)),
+            e
+        );
       }
     }
   }
@@ -135,12 +135,7 @@ public class KsqlRestoreCommandTopic {
     final Console console = System.console();
     final String decision = console.readLine();
 
-    switch (decision.toLowerCase()) {
-      case "yes":
-        return true;
-      default:
-        return false;
-    }
+    return "yes".equals(decision.toLowerCase());
   }
 
   /**

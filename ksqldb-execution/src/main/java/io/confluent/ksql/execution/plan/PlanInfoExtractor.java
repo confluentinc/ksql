476326f52f477619014f8bed5ea04ea2349009e2
Victoria Xia <victoria.xia@confluent.io>
2020-12-03 17:32:08 -0800

    chore: remove ExecutionStepVisitor

--- ksqldb-execution/src/main/java/io/confluent/ksql/execution/plan/PlanInfoExtractor.java
+++ ksqldb-execution/src/main/java/io/confluent/ksql/execution/plan/PlanInfoExtractor.java
@@ -22,126 +22,102 @@ package io.confluent.ksql.execution.plan;
  *
  * <p>See {@link PlanInfo} description for more.
  */
-public class PlanInfoExtractor implements ExecutionStepVisitor<PlanInfo> {
+public class PlanInfoExtractor {
   
-  @Override
   public <K> PlanInfo visitStreamFilter(final StreamFilter<K> streamFilter) {
     return visitSingleSourceStep(streamFilter);
   }
 
-  @Override
   public <K> PlanInfo visitStreamGroupBy(final StreamGroupBy<K> streamGroupBy) {
     return visitSingleSourceStep(streamGroupBy);
   }
 
-  @Override
   public PlanInfo visitStreamGroupByKey(final StreamGroupByKey streamGroupByKey) {
     return visitSingleSourceStep(streamGroupByKey);
   }
 
-  @Override
   public PlanInfo visitStreamAggregate(final StreamAggregate streamAggregate) {
     return visitSingleSourceStep(streamAggregate);
   }
 
-  @Override
   public <K> PlanInfo visitStreamSelect(final StreamSelect<K> streamSelect) {
     return visitSingleSourceStep(streamSelect);
   }
 
-  @Override
   public <K> PlanInfo visitFlatMap(final StreamFlatMap<K> streamFlatMap) {
     return visitSingleSourceStep(streamFlatMap);
   }
 
-  @Override
   public PlanInfo visitStreamSelectKey(final StreamSelectKeyV1 streamSelectKey) {
     return visitRepartitionStep(streamSelectKey);
   }
 
-  @Override
   public <K> PlanInfo visitStreamSelectKey(final StreamSelectKey<K> streamSelectKey) {
     return visitRepartitionStep(streamSelectKey);
   }
 
-  @Override
   public <K> PlanInfo visitStreamSink(final StreamSink<K> streamSink) {
     return visitSingleSourceStep(streamSink);
   }
 
-  @Override
   public PlanInfo visitStreamSource(final StreamSource streamSource) {
     return visitSourceStep(streamSource);
   }
 
-  @Override
   public PlanInfo visitWindowedStreamSource(final WindowedStreamSource windowedStreamSource) {
     return visitSourceStep(windowedStreamSource);
   }
 
-  @Override
   public <K> PlanInfo visitStreamStreamJoin(final StreamStreamJoin<K> streamStreamJoin) {
     return visitJoinStep(streamStreamJoin);
   }
 
-  @Override
   public <K> PlanInfo visitStreamTableJoin(final StreamTableJoin<K> streamTableJoin) {
     return visitJoinStep(streamTableJoin);
   }
 
-  @Override
   public PlanInfo visitTableSource(final TableSource tableSource) {
     return visitSourceStep(tableSource);
   }
 
-  @Override
   public PlanInfo visitWindowedTableSource(final WindowedTableSource windowedTableSource) {
     return visitSourceStep(windowedTableSource);
   }
 
-  @Override
   public PlanInfo visitStreamWindowedAggregate(
       final StreamWindowedAggregate streamWindowedAggregate
   ) {
     return visitSingleSourceStep(streamWindowedAggregate);
   }
 
-  @Override
   public PlanInfo visitTableAggregate(final TableAggregate tableAggregate) {
     return visitSingleSourceStep(tableAggregate);
   }
 
-  @Override
   public <K> PlanInfo visitTableFilter(final TableFilter<K> tableFilter) {
     return visitSingleSourceStep(tableFilter);
   }
 
-  @Override
   public <K> PlanInfo visitTableGroupBy(final TableGroupBy<K> tableGroupBy) {
     return visitSingleSourceStep(tableGroupBy);
   }
 
-  @Override
   public <K> PlanInfo visitTableSelect(final TableSelect<K> tableSelect) {
     return visitSingleSourceStep(tableSelect);
   }
 
-  @Override
   public <K> PlanInfo visitTableSelectKey(final TableSelectKey<K> tableSelectKey) {
     return visitRepartitionStep(tableSelectKey);
   }
 
-  @Override
   public <K> PlanInfo visitTableSink(final TableSink<K> tableSink) {
     return visitSingleSourceStep(tableSink);
   }
 
-  @Override
   public <K> PlanInfo visitTableSuppress(final TableSuppress<K> tableSuppress) {
     return visitSingleSourceStep(tableSuppress);
   }
 
-  @Override
   public <K> PlanInfo visitTableTableJoin(final TableTableJoin<K> tableTableJoin) {
     return visitJoinStep(tableTableJoin);
   }

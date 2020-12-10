99d84ce46c520aed905cc1417a2eec21f047cb23
Vicky Papavasileiou <vpapavasileiou@confluent.io>
2020-12-09 16:24:28 -0800

    work in progress

--- ksqldb-engine/src/main/java/io/confluent/ksql/planner/plan/PullQueryFilterNode.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/planner/plan/PullQueryFilterNode.java
@@ -15,23 +15,34 @@
 
 package io.confluent.ksql.planner.plan;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
-import io.confluent.ksql.analyzer.Analysis;
 import io.confluent.ksql.analyzer.PullQueryValidator;
 import io.confluent.ksql.analyzer.RewrittenAnalysis;
+import io.confluent.ksql.engine.generic.GenericExpressionResolver;
 import io.confluent.ksql.execution.builder.KsqlQueryBuilder;
-import io.confluent.ksql.execution.context.QueryContext.Stacker;
+import io.confluent.ksql.execution.codegen.CodeGenRunner;
+import io.confluent.ksql.execution.codegen.ExpressionMetadata;
 import io.confluent.ksql.execution.expression.tree.ComparisonExpression;
 import io.confluent.ksql.execution.expression.tree.ComparisonExpression.Type;
 import io.confluent.ksql.execution.expression.tree.Expression;
 import io.confluent.ksql.execution.expression.tree.InPredicate;
+import io.confluent.ksql.execution.expression.tree.Literal;
+import io.confluent.ksql.execution.expression.tree.LogicalBinaryExpression;
+import io.confluent.ksql.execution.expression.tree.NullLiteral;
+import io.confluent.ksql.execution.expression.tree.TraversalExpressionVisitor;
 import io.confluent.ksql.execution.expression.tree.UnqualifiedColumnReferenceExp;
+import io.confluent.ksql.metastore.MetaStore;
 import io.confluent.ksql.name.ColumnName;
-import io.confluent.ksql.physical.pull.operators.WhereInfo.KeyAndWindowBounds;
+import io.confluent.ksql.schema.ksql.Column;
+import io.confluent.ksql.schema.ksql.DefaultSqlValueCoercer;
 import io.confluent.ksql.schema.ksql.LogicalSchema;
 import io.confluent.ksql.schema.ksql.SystemColumns;
+import io.confluent.ksql.schema.utils.FormatOptions;
+import io.confluent.ksql.util.KsqlConfig;
 import io.confluent.ksql.util.KsqlException;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -48,6 +59,7 @@ public class PullQueryFilterNode extends SingleSourcePlanNode {
   private final Expression predicate;
   private final boolean isWindowed;
   private final RewrittenAnalysis analysis;
+  private final ExpressionMetadata compiledWhereClause;
 
   private boolean isKeyedQuery;
 
@@ -55,7 +67,9 @@ public class PullQueryFilterNode extends SingleSourcePlanNode {
       final PlanNodeId id,
       final PlanNode source,
       final Expression predicate,
-      final RewrittenAnalysis analysis
+      final RewrittenAnalysis analysis,
+      final MetaStore metaStore,
+      final KsqlConfig ksqlConfig
   ) {
     super(id, source.getNodeOutputType(), source.getSourceName(), source);
 
@@ -66,6 +80,18 @@ public class PullQueryFilterNode extends SingleSourcePlanNode {
         .getDataSource()
         .getKsqlTopic()
         .getKeyFormat().isWindowed();
+    final Validator validator = new Validator();
+    validator.process(predicate, null);
+    if (!isKeyedQuery) {
+      throw invalidWhereClauseException("WHERE clause missing key column", isWindowed);
+    }
+    compiledWhereClause = CodeGenRunner.compileExpression(
+        predicate,
+        "Predicate",
+        getSchema(),
+        ksqlConfig,
+        metaStore
+    );
   }
 
   public Expression getPredicate() {
@@ -82,27 +108,78 @@ public class PullQueryFilterNode extends SingleSourcePlanNode {
     throw new UnsupportedOperationException();
   }
 
+  public ExpressionMetadata getCompiledWhereClause() {
+    return compiledWhereClause;
+  }
+
+  private List<Object> extractKeys() {
+    final List<Object> keys;
+
+    if (keyComparison.size() > 0) {
+      keys = ImmutableList.of(
+          extractKeyWhereClause(
+              keyComparison,
+              windowed,
+              query.getLogicalSchema(),
+              metaStore,
+              config)
+      );
+    } else {
+      keys = extractKeysFromInPredicate(
+          inPredicate,
+          windowed,
+          query.getLogicalSchema(),
+          metaStore,
+          config
+      );
+    }
+  }
+
   /**
    * Validate the WHERE clause for pull queries.
-   * 1. There must be at least one equality condition or IN predicate that involves a key.
+   * 1. There must be exactly one equality condition or one IN predicate that involves a key.
    * 2. An IN predicate can refer to a single key.
    * 3. The IN predicate cannot be combined with other conditions.
+   * 4. Only AND is allowed.
    */
-  private void validateWhereClause() {
-    // Visit all comparison expressions in WHERE clause
+  private final class Validator extends TraversalExpressionVisitor<Object> {
 
-    if (predicate instanceof ComparisonExpression) {
+    //TODO any other expression should throw exception
+
+    @Override
+    public Void visitLogicalBinaryExpression(
+        final LogicalBinaryExpression node,
+        final Object context
+    ) {
+      if (node.getType() != LogicalBinaryExpression.Type.AND) {
+        throw invalidWhereClauseException("Only AND expressions are supported: " + node, false);
+      }
+      process(node.getLeft(), context);
+      process(node.getRight(), context);
+      return null;
+    }
+
+    @Override
+    public Void visitComparisonExpression(
+        final ComparisonExpression node,
+        final Object context
+    ) {
       final ComparisonExpression comparison = (ComparisonExpression) predicate;
+      final ColumnName keyColumn = Iterables.getOnlyElement(getSource().getSchema().key()).name();
+      if (comparison.getType() != Type.EQUAL) {
+        throw invalidWhereClauseException(
+            "Bound on '" + keyColumn.text() + "' must currently be '='", isWindowed);
+      }
       final UnqualifiedColumnReferenceExp column;
       if (comparison.getRight() instanceof UnqualifiedColumnReferenceExp) {
         column = (UnqualifiedColumnReferenceExp) comparison.getRight();
       } else if (comparison.getLeft() instanceof UnqualifiedColumnReferenceExp) {
         column = (UnqualifiedColumnReferenceExp) comparison.getLeft();
       } else {
-        return;
+        return null;
       }
       final ColumnName columnName = column.getColumnName();
-      final ColumnName keyColumn = Iterables.getOnlyElement(getSource().getSchema().key()).name();
+
       if (columnName.equals(keyColumn)) {
         if (isKeyedQuery) {
           throw invalidWhereClauseException(
@@ -110,10 +187,18 @@ public class PullQueryFilterNode extends SingleSourcePlanNode {
                   + " such as an IN predicate",
               isWindowed);
         }
+        // TODO check if columnName is window and throw if table not windowed
+        // TODO Check if column is not key and throw
         isKeyedQuery = true;
-        return;
       }
-    } else if (predicate instanceof InPredicate) {
+      return null;
+    }
+
+    @Override
+    public Void visitInPredicate(
+        final InPredicate node,
+        final Object context
+    ) {
       final InPredicate inPredicate = (InPredicate) predicate;
       final UnqualifiedColumnReferenceExp column
           = (UnqualifiedColumnReferenceExp) inPredicate.getValue();
@@ -124,36 +209,72 @@ public class PullQueryFilterNode extends SingleSourcePlanNode {
               "The IN predicate cannot be combined with other comparisons on the key column",
               isWindowed);
         }
+        // TODO Check if column is not key and throw
         isKeyedQuery = true;
-        return;
       }
+      return null;
     }
   }
 
   private KsqlException invalidWhereClauseException(
-    final String msg,
-    final boolean windowed
+      final String msg,
+      final boolean windowed
   ) {
-      final String additional = !windowed
-          ? ""
-          : System.lineSeparator()
-              + " - (optionally) limits the time bounds of the windowed table."
-              + System.lineSeparator()
-              + "\t Bounds on " + SystemColumns.windowBoundsColumnNames() + " are supported"
-              + System.lineSeparator()
-              + "\t Supported operators are " + VALID_WINDOW_BOUND_COMPARISONS;
+    final String additional = !windowed
+        ? ""
+        : System.lineSeparator()
+            + " - (optionally) limits the time bounds of the windowed table."
+            + System.lineSeparator()
+            + "\t Bounds on " + SystemColumns.windowBoundsColumnNames() + " are supported"
+            + System.lineSeparator()
+            + "\t Supported operators are " + VALID_WINDOW_BOUND_COMPARISONS;
 
-      return new KsqlException(
-          msg
-              + ". "
-              + PullQueryValidator.PULL_QUERY_SYNTAX_HELP
-              + System.lineSeparator()
-              + "Pull queries require a WHERE clause that:"
-              + System.lineSeparator()
-              + " - limits the query to a single key, e.g. `SELECT * FROM X WHERE <key-column>=Y;`."
-              + additional
-      );
+    return new KsqlException(
+        msg
+            + ". "
+            + PullQueryValidator.PULL_QUERY_SYNTAX_HELP
+            + System.lineSeparator()
+            + "Pull queries require a WHERE clause that:"
+            + System.lineSeparator()
+            + " - limits the query to a single key, e.g. `SELECT * FROM X WHERE <key-column>=Y;`."
+            + additional
+    );
+  }
+
+  private static Object resolveKey(
+      final Expression exp,
+      final Column keyColumn,
+      final MetaStore metaStore,
+      final KsqlConfig config,
+      final Expression errorMessageHint
+  ) {
+    final Object obj;
+    if (exp instanceof NullLiteral) {
+      obj = null;
+    } else if (exp instanceof Literal) {
+      // skip the GenericExpressionResolver because this is
+      // a critical code path executed once-per-query
+      obj = ((Literal) exp).getValue();
+    } else {
+      obj = new GenericExpressionResolver(
+          keyColumn.type(),
+          keyColumn.name(),
+          metaStore,
+          config,
+          "pull query"
+      ).resolve(exp);
     }
+
+    if (obj == null) {
+      throw new KsqlException("Primary key columns can not be NULL: " + errorMessageHint);
+    }
+
+    return DefaultSqlValueCoercer.STRICT.coerce(obj, keyColumn.type())
+        .orElseThrow(() -> new KsqlException("'" + obj + "' can not be converted "
+                                                 + "to the type of the key column: " + keyColumn.toString(
+            FormatOptions.noEscape())))
+        .orElse(null);
+  }
 
 
 }

7dd8af2a72f75c6612b7c20499825516b157d1d2
Rohan <desai.p.rohan@gmail.com>
2021-01-19 23:04:19 -0800

    fix: fix how the buffer limit check evaluates streams config
    The buffer limit check first creates a dummy streams config to get the current
    max bytes buffering value. To do this it creates a map of the streams props and
    creates the config (with dummy values for all the required fields). However
    sometimes those fields are already provided and ImmutableMap doesn't like
    overwrites in the builder. So, use a plain old HashMap

--- ksqldb-engine/src/main/java/io/confluent/ksql/query/KafkaStreamsQueryValidator.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/query/KafkaStreamsQueryValidator.java
@@ -23,6 +23,8 @@ import io.confluent.ksql.util.PersistentQueryMetadata;
 import io.confluent.ksql.util.QueryMetadata;
 import io.confluent.ksql.util.TransientQueryMetadata;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.apache.kafka.streams.StreamsConfig;
 
@@ -87,12 +89,10 @@ public class KafkaStreamsQueryValidator implements QueryValidator {
 
   private StreamsConfig getDummyStreamsConfig(final SessionConfig config) {
     // hack to get at default config value
-    return new StreamsConfig(
-        ImmutableMap.<String, Object>builder()
-            .put(StreamsConfig.APPLICATION_ID_CONFIG, "dummy.app.id")
-            .put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy.bootstrap")
-            .putAll(config.getConfig(true).getKsqlStreamConfigProps())
-            .build()
-    );
+    final Map<String, Object> properties = new HashMap<>();
+    properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "dummy.app.id");
+    properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy.bootstrap");
+    properties.putAll(config.getConfig(true).getKsqlStreamConfigProps());
+    return new StreamsConfig(properties);
   }
 }

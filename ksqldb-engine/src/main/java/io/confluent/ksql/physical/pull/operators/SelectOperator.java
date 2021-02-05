1338ea2d425c6f576bdcccc96618fb224f03af82
Alan Sheinberg <asheinberg@confluent.io>
2021-02-05 15:46:32 -0800

    Lint

--- ksqldb-engine/src/main/java/io/confluent/ksql/physical/pull/operators/SelectOperator.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/physical/pull/operators/SelectOperator.java
@@ -30,7 +30,6 @@ import io.confluent.ksql.planner.plan.PullFilterNode;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Function;
 
 public class SelectOperator extends AbstractPhysicalOperator implements UnaryPhysicalOperator {
 

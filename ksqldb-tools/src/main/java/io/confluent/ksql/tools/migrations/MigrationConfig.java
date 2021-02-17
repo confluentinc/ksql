8bdb09a2751365966cf5a4a1436276d7a3e0d9e1
Zara Lim <zlim@confluent.io>
2021-02-17 01:13:26 -0800

    feat: new and initialize commands for migration tool (#6988)
    * new and initialize commands
    
    * add new command
    
    * switch to Java client, refactor configs, use logger instead of stdout
    
    * add log4j config file, other small changes
    
    * fix version number
    
    * fix checkstyle
    
    * final class
    
    * suppress dm_exit warnings
    
    * suppress fb warnings

---
+++ ksqldb-tools/src/main/java/io/confluent/ksql/tools/migrations/MigrationConfig.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2021 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"; you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.confluent.ksql.tools.migrations;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.confluent.ksql.properties.PropertiesUtil;
+import io.confluent.ksql.rest.client.KsqlRestClient;
+import io.confluent.ksql.rest.client.KsqlRestClientException;
+import io.confluent.ksql.rest.client.RestResponse;
+import io.confluent.ksql.rest.entity.ServerInfo;
+import io.confluent.ksql.util.KsqlException;
+import java.io.File;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigDef.Importance;
+import org.apache.kafka.common.config.ConfigDef.Type;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class MigrationConfig extends AbstractConfig {
+
+  public static final String KSQL_SERVER_URL = "ksql.server.url";
+
+  public static final String KSQL_MIGRATIONS_STREAM_NAME = "ksql.migrations.stream.name";
+  public static final String KSQL_MIGRATIONS_STREAM_NAME_DEFAULT = "migration_events";
+  public static final String KSQL_MIGRATIONS_TABLE_NAME = "ksql.migrations.table.name";
+  public static final String KSQL_MIGRATIONS_TABLE_NAME_DEFAULT = "schema_version";
+  public static final String KSQL_MIGRATIONS_STREAM_TOPIC_NAME =
+      "ksql.migrations.stream.topic.name";
+  public static final String KSQL_MIGRATIONS_TABLE_TOPIC_NAME = "ksql.migrations.table.topic.name";
+  public static final String KSQL_MIGRATIONS_TOPIC_REPLICAS = "ksql.migrations.topic.replicas";
+  public static final int KSQL_MIGRATIONS_TOPIC_REPLICAS_DEFAULT = 1;
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(MigrationConfig.class);
+
+  @SuppressFBWarnings("DM_EXIT")
+  public static MigrationConfig load() {
+    try {
+      final Map<String, String> configsMap =
+          PropertiesUtil.loadProperties(new File("ksql-migrations.properties"));
+      return new MigrationConfig(configsMap, getServiceId(configsMap));
+    } catch (KsqlException e) {
+      LOGGER.error(e.getMessage());
+      System.exit(1);
+    }
+    return null;
+  }
+
+  private MigrationConfig(final Map<String, String> configs, final String id) {
+    super(new ConfigDef()
+        .define(
+            KSQL_SERVER_URL,
+            Type.STRING,
+            "",
+            Importance.HIGH,
+            "The URL for the KSQL server"
+        )
+        .define(
+            KSQL_MIGRATIONS_STREAM_NAME,
+            Type.STRING,
+            KSQL_MIGRATIONS_STREAM_NAME_DEFAULT,
+            Importance.MEDIUM,
+            "The name of the migration stream. It defaults to "
+                + KSQL_MIGRATIONS_STREAM_NAME_DEFAULT
+        ).define(
+            KSQL_MIGRATIONS_TABLE_NAME,
+            Type.STRING,
+            KSQL_MIGRATIONS_TABLE_NAME_DEFAULT,
+            Importance.MEDIUM,
+            "The name of the migration table. It defaults to "
+                + KSQL_MIGRATIONS_TABLE_NAME_DEFAULT
+        ).define(
+            KSQL_MIGRATIONS_STREAM_TOPIC_NAME,
+            Type.STRING,
+            id + "ksql_" + configs
+                .getOrDefault(KSQL_MIGRATIONS_STREAM_NAME, KSQL_MIGRATIONS_STREAM_NAME_DEFAULT),
+            Importance.MEDIUM,
+            "The name of the migration stream topic. It defaults to " + id + "ksql_" + configs
+                .getOrDefault(KSQL_MIGRATIONS_STREAM_NAME, KSQL_MIGRATIONS_STREAM_NAME_DEFAULT)
+        ).define(
+            KSQL_MIGRATIONS_TABLE_TOPIC_NAME,
+            Type.STRING,
+            id + "ksql_" + configs
+                .getOrDefault(KSQL_MIGRATIONS_TABLE_NAME, KSQL_MIGRATIONS_TABLE_NAME_DEFAULT),
+            Importance.MEDIUM,
+            "The name of the migration table topic. It defaults to" + id + "ksql_" + configs
+                .getOrDefault(KSQL_MIGRATIONS_TABLE_NAME, KSQL_MIGRATIONS_TABLE_NAME_DEFAULT)
+        ).define(
+            KSQL_MIGRATIONS_TOPIC_REPLICAS,
+            Type.INT,
+            KSQL_MIGRATIONS_TOPIC_REPLICAS_DEFAULT,
+            Importance.MEDIUM,
+            "The number of replicas for the migration stream topic. It defaults to "
+                + KSQL_MIGRATIONS_TOPIC_REPLICAS_DEFAULT
+        ), configs);
+  }
+
+  @SuppressFBWarnings("DM_EXIT")
+  private static String getServiceId(final Map<String, String> configs) {
+    final String ksqlServerUrl = configs.get(MigrationConfig.KSQL_SERVER_URL);
+    if (ksqlServerUrl == null) {
+      LOGGER.error("Missing required property: " + MigrationConfig.KSQL_SERVER_URL);
+      System.exit(1);
+    }
+
+    final KsqlRestClient client;
+    try {
+      client = KsqlRestClient.create(
+          ksqlServerUrl,
+          Collections.EMPTY_MAP,
+          Collections.EMPTY_MAP,
+          Optional.empty()
+      );
+    } catch (KsqlRestClientException e) {
+      LOGGER.error("Invalid ksql server URL: " + ksqlServerUrl);
+      System.exit(1);
+      return null;
+    }
+
+    final RestResponse<ServerInfo> response = client.getServerInfo();
+    client.close();
+
+    if (!response.isSuccessful()) {
+      LOGGER.error("Failed to query " + MigrationConfig.KSQL_SERVER_URL + "/info");
+      System.exit(1);
+    }
+
+    return response.getResponse().getKsqlServiceId();
+  }
+}
--- ksqldb-tools/src/main/java/io/confluent/ksql/tools/migrations/commands/NewMigrationCommand.java
+++ ksqldb-tools/src/main/java/io/confluent/ksql/tools/migrations/commands/NewMigrationCommand.java
@@ -18,6 +18,14 @@ package io.confluent.ksql.tools.migrations.commands;
 import com.github.rvesse.airline.annotations.Arguments;
 import com.github.rvesse.airline.annotations.Command;
 import com.github.rvesse.airline.annotations.restrictions.Required;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Command(
     name = "new",
@@ -25,13 +33,62 @@ import com.github.rvesse.airline.annotations.restrictions.Required;
 )
 public class NewMigrationCommand extends BaseCommand {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(NewMigrationCommand.class);
+
   @Required
   @Arguments(description = "the project path to create the directory", title = "project-path")
   private String projectPath;
 
   @Override
+  @SuppressFBWarnings("DM_EXIT")
   public void run() {
-    throw new UnsupportedOperationException();
+    final long startTime = System.currentTimeMillis();
+
+    if (tryCreateDirectory(projectPath)
+        && tryCreateDirectory(projectPath + "/migrations")
+        && tryCreatePropertiesFile(projectPath + "/ksql-migrations.properties")) {
+      final long endTime = System.currentTimeMillis();
+      LOGGER.info("Migrations project directory created successfully (execution time "
+          + (endTime - startTime) / 1000.0 + "s)");
+    } else {
+      System.exit(1);
+    }
   }
 
+  private boolean tryCreateDirectory(final String path) {
+    final File directory = new File(path);
+
+    if (directory.exists() && directory.isDirectory()) {
+      LOGGER.warn(path + " already exists. Skipping directory creation.");
+      return true;
+    } else if (directory.exists() && !directory.isDirectory()) {
+      LOGGER.error(path + " already exists as a file. Cannot create directory.");
+      return false;
+    }
+
+    try {
+      LOGGER.info("Creating directory: " + path);
+      Files.createDirectories(Paths.get(path));
+    } catch (FileSystemException e) {
+      LOGGER.error("Permission denied: create directory " + path);
+      return false;
+    } catch (IOException e) {
+      LOGGER.error(String.format("Failed to create directory %s: %s", path, e.getMessage()));
+      return false;
+    }
+    return true;
+  }
+
+  private boolean tryCreatePropertiesFile(final String path) {
+    try {
+      LOGGER.info("Creating file: " + path);
+      if (!new File(path).createNewFile()) {
+        LOGGER.warn(path + " already exists. Skipping file creation.");
+      }
+    } catch (IOException e) {
+      LOGGER.error(String.format("Failed to create file %s: %s", path, e.getMessage()));
+      return false;
+    }
+    return true;
+  }
 }

a0b3d201c79dc1336123784a47775e8b93a1a303
Victoria Xia <victoria.xia@confluent.io>
2021-03-02 23:40:10 -0800

    test: clean up integ test

--- ksqldb-tools/src/test/java/io/confluent/ksql/tools/migrations/MigrationsTest.java
+++ ksqldb-tools/src/test/java/io/confluent/ksql/tools/migrations/MigrationsTest.java
@@ -85,6 +85,8 @@ public class MigrationsTest {
 
     configFilePath = Paths.get(testDir, MigrationsDirectoryUtil.MIGRATIONS_CONFIG_FILE).toString();
     initializeAndVerifyMetadataStreamAndTable(configFilePath);
+
+    waitForMetadataTableReady();
   }
 
   @AfterClass
@@ -93,8 +95,8 @@ public class MigrationsTest {
   }
 
   @Test
-  public void testApply() throws IOException {
-    // MigrationFile file
+  public void shouldApplyMigrationsAndDisplayInfo() throws IOException {
+    // Given:
     createMigrationFile(
         1,
         "foo FOO fO0",
@@ -108,42 +110,45 @@ public class MigrationsTest {
         "CREATE STREAM BAR (A STRING) WITH (KAFKA_TOPIC='BAR', PARTITIONS=1, VALUE_FORMAT='DELIMITED');"
     );
 
-    // This is needed to make sure that the table is fully done being created.
-    // It's a similar situation to https://github.com/confluentinc/ksql/issues/6249
-    assertThatEventually(
-        () -> makeKsqlQuery("SELECT * FROM migration_schema_versions WHERE VERSION_KEY='CURRENT';").size(),
-        is(1)
-    );
+    // When:
     final int status = MIGRATIONS_CLI.parse("--config-file", configFilePath, "apply", "-a").run();
-    assertThat(status, is(0));
 
+    // Then:
+    assertThat(status, is(0));
+    verifyMigrationsApplied();
+  }
+
+  private static void verifyMigrationsApplied() {
     // verify FOO and BAR were registered
     describeSource("FOO");
     describeSource("BAR");
 
-    // verify current
-    final List<StreamedRow> current = makeKsqlQuery("SELECT * FROM migration_schema_versions WHERE VERSION_KEY='CURRENT';");
-    assertThatEventually(() -> current.size(), is(2));
-    assertThatEventually(() -> current.get(1).getRow().get().getColumns().get(1), is("2"));
-    assertThatEventually(() -> current.get(1).getRow().get().getColumns().get(2), is("bar bar BAR"));
-    assertThatEventually(() -> current.get(1).getRow().get().getColumns().get(3), is("MIGRATED"));
-    assertThatEventually(() -> current.get(1).getRow().get().getColumns().get(7), is("1"));
-
     // verify version 1
-    final List<StreamedRow> version1 = makeKsqlQuery("SELECT * FROM migration_schema_versions WHERE VERSION_KEY='1';");
-    assertThatEventually(() -> version1.size(), is(2));
-    assertThatEventually(() -> version1.get(1).getRow().get().getColumns().get(1), is("1"));
-    assertThatEventually(() -> version1.get(1).getRow().get().getColumns().get(2), is("foo FOO fO0"));
-    assertThatEventually(() -> version1.get(1).getRow().get().getColumns().get(3), is("MIGRATED"));
-    assertThatEventually(() -> version1.get(1).getRow().get().getColumns().get(7), is("<none>"));
+    final List<StreamedRow> version1 = assertThatEventually(
+        () -> makeKsqlQuery("SELECT * FROM migration_schema_versions WHERE VERSION_KEY='1';"),
+        hasSize(2));
+    assertThat(version1.get(1).getRow().get().getColumns().get(1), is("1"));
+    assertThat(version1.get(1).getRow().get().getColumns().get(2), is("foo FOO fO0"));
+    assertThat(version1.get(1).getRow().get().getColumns().get(3), is("MIGRATED"));
+    assertThat(version1.get(1).getRow().get().getColumns().get(7), is("<none>"));
 
     // verify version 2
-    final List<StreamedRow> version2 = makeKsqlQuery("SELECT * FROM migration_schema_versions WHERE VERSION_KEY='CURRENT';");
-    assertThatEventually(() -> version2.size(), is(2));
-    assertThatEventually(() -> version2.get(1).getRow().get().getColumns().get(1), is("2"));
-    assertThatEventually(() -> version2.get(1).getRow().get().getColumns().get(2), is("bar bar BAR"));
-    assertThatEventually(() -> version2.get(1).getRow().get().getColumns().get(3), is("MIGRATED"));
-    assertThatEventually(() -> version2.get(1).getRow().get().getColumns().get(7), is("1"));
+    final List<StreamedRow> version2 = assertThatEventually(
+        () -> makeKsqlQuery("SELECT * FROM migration_schema_versions WHERE VERSION_KEY='CURRENT';"),
+        hasSize(2));
+    assertThat(version2.get(1).getRow().get().getColumns().get(1), is("2"));
+    assertThat(version2.get(1).getRow().get().getColumns().get(2), is("bar bar BAR"));
+    assertThat(version2.get(1).getRow().get().getColumns().get(3), is("MIGRATED"));
+    assertThat(version2.get(1).getRow().get().getColumns().get(7), is("1"));
+
+    // verify current
+    final List<StreamedRow> current =
+        makeKsqlQuery("SELECT * FROM migration_schema_versions WHERE VERSION_KEY='CURRENT';");
+    assertThat(current.size(), is(2));
+    assertThat(current.get(1).getRow().get().getColumns().get(1), is("2"));
+    assertThat(current.get(1).getRow().get().getColumns().get(2), is("bar bar BAR"));
+    assertThat(current.get(1).getRow().get().getColumns().get(3), is("MIGRATED"));
+    assertThat(current.get(1).getRow().get().getColumns().get(7), is("1"));
   }
 
   private static void createAndVerifyDirectoryStructure(final String testDir) throws Exception {
@@ -179,13 +184,13 @@ public class MigrationsTest {
 
     // verify metadata stream
     final SourceDescription streamDesc = describeSource("migration_events");
-    assertThatEventually(() -> streamDesc.getType(), is("STREAM"));
-    assertThatEventually(() -> streamDesc.getTopic(), is("default_ksql_migration_events"));
-    assertThatEventually(() -> streamDesc.getKeyFormat(), is("KAFKA"));
-    assertThatEventually(() -> streamDesc.getValueFormat(), is("JSON"));
-    assertThatEventually(() -> streamDesc.getPartitions(), is(1));
-    assertThatEventually(() -> streamDesc.getReplication(), is(1));
-    assertThatEventually(() -> streamDesc.getFields(), containsInAnyOrder(
+    assertThat(streamDesc.getType(), is("STREAM"));
+    assertThat(streamDesc.getTopic(), is("default_ksql_migration_events"));
+    assertThat(streamDesc.getKeyFormat(), is("KAFKA"));
+    assertThat(streamDesc.getValueFormat(), is("JSON"));
+    assertThat(streamDesc.getPartitions(), is(1));
+    assertThat(streamDesc.getReplication(), is(1));
+    assertThat(streamDesc.getFields(), containsInAnyOrder(
         fieldInfo("VERSION_KEY", "STRING", true),
         fieldInfo("VERSION", "STRING", false),
         fieldInfo("NAME", "STRING", false),
@@ -199,13 +204,13 @@ public class MigrationsTest {
 
     // verify metadata table
     final SourceDescription tableDesc = describeSource("migration_schema_versions");
-    assertThatEventually(() -> tableDesc.getType(), is("TABLE"));
-    assertThatEventually(() -> tableDesc.getTopic(), is("default_ksql_migration_schema_versions"));
-    assertThatEventually(() -> tableDesc.getKeyFormat(), is("KAFKA"));
-    assertThatEventually(() -> tableDesc.getValueFormat(), is("JSON"));
-    assertThatEventually(() -> tableDesc.getPartitions(), is(1));
-    assertThatEventually(() -> tableDesc.getReplication(), is(1));
-    assertThatEventually(() -> tableDesc.getFields(), containsInAnyOrder(
+    assertThat(tableDesc.getType(), is("TABLE"));
+    assertThat(tableDesc.getTopic(), is("default_ksql_migration_schema_versions"));
+    assertThat(tableDesc.getKeyFormat(), is("KAFKA"));
+    assertThat(tableDesc.getValueFormat(), is("JSON"));
+    assertThat(tableDesc.getPartitions(), is(1));
+    assertThat(tableDesc.getReplication(), is(1));
+    assertThat(tableDesc.getFields(), containsInAnyOrder(
         fieldInfo("VERSION_KEY", "STRING", true),
         fieldInfo("VERSION", "STRING", false),
         fieldInfo("NAME", "STRING", false),
@@ -219,10 +224,11 @@ public class MigrationsTest {
   }
 
   private static SourceDescription describeSource(final String name) {
-    final List<KsqlEntity> entities = makeKsqlRequest("DESCRIBE " + name + ";");
+    final List<KsqlEntity> entities = assertThatEventually(
+        () -> makeKsqlRequest("DESCRIBE " + name + ";"),
+        hasSize(1));
 
-    assertThatEventually(() -> entities, hasSize(1));
-    assertThatEventually(() -> entities.get(0), instanceOf(SourceDescriptionEntity.class));
+    assertThat(entities.get(0), instanceOf(SourceDescriptionEntity.class));
     SourceDescriptionEntity entity = (SourceDescriptionEntity) entities.get(0);
 
     return entity.getSourceDescription();
@@ -294,5 +300,14 @@ public class MigrationsTest {
     try (PrintWriter out = new PrintWriter(filePath, Charset.defaultCharset().name())) {
       out.println(content);
     }
+  }
+
+  private static void waitForMetadataTableReady() {
+    // This is needed to make sure that the table is fully done being created.
+    // It's a similar situation to https://github.com/confluentinc/ksql/issues/6249
+    assertThatEventually(
+        () -> makeKsqlQuery("SELECT * FROM migration_schema_versions WHERE VERSION_KEY='CURRENT';").size(),
+        is(1)
+    );
   }
 }

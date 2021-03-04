761659b4ff362750f3bc6a0dafa08dcdbc76a7f7
Walker Carlson <wcarlson@confluent.io>
2021-03-03 16:31:39 -0800

    update serde

--- ksqldb-rest-model/src/main/java/io/confluent/ksql/rest/entity/QueryDescription.java
+++ ksqldb-rest-model/src/main/java/io/confluent/ksql/rest/entity/QueryDescription.java
@@ -18,6 +18,7 @@ package io.confluent.ksql.rest.entity;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
@@ -26,7 +27,6 @@ import io.confluent.ksql.query.QueryError;
 import io.confluent.ksql.query.QueryId;
 import io.confluent.ksql.util.KsqlConstants.KsqlQueryStatus;
 import io.confluent.ksql.util.KsqlConstants.KsqlQueryType;
-import org.apache.kafka.streams.processor.TaskMetadata;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -36,6 +36,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+
+import org.apache.kafka.streams.processor.TaskMetadata;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class QueryDescription {

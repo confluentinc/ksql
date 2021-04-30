4ddac717b8edc3ce8c4ffc5be243195f9c4970e8
Tomasz Nguyen <tonguyen@confluent.io>
2021-04-30 12:30:14 +0100

    feat: add test to querypolicy to verify disabling

--- ksqldb-rest-app/src/test/java/io/confluent/ksql/logging/query/QueryAnonymizingRewritePolicyTest.java
+++ ksqldb-rest-app/src/test/java/io/confluent/ksql/logging/query/QueryAnonymizingRewritePolicyTest.java
@@ -6,6 +6,7 @@ import io.confluent.ksql.util.QueryAnonymizer;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.rewrite.RewriteAppender;
+import org.apache.log4j.spi.LoggingEvent;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,32 +17,27 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class QueryAnonymizingRewritePolicyTest {
+  @Mock public KsqlConfig config;
   private Logger logger = LogManager.getLogger(QueryAnonymizingRewritePolicyTest.class);
   private TestAppender testAppender = new TestAppender();
   private QueryAnonymizer anonymizer = new QueryAnonymizer();
-  @Mock
-  public KsqlConfig config;
-
 
   @Before
   public void setUp() throws Exception {
     // when
-    when(config.getBoolean(KsqlConfig.KSQL_QUERYANONYMIZER_ENABLED))
-        .thenReturn(true);
+    when(config.getBoolean(KsqlConfig.KSQL_QUERYANONYMIZER_ENABLED)).thenReturn(true);
     when(config.getString(KsqlConfig.KSQL_QUERYANONYMIZER_CLUSTER_NAMESPACE))
         .thenReturn("cathouse.org.meowcluster");
-
 
     final RewriteAppender rewriteAppender = new RewriteAppender();
     rewriteAppender.setRewritePolicy(new QueryAnonymizingRewritePolicy(config));
     rewriteAppender.addAppender(testAppender);
     LogManager.getLogger(QueryAnonymizingRewritePolicyTest.class).addAppender(rewriteAppender);
-
   }
 
   @Test
@@ -49,8 +45,8 @@ public class QueryAnonymizingRewritePolicyTest {
     // when
     logger.error(ImmutableMap.of("message", "cat", "query", "CREATE TABLE CAT;"));
     // then
-    final ImmutableMap<String, String> message = (ImmutableMap<String, String>) testAppender.getLog()
-        .get(0).getMessage();
+    final ImmutableMap<String, String> message =
+        (ImmutableMap<String, String>) testAppender.getLog().get(0).getMessage();
     assertEquals(message.get("query"), anonymizer.anonymize("CREATE TABLE CAT;"));
   }
 
@@ -59,9 +55,26 @@ public class QueryAnonymizingRewritePolicyTest {
     // when
     logger.error(ImmutableMap.of("message", "cat", "query", "CREATE TABLE CAT;"));
     // then
-    final ImmutableMap<String, String> message = (ImmutableMap<String, String>) testAppender.getLog()
-        .get(0).getMessage();
+    final ImmutableMap<String, String> message =
+        (ImmutableMap<String, String>) testAppender.getLog().get(0).getMessage();
     assertThat(message.get("structuralGUID"), not(isEmptyOrNullString()));
     assertThat(message.get("queryGUID"), not(isEmptyOrNullString()));
+  }
+
+  @Test
+  public void shouldPassThroughIfAnonymizerDisabled() {
+    //    given a config with disabled rewrite policy
+    when(config.getBoolean(KsqlConfig.KSQL_QUERYANONYMIZER_ENABLED)).thenReturn(false);
+    final QueryAnonymizingRewritePolicy rewritePolicy = new QueryAnonymizingRewritePolicy(config);
+    //    when rewriting a LoggingEvent
+    final LoggingEvent loggingEvent = mock(LoggingEvent.class);
+    when(loggingEvent.getLogger()).thenReturn(LogManager.getLogger("cattest"));
+    final ImmutableMap<String, String> msg =
+        ImmutableMap.of("message", "cat", "query", "CREATE TABLE CAT;");
+    when(loggingEvent.getMessage()).thenReturn(msg);
+    //    then we get nothign changed
+    final ImmutableMap<String, String> result =
+        (ImmutableMap<String, String>) rewritePolicy.rewrite(loggingEvent).getMessage();
+    assertEquals(msg.get("query"), result.get("query"));
   }
 }

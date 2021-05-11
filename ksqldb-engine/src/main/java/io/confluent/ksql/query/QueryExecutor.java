1c89ffdb28f3fb437bef0826704460c7b3429a86
Walker Carlson <wcarlson@confluent.io>
2021-05-11 14:45:05 -0700

    improve sources

--- ksqldb-engine/src/main/java/io/confluent/ksql/query/QueryExecutor.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/query/QueryExecutor.java
@@ -284,7 +284,7 @@ final class QueryExecutor {
     final boolean useBinPacked = true;
     if (useBinPacked) {
       BinPackedStreamsMetadata binPackedStreamsMetadata = getStream(sources);
-      binPackedStreamsMetadata.addQuery(classifier, streamsProperties, sources);
+      binPackedStreamsMetadata.addQuery(classifier, streamsProperties, sources, queryId, listener);
       return new BinPackedPersistentQueryMetadataImpl(
           statementText,
           querySchema.logicalSchema(),

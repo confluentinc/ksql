c0436b465ae7ffc8224f85f0af8496d1629180fc
Victoria Xia <victoria.xia@confluent.io>
2021-05-20 18:02:45 -0400

    fix: filter by query status

--- ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/server/execution/RemoteHostExecutorTest.java
+++ ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/server/execution/RemoteHostExecutorTest.java
@@ -1,5 +1,19 @@
-package io.confluent.ksql.rest.server.execution;
+/*
+ * Copyright 2021 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"; you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 
+package io.confluent.ksql.rest.server.execution;
 
 import io.confluent.ksql.engine.KsqlEngine;
 import io.confluent.ksql.parser.tree.DescribeStreams;
@@ -38,7 +52,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class RemoteHostExecutorTest {

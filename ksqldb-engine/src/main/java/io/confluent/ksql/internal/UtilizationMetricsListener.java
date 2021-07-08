5ab2fa52147e5f2928a08d7c22a3a8d18b54ef13
Leah Thomas <lthomas@confluent.io>
2021-07-08 13:17:42 -0500

    task level disk usage + cleanup

--- ksqldb-engine/src/main/java/io/confluent/ksql/internal/UtilizationMetricsListener.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/internal/UtilizationMetricsListener.java
@@ -17,17 +17,10 @@ package io.confluent.ksql.internal;
 
 import io.confluent.ksql.engine.QueryEventListener;
 import io.confluent.ksql.metastore.MetaStore;
+import io.confluent.ksql.query.QueryId;
 import io.confluent.ksql.services.ServiceContext;
 import io.confluent.ksql.util.KsqlConfig;
 import io.confluent.ksql.util.QueryMetadata;
-
-import java.io.File;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.apache.kafka.common.Metric;
 import org.apache.kafka.common.utils.Time;
 import org.apache.kafka.streams.KafkaStreams;
@@ -35,51 +28,57 @@ import org.apache.kafka.streams.StreamsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class UtilizationMetricsListener implements Runnable, QueryEventListener {
+import java.io.File;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-  private final List<KafkaStreams> kafkaStreams;
+public class UtilizationMetricsListener implements Runnable, QueryEventListener {
+  private final Map<QueryId, KafkaStreams> kafkaStreams;
   private final List<File> streamsDirectories;
   private final Logger logger = LoggerFactory.getLogger(UtilizationMetricsListener.class);
   private final List<String> metrics;
   private final Time time;
   private long lastSampleTime;
-  private File baseDir;
+  private final File baseDir;
 
   public UtilizationMetricsListener(final KsqlConfig config) {
-    this.kafkaStreams = new ArrayList<>();
+    this.kafkaStreams = new HashMap<>();
+    this.metrics = new ArrayList<>();
     this.streamsDirectories = new ArrayList<>();
-    this.metrics = new LinkedList<>();
-    time = Time.SYSTEM;
-    lastSampleTime = time.milliseconds();
     this.baseDir = new File(config.getKsqlStreamConfigProps()
       .getOrDefault(
         StreamsConfig.STATE_DIR_CONFIG,
         StreamsConfig.configDef().defaultValues().get(StreamsConfig.STATE_DIR_CONFIG))
       .toString());
+    time = Time.SYSTEM;
   }
 
-  // for testing
-  public UtilizationMetricsListener(final List<KafkaStreams> streams, final Time time, final long lastSample) {
-    this.kafkaStreams = streams;
-    this.metrics = new LinkedList<>();
-    this.streamsDirectories = new ArrayList<>();
-    this.baseDir = new File("");
-    this.time = time;
+    // for testing
+  public UtilizationMetricsListener(final HashMap<QueryId, KafkaStreams> kafkaStreams, final List<File> streamsDirectories, final File baseDir) {
+    this.kafkaStreams = kafkaStreams;
+    this.metrics = new ArrayList<>();
+    this.streamsDirectories = streamsDirectories;
+    this.baseDir = baseDir;
+    time = Time.SYSTEM;
   }
 
   @Override
   public void onCreate(
-      final ServiceContext serviceContext,
-      final MetaStore metaStore,
-      final QueryMetadata queryMetadata) {
-    kafkaStreams.add(queryMetadata.getKafkaStreams());
+    final ServiceContext serviceContext,
+    final MetaStore metaStore,
+    final QueryMetadata queryMetadata) {
+    kafkaStreams.put(queryMetadata.getQueryId(), queryMetadata.getKafkaStreams());
     streamsDirectories.add(new File(baseDir, queryMetadata.getQueryApplicationId()));
   }
 
   @Override
   public void onDeregister(final QueryMetadata query) {
-    final KafkaStreams streams = query.getKafkaStreams();
-    kafkaStreams.remove(streams);
+    kafkaStreams.remove(query.getQueryId());
+    streamsDirectories.remove(new File(baseDir, query.getQueryApplicationId()));
   }
 
   @Override
@@ -87,35 +86,39 @@ public class UtilizationMetricsListener implements Runnable, QueryEventListener 
     logger.info("Reporting Observability Metrics");
     final Long currentTime = time.milliseconds();
     // here is where we would report metrics
+    logger.info("Reporting CSU thread level metrics");
     nodeDiskUsage();
     taskDiskUsage();
     lastSampleTime = currentTime;
   }
 
-    private double percentage(final double b, final double w) {
-        return Math.round((b / w) * 100);
-    }
+  private double percentage(final double b, final double w) {
+    return Math.round((b / w) * 100);
+  }
 
-    final void nodeDiskUsage() {
-        for (File f : streamsDirectories) {
-            final long freeSpace = f.getFreeSpace();
-            final long totalSpace = f.getTotalSpace();
-            final double percFree = percentage((double) freeSpace, (double) totalSpace);
-            logger.info("The disk usage for {} is {}", f.getName(), freeSpace);
-            logger.info("The % disk space free for {} is {}%", f.getName(), percFree);
-        }
+  public void nodeDiskUsage() {
+    for (File f : streamsDirectories) {
+      final long freeSpace = f.getFreeSpace();
+      final long totalSpace = f.getTotalSpace();
+      final double percFree = percentage((double) freeSpace, (double) totalSpace);
+      logger.info("The disk usage for {} is {}", f.getName(), freeSpace);
+      logger.info("The % disk space free for {} is {}%", f.getName(), percFree);
     }
+  }
 
-    final void taskDiskUsage() {
-        for (KafkaStreams streams : kafkaStreams) {
-            logger.info("Recording task level disk usage for {}", streams);
-            final List<Metric> fileSizePerTask = streams.metrics().values().stream()
-                .filter(m -> m.metricName().name().equals("total-sst-files-size"))
-                .collect(Collectors.toList());
-            for (Metric m : fileSizePerTask) {
-                final BigInteger usage = (BigInteger) m.metricValue();
-                logger.info("Disk usage for {} is {}", m.metricName().tags().getOrDefault("task-id", ""), usage);
-            }
-        }
+  public void taskDiskUsage() {
+    for (Map.Entry<QueryId, KafkaStreams> streams : kafkaStreams.entrySet()) {
+      final List<Metric> fileSizePerTask = streams.getValue().metrics().values().stream()
+        .filter(m -> m.metricName().name().equals("total-sst-files-size"))
+        .collect(Collectors.toList());
+      BigInteger totalDiskUsage = BigInteger.valueOf(0);
+      for (Metric m : fileSizePerTask) {
+        logger.info("Recording task level disk usage for {}", streams.getKey());
+        final BigInteger usage = (BigInteger) m.metricValue();
+        logger.info("Disk usage for task {} is {}", m.metricName().tags().getOrDefault("task-id", ""), usage);
+        totalDiskUsage.add(usage);
+      }
+      logger.info("Total disk usage for query {} is {}",streams.getKey(), totalDiskUsage);
     }
+  }
 }
--- ksqldb-engine/src/test/java/io/confluent/ksql/internal/UtilizationMetricsListenerTest.java
+++ ksqldb-engine/src/test/java/io/confluent/ksql/internal/UtilizationMetricsListenerTest.java
@@ -1,22 +1,78 @@
 package io.confluent.ksql.internal;
 
+import com.google.common.collect.ImmutableMap;
+import io.confluent.ksql.logging.query.TestAppender;
+import io.confluent.ksql.metastore.MetaStore;
+import io.confluent.ksql.query.QueryId;
+import io.confluent.ksql.services.ServiceContext;
+import io.confluent.ksql.util.QueryMetadata;
+import org.apache.kafka.common.MetricName;
+import org.apache.kafka.common.metrics.KafkaMetric;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UtilizationMetricsListenerTest {
 
-  List<KafkaStreams> streamsList;
+  List<File> directories;
+  HashMap<QueryId, KafkaStreams> streams;
+  private UtilizationMetricsListener listener;
 
+  @Mock
+  private QueryMetadata query;
+  @Mock
+  private ServiceContext serviceContext;
+  @Mock
+  private MetaStore metaStore;
+  @Mock
+  private KafkaStreams s1;
+  @Mock
+  private KafkaMetric sst_1;
+  @Mock
+  private KafkaMetric sst_2;
 
   @Before
   public void setUp() {
-    streamsList = new ArrayList<>();
+    when(query.getQueryApplicationId()).thenReturn("app-id");
+
+    directories = new ArrayList<>();
+    streams = new HashMap<>();
+    streams.put(new QueryId("blah"), s1);
+    listener = new UtilizationMetricsListener(streams, directories, new File("tmp/cat/"));
   }
 
+  @Test
+  public void shouldAddStateStoreOnCreation() {
+    // When:
+    listener.onCreate(serviceContext, metaStore, query);
+
+    // Then:
+    assertTrue(directories.contains(new File("tmp/cat/app-id")));
+  }
+
+  @Test
+  public void shouldRemoveStateStoreOnTermination() {
+    // When:
+    listener.onCreate(serviceContext, metaStore, query);
+    listener.onDeregister(query);
+
+    // Then:
+    assertTrue(directories.isEmpty());
+  }
 }

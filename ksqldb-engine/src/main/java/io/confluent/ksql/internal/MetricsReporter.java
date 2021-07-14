6a783f73dbd971f564b03572203b37d15e7ce3eb
Bruno Cadonna <bruno@confluent.io>
2021-07-14 10:52:13 +0200

    Fix checkstyle issues

--- ksqldb-engine/src/main/java/io/confluent/ksql/internal/MetricsReporter.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/internal/MetricsReporter.java
@@ -35,78 +35,78 @@ import java.util.function.Supplier;
  */
 public interface MetricsReporter extends Closeable, Configurable {
 
-    /**
-     * A data point that should be reported.
-     */
-    class DataPoint {
-        private final String name;
-        private final Instant time;
-        private final Object value;
-        private final Map<String, String> tags;
+  /**
+   * A data point that should be reported.
+   */
+  class DataPoint {
+    private final String name;
+    private final Instant time;
+    private final Object value;
+    private final Map<String, String> tags;
 
-        public DataPoint(final Instant time, final String name, final Object value) {
-            this(time, name, value, Collections.emptyMap());
-        }
-
-        public DataPoint(
-                final Instant time,
-                final String name,
-                final Object value,
-                final Map<String, String> tags
-        ) {
-            this.name = Objects.requireNonNull(name, "name");
-            this.time = time;
-            this.value = value;
-            this.tags = ImmutableMap.copyOf(Objects.requireNonNull(tags, "tags"));
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public Instant getTime() {
-            return time;
-        }
-
-        public Object getValue() {
-            return value;
-        }
-
-        public Map<String, String> getTags() {
-            return tags;
-        }
-
-        @Override
-        public boolean equals(final Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            final DataPoint dataPoint = (DataPoint) o;
-            return time == dataPoint.time
-                    && value == dataPoint.value
-                    && Objects.equals(name, dataPoint.name)
-                    && Objects.equals(tags, dataPoint.tags);
-        }
-
-        @Override
-        public String toString() {
-            return "DataPoint{" + "name='" + name + '\''
-                    + ", time=" + time + ", value=" + value + ", tags=" + tags + '}';
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name, time, value, tags);
-        }
+    public DataPoint(final Instant time, final String name, final Object value) {
+      this(time, name, value, Collections.emptyMap());
     }
 
-    /**
-     * Reports a list of data points.
-     *
-     * @param dataPointSupplier supplier of the list of data points
-     */
-    void report(final Supplier<List<DataPoint>> dataPointSupplier);
+    public DataPoint(
+        final Instant time,
+        final String name,
+        final Object value,
+        final Map<String, String> tags
+    ) {
+      this.name = Objects.requireNonNull(name, "name");
+      this.time = time;
+      this.value = value;
+      this.tags = ImmutableMap.copyOf(Objects.requireNonNull(tags, "tags"));
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public Instant getTime() {
+      return time;
+    }
+
+    public Object getValue() {
+      return value;
+    }
+
+    public Map<String, String> getTags() {
+      return tags;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      final DataPoint dataPoint = (DataPoint) o;
+      return time == dataPoint.time
+          && value == dataPoint.value
+          && Objects.equals(name, dataPoint.name)
+          && Objects.equals(tags, dataPoint.tags);
+    }
+
+    @Override
+    public String toString() {
+      return "DataPoint{" + "name='" + name + '\''
+          + ", time=" + time + ", value=" + value + ", tags=" + tags + '}';
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(name, time, value, tags);
+    }
+  }
+
+  /**
+   * Reports a list of data points.
+   *
+   * @param dataPointSupplier supplier of the list of data points
+   */
+  void report(Supplier<List<DataPoint>> dataPointSupplier);
 }

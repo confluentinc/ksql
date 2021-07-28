09bc4979b6f901ddf2db2e0907a0e5c2af0bc92b
patrick <psullivan@confluent.io>
2021-07-27 23:02:15 -0400

    added BYTES to LPAD and RPAD UDFs

--- ksqldb-engine/src/main/java/io/confluent/ksql/function/udf/string/RPad.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/function/udf/string/RPad.java
@@ -18,14 +18,17 @@ import io.confluent.ksql.function.FunctionCategory;
 import io.confluent.ksql.function.udf.Udf;
 import io.confluent.ksql.function.udf.UdfDescription;
 import io.confluent.ksql.function.udf.UdfParameter;
+import io.confluent.ksql.util.BytesUtils;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 @UdfDescription(
     name = "RPad",
     category = FunctionCategory.STRING,
-    description = "Pads the input string, starting from the end, with the specified padding"
-        + " string until the target length is reached. If the input string is longer than the"
-        + " specified target length it will be truncated. If the padding string is empty or"
-        + " NULL, or the target length is negative, then NULL is returned.")
+    description = "Pads the input string or bytes, starting from the end, with the specified"
+        + " padding string until the target length is reached. If the input string or bytes are"
+        + " longer than the specified target length it will be truncated. If the padding string or"
+        + " bytes are empty or NULL, or the target length is negative, then NULL is returned.")
 public class RPad {
 
   @Udf
@@ -48,5 +51,42 @@ public class RPad {
     }
     sb.setLength(targetLen);
     return sb.toString();
+  }
+
+  @Udf
+  public ByteBuffer rpad(
+      @UdfParameter(description = "Bytes to be padded") final ByteBuffer input,
+      @UdfParameter(description = "Target length") final Integer targetLen,
+      @UdfParameter(description = "Padding bytes") final ByteBuffer padding) {
+
+    if (input == null) {
+      return null;
+    }
+    if (padding == null
+        || BytesUtils.getByteArray(padding).length == 0
+        || targetLen == null
+        || targetLen < 0) {
+      return null;
+    }
+
+    final byte[] start = BytesUtils.getByteArray(input);
+
+    if (start.length > targetLen) {
+      return ByteBuffer.wrap(Arrays.copyOfRange(start, 0, targetLen));
+    }
+
+    final byte[] padded = new byte[targetLen];
+    final byte[] paddingArray = BytesUtils.getByteArray(padding);
+
+    for (int i = 0; i < targetLen; i++) {
+      if (i < start.length) {
+        padded[i] = start[i];
+      } else {
+        final int paddingIndex = (i - start.length) % paddingArray.length;
+        padded[i] = paddingArray[paddingIndex];
+      }
+
+    }
+    return ByteBuffer.wrap(padded);
   }
 }

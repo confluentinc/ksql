b3944b1c4cc532868861613e9c9a173729bb72f4
patrick <psullivan@confluent.io>
2021-07-29 11:34:02 -0400

    added bytes functionality to replace

--- ksqldb-engine/src/main/java/io/confluent/ksql/function/udf/string/Replace.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/function/udf/string/Replace.java
@@ -19,14 +19,17 @@ import io.confluent.ksql.function.FunctionCategory;
 import io.confluent.ksql.function.udf.Udf;
 import io.confluent.ksql.function.udf.UdfDescription;
 import io.confluent.ksql.function.udf.UdfParameter;
+import io.confluent.ksql.util.BytesUtils;
 import io.confluent.ksql.util.KsqlConstants;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 @SuppressWarnings("unused") // Invoked via reflection.
 @UdfDescription(
     name = "replace",
     category = FunctionCategory.STRING,
     author = KsqlConstants.CONFLUENT_AUTHOR,
-    description = "Replaces all occurances of a substring in a string with a new substring."
+    description = "Replaces all occurences of a substring in a string with a new substring."
 )
 public class Replace {
   @Udf(description = "Returns a new string with all occurences of oldStr in str with newStr")
@@ -44,5 +47,47 @@ public class Replace {
     }
 
     return str.replace(oldStr, newStr);
+  }
+
+  @Udf
+  public ByteBuffer replace(
+      @UdfParameter(
+          description = "The source bytes."
+              + " If null, then function returns null.") final ByteBuffer source,
+      @UdfParameter(
+          description = "The byte subset to replace."
+              + " If null, then function returns null.") final ByteBuffer toReplace,
+      @UdfParameter(
+          description = "The bytes to replace the old bytes with."
+              + " If null, then function returns null.") final ByteBuffer replaceWith) {
+
+    if (source == null || toReplace == null || replaceWith == null) {
+      return null;
+    }
+
+    //if replaceWith.length == toReplace.length, DON'T create a new array
+    //else, create an arraylist. Create another arraylist2 and a value matchingBytesNum.
+    //populate arraylist2 as long as the next byte matches the next byte in our toReplace[matchingBytesNum]
+    //if matchingBytesNum reaches the length of toReplace, we have a matching subbytes and append
+    //to our 1st arraylist the replaceWith bytes. If they diverge, just dump the original arraylist
+    final byte[] sourceArr = BytesUtils.getByteArray(source);
+    final byte[] toReplaceArr = BytesUtils.getByteArray(toReplace);
+    final byte[] replaceWithArr = BytesUtils.getByteArray(replaceWith);
+
+
+    final String sourceString =
+        new String(sourceArr, 0, sourceArr.length, StandardCharsets.US_ASCII);
+    final String toReplaceString =
+        new String(toReplaceArr, 0, toReplaceArr.length, StandardCharsets.US_ASCII);
+    final String replaceWithString
+        = new String(replaceWithArr, 0, replaceWithArr.length, StandardCharsets.US_ASCII);
+
+
+    final String replacedString = sourceString.replace(toReplaceString, replaceWithString);
+
+    final byte[] replacedStringBytes = replacedString.getBytes(StandardCharsets.US_ASCII);
+
+    return ByteBuffer.wrap(replacedStringBytes);
+
   }
 }

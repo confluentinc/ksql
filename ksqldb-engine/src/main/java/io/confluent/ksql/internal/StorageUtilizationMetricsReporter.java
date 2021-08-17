afa0ba2c92ea8ca2f91784d875a5b07537eb7db4
Leah Thomas <lthomas@confluent.io>
2021-08-17 11:18:47 -0700

    spotbugs

--- ksqldb-engine/src/main/java/io/confluent/ksql/internal/StorageUtilizationMetricsReporter.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/internal/StorageUtilizationMetricsReporter.java
@@ -53,9 +53,10 @@ public class StorageUtilizationMetricsReporter implements MetricsReporter {
     this(MetricCollectors.getMetrics());
   }
 
+  @VisibleForTesting
   public StorageUtilizationMetricsReporter(final Metrics metricRegistry) {
     this.metricsSeen = new HashMap<>();
-    this.metricRegistry = metricRegistry;
+    this.metricRegistry = Objects.requireNonNull(metricRegistry);
   }
 
   @Override
@@ -277,18 +278,6 @@ public class StorageUtilizationMetricsReporter implements MetricsReporter {
         newValue = newValue.add(value);
       }
       return newValue;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-      if (this == o) {
-        return true;
-      }
-      if (o == null || getClass() != o.getClass()) {
-        return false;
-      }
-      final TaskStorageMetric storageMetric = (TaskStorageMetric) o;
-      return Objects.equals(metricName, storageMetric.metricName);
     }
   }
 

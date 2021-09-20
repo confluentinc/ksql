cc5c02b8a76ef1141fb01664b5ee440b1aceb920
Vicky Papavasileiou <vpapavas@users.noreply.github.com>
2021-09-20 21:54:03 +0100

    test: add ignore to flaky test (PullQueryMetricsFunctionalTest) (#8153)
    * ignore flaky test
    
    * dummy

--- ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/integration/PullQueryMetricsFunctionalTest.java
+++ ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/integration/PullQueryMetricsFunctionalTest.java
@@ -44,11 +44,13 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.junit.rules.Timeout;
 
+@Ignore 
 public class PullQueryMetricsFunctionalTest {
 
   private static final PageViewDataProvider PAGE_VIEWS_PROVIDER = new PageViewDataProvider();

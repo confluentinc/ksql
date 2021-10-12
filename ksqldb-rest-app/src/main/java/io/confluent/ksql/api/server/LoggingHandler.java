69bf46d28e3e4290f7745c0336e59ddde7ea0828
Almog Gavra <almog@confluent.io>
2021-10-12 13:29:17 -0700

    feat: add configurations around endpoint logging (#8249)

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/api/server/LoggingHandler.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/api/server/LoggingHandler.java
@@ -19,6 +19,7 @@ import static java.util.Objects.requireNonNull;
 
 import com.google.common.annotations.VisibleForTesting;
 import io.confluent.ksql.api.auth.ApiUser;
+import io.confluent.ksql.rest.server.KsqlRestConfig;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpVersion;
@@ -27,6 +28,7 @@ import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.impl.Utils;
 import java.time.Clock;
 import java.util.Optional;
+import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,6 +40,8 @@ public class LoggingHandler implements Handler<RoutingContext> {
   private final Logger logger;
   private final Clock clock;
   private final LoggingRateLimiter loggingRateLimiter;
+  private final Optional<Pattern> endpointFilter;
+  private final boolean enableQueryLogging;
 
   public LoggingHandler(final Server server, final LoggingRateLimiter loggingRateLimiter) {
     this(server, loggingRateLimiter, LOG, Clock.systemUTC());
@@ -53,6 +57,24 @@ public class LoggingHandler implements Handler<RoutingContext> {
     this.loggingRateLimiter = requireNonNull(loggingRateLimiter);
     this.logger = logger;
     this.clock = clock;
+
+    final String endpointRegex = server.getConfig()
+        .getString(KsqlRestConfig.KSQL_ENDPOINT_LOGGING_IGNORED_PATHS_REGEX_CONFIG);
+
+    Optional<Pattern> endpointFilter;
+    try {
+      endpointFilter = endpointRegex.isEmpty()
+          ? Optional.empty()
+          : Optional.of(Pattern.compile(endpointRegex));
+    } catch (final Exception e) {
+      LOG.warn("Could not set up regex for Logging Handler", e);
+      endpointFilter = Optional.empty();
+    }
+    this.endpointFilter = endpointFilter;
+
+    this.enableQueryLogging = server
+        .getConfig()
+        .getBoolean(KsqlRestConfig.KSQL_ENDPOINT_LOGGING_LOG_QUERIES_CONFIG);
   }
 
   @Override
@@ -66,7 +88,14 @@ public class LoggingHandler implements Handler<RoutingContext> {
       final long contentLength = routingContext.request().response().bytesWritten();
       final HttpVersion version = routingContext.request().version();
       final HttpMethod method = routingContext.request().method();
-      final String uri = routingContext.request().uri();
+      final String uri = enableQueryLogging
+          ? routingContext.request().uri()
+          : routingContext.request().path();
+
+      if (endpointFilter.isPresent() && endpointFilter.get().matcher(uri).matches()) {
+        return;
+      }
+
       final long requestBodyLength = routingContext.request().bytesRead();
       final String versionFormatted;
       switch (version) {

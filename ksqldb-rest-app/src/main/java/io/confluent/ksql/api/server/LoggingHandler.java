59ab3be8d8e385a417f334743620ef2f216912d7
Almog Gavra <almog@confluent.io>
2021-10-11 17:26:03 -0700

    feat: add configurations around endpoint logging

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/api/server/LoggingHandler.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/api/server/LoggingHandler.java
@@ -19,14 +19,18 @@ import static java.util.Objects.requireNonNull;
 
 import com.google.common.annotations.VisibleForTesting;
 import io.confluent.ksql.api.auth.ApiUser;
+import io.confluent.ksql.rest.server.KsqlRestConfig;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpVersion;
 import io.vertx.core.net.SocketAddress;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.impl.Utils;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.Clock;
 import java.util.Optional;
+import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,6 +42,8 @@ public class LoggingHandler implements Handler<RoutingContext> {
   private final Logger logger;
   private final Clock clock;
   private final LoggingRateLimiter loggingRateLimiter;
+  private final Optional<Pattern> endpointFilter;
+  private final boolean enableQueryLogging;
 
   public LoggingHandler(final Server server, final LoggingRateLimiter loggingRateLimiter) {
     this(server, loggingRateLimiter, LOG, Clock.systemUTC());
@@ -53,6 +59,24 @@ public class LoggingHandler implements Handler<RoutingContext> {
     this.loggingRateLimiter = requireNonNull(loggingRateLimiter);
     this.logger = logger;
     this.clock = clock;
+
+    final String endpointRegex = server.getConfig()
+        .getString(KsqlRestConfig.KSQL_ENDPOINT_LOGGING_FILTER_CONFIG);
+
+    Optional<Pattern> endpointFilter;
+    try {
+      endpointFilter = endpointRegex.isEmpty()
+          ? Optional.empty()
+          : Optional.of(Pattern.compile(endpointRegex));
+    } catch (final Exception e) {
+      LOG.warn("Could not set up regex for Logging Handler", e);
+      endpointFilter = Optional.empty();
+    }
+    this.endpointFilter = endpointFilter;
+
+    this.enableQueryLogging = server
+        .getConfig()
+        .getBoolean(KsqlRestConfig.KSQL_ENDPOINT_LOGGING_LOG_QUERIES_CONFIG);
   }
 
   @Override
@@ -66,7 +90,33 @@ public class LoggingHandler implements Handler<RoutingContext> {
       final long contentLength = routingContext.request().response().bytesWritten();
       final HttpVersion version = routingContext.request().version();
       final HttpMethod method = routingContext.request().method();
-      final String uri = routingContext.request().uri();
+      String uri;
+      if (enableQueryLogging) {
+        uri = routingContext.request().uri();
+      } else {
+        try {
+          // do not log the query that was sent with the URI, since this may contain
+          // sensitive user information
+          final URI uriWithQueryString = new URI(routingContext.request().uri());
+          uri = new URI(
+              uriWithQueryString.getScheme(),
+              uriWithQueryString.getAuthority(),
+              uriWithQueryString.getPath(),
+              null,
+              uriWithQueryString.getFragment()
+          ).toString();
+        } catch (URISyntaxException e) {
+          // log this at DEBUG level to avoid logging sensitive information at
+          // the INFO level or above
+          LOG.debug("Could not parse URI: {}", routingContext.request().uri(), e);
+          uri = "invalid";
+        }
+      }
+
+      if (endpointFilter.isPresent() && endpointFilter.get().matcher(uri).matches()) {
+        return;
+      }
+
       final long requestBodyLength = routingContext.request().bytesRead();
       final String versionFormatted;
       switch (version) {

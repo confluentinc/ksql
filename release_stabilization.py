3bc0d3168bf5ef2172ed9a3338832a7c67807212
Natea Eshetu Beshada <nbeshada+git@confluent.io>
2021-10-14 11:02:24 -0700

    chore: add doc string

--- release_stabilization.py
+++ release_stabilization.py
@@ -3,5 +3,7 @@ class Callbacks:
     def __init__(self, working_dir, leaf, dry_run):
         self.leaf = leaf
 
+    """This is a callback to Confluent's cloud release tooling,
+    and allows us to have consistent versioning"""
     def version_as_leaf(self):
         return self.leaf == 'cc-docker-ksql'

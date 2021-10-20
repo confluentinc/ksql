ff277ba76d05ffe5c9d9f0276d345b08b12271cb
Walker Carlson <18128741+wcarlson5@users.noreply.github.com>
2021-10-20 10:15:29 -0500

    fix: change metrics tag (#8268)
    (cherry picked from commit caded5ae61260df05b518904740cbab17d934edb)

--- ksqldb-engine/src/test/java/io/confluent/ksql/internal/QueryStateMetricsReportingListenerTest.java
+++ ksqldb-engine/src/test/java/io/confluent/ksql/internal/QueryStateMetricsReportingListenerTest.java
@@ -53,6 +53,7 @@ public class QueryStateMetricsReportingListenerTest {
   private static final MetricName METRIC_NAME_2 =
       new MetricName("dylan", "g1", "d1", ImmutableMap.of());
   private static final QueryId QUERY_ID = new QueryId("foo");
+  private static final String TAG = "_confluent-ksql-" + "some-prefix-" + "query_" + QUERY_ID.toString();
 
   @Mock
   private Metrics metrics;
@@ -73,7 +74,7 @@ public class QueryStateMetricsReportingListenerTest {
         .thenReturn(METRIC_NAME_2);
     when(query.getQueryId()).thenReturn(QUERY_ID);
 
-    listener = new QueryStateMetricsReportingListener(metrics, "");
+    listener = new QueryStateMetricsReportingListener(metrics, "some-prefix-");
   }
 
   @Test
@@ -87,12 +88,12 @@ public class QueryStateMetricsReportingListenerTest {
     listener.onCreate(serviceContext, metaStore, query);
 
     // Then:
-    verify(metrics).metricName("query-status", "ksql-queries",
+    verify(metrics).metricName("query-status", "some-prefix-ksql-queries",
         "The current status of the given query.",
-        ImmutableMap.of("status", QUERY_ID.toString()));
-    verify(metrics).metricName("error-status", "ksql-queries",
+        ImmutableMap.of("status", TAG));
+    verify(metrics).metricName("error-status", "some-prefix-ksql-queries",
         "The current error status of the given query, if the state is in ERROR state",
-        ImmutableMap.of("status", QUERY_ID.toString()));
+        ImmutableMap.of("status", TAG));
 
     verify(metrics).addMetric(eq(METRIC_NAME_1), isA(Gauge.class));
     verify(metrics).addMetric(eq(METRIC_NAME_2), isA(Gauge.class));
@@ -132,10 +133,10 @@ public class QueryStateMetricsReportingListenerTest {
     // Then:
     verify(metrics).metricName("query-status", groupPrefix + "ksql-queries",
         "The current status of the given query.",
-        ImmutableMap.of("status", QUERY_ID.toString()));
+        ImmutableMap.of("status", TAG));
     verify(metrics).metricName("error-status", groupPrefix + "ksql-queries",
         "The current error status of the given query, if the state is in ERROR state",
-        ImmutableMap.of("status", QUERY_ID.toString()));
+        ImmutableMap.of("status", TAG));
   }
 
   @Test

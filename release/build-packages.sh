d20f8847a3f051e912947d284929def2531833e5
Natea Eshetu Beshada <nbeshada+git@confluent.io>
2021-11-11 04:11:32 -0500

    fix: add more scripts

---
+++ release/build-packages.sh
@@ -0,0 +1,175 @@
+ #!/usr/bin/env bash
+# Build logic for KSQLDB & its OS packages
+
+set -o pipefail -o nounset -o errexit
+
+function error {
+    echo "ERROR: ${*}"
+    exit 1
+}
+
+WORKSPACE=""
+FULL_VERSION=""
+UPSTREAM_VERSION=""
+DOCKER_REGISTRY=""
+BUILD_JAR="false"
+MAVEN_SKIP_TESTS="false"
+while [[ "${#}" -gt 0 ]]; do
+    arg="${1}"
+    case "${arg}" in
+	    -w|--workspace)
+            WORKSPACE="${2}"
+            [ -d "${WORKSPACE}" ] || error "'${WORKSPACE}' Doesn't exist or is not a directory"
+            shift 2
+            ;;
+        -p|--project-version)
+            FULL_VERSION="${2}"
+            shift 2
+            ;;
+        -u|--upstream-version)
+            UPSTREAM_VERSION="${2}"
+            shift 2
+            ;;
+        -j|--jar)
+            BUILD_JAR="true"
+            shift
+            ;;
+        -d|--docker-registry)
+            DOCKER_REGISTRY="${2}"
+            shift 2
+            ;;
+        -smt|--skip-maven-tests)
+            MAVEN_SKIP_TESTS="true"
+            shift
+            ;;
+        *)
+            error "Unknown arg ${arg}"
+            ;;
+    esac
+done
+
+[[ -z "${WORKSPACE}" ]] && error "-w/--workspace not specified and is required"
+[[ -z "${FULL_VERSION}" ]] && error "-p/--project-version not specified and is required"
+[[ -z "${UPSTREAM_VERSION}" ]] && error "-u/--upstream-version not specified and is required"
+[[ -z "${DOCKER_REGISTRY}" ]] && error "-d/--docker-registry not specified and is required"
+
+
+# This is for two conditions:
+# * When we are building a release candidate (when there is a -rcNNN in the FULL_VERSION)
+# * When we are build a real release (when there is no -rcNNN and it matches a 3-digit version)
+# * All other cases just barf (invalid input/doesn't know how to handle input)
+#
+# First consider the real release case, we set the release to 1 (as in its ${VERSION}-1)
+# next consider the rc case, we set the release to 0.rcNNN (As in its ${VERSION}-0.rcNNN)
+#
+# We do this so a "real" release is a higher effective "version" (to package managers) than any
+# release candidate we generate (1 > 0.rcNNNN), this is then feed into the package builders
+# to produce higher versione-d software in the event a testing system would need to "upgrade"
+# from an ${VERSION}-0.rcNNN to ${VERSION}-1.
+if [[ "${FULL_VERSION}" =~ ^[0-9]+.[0-9]+.[0-9]+-rc[0-9]+$ ]]; then
+    RELEASE="0.${FULL_VERSION##*-rc}"
+    VERSION="${FULL_VERSION%%-rc*}"
+    MAVEN_ARTIFACT_VERSION="${FULL_VERSION}"
+elif [[ "${FULL_VERSION}" =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; then
+    RELEASE=1
+    VERSION="${FULL_VERSION}"
+    FULL_VERSION="${FULL_VERSION}-${RELEASE}"
+    MAVEN_ARTIFACT_VERSION="${VERSION}"
+else
+    error "Unknown version format '${FULL_VERSION}' "
+fi
+
+REQUIRED_UTILS=(
+    mvn
+    make
+    tar
+    zip
+    rpmbuild
+    debuild
+    git-buildpackage
+    xmlstarlet
+)
+
+for cmd in "${REQUIRED_UTILS[@]}"; do
+    if ! command -v "${cmd}" >/dev/null; then
+        echo "Required Utility '${cmd}' not found"
+        exit 1
+    fi
+done
+
+echo "FULL_VERSION=${FULL_VERSION} VERSION=${VERSION} RELEASE=${RELEASE} MAVEN_ARTIFACT_VERSION=${MAVEN_ARTIFACT_VERSION} UPSTREAM_VERSION=${UPSTREAM_VERSION}"
+cd "${WORKSPACE}"
+work_branch="debian-${FULL_VERSION}"
+git checkout -b "${work_branch}"
+export DEBEMAIL="Confluent Packaging <packages@confluent.io>"
+
+
+# Set version (Final or RC)
+
+# Maven does not provide a way to set the parent project version at the root pom, so we use
+# a tool called xmlstarlet to edit it.
+# ed       = Edit
+# --inpace = Edit in place
+# -P       = Perserve Whitespace nodes (will not preserve attribute node whitespace)
+# --update = Update this XPATH Node
+# --value  = The value in which to place things.
+# The XPATH requires the lambda (_) namespace specified to get to /project/parent/version
+xmlstarlet ed --inplace -P --update "/_:project/_:parent/_:version" --value "${UPSTREAM_VERSION}" pom.xml
+
+# Maven provides some helpfull commands for setting versions/properties
+mvn --batch-mode versions:set          -DgenerateBackupPoms=false "-DnewVersion=${MAVEN_ARTIFACT_VERSION}"
+mvn --batch-mode versions:set-property -DgenerateBackupPoms=false "-DnewVersion=${MAVEN_ARTIFACT_VERSION}" -Dproperty=io.confluent.ksql.version
+mvn --batch-mode versions:set-property -DgenerateBackupPoms=false "-DnewVersion=${UPSTREAM_VERSION}" -Dproperty=io.confluent.schema-registry.version
+
+# Set version for Debian Package
+dch --newversion "${FULL_VERSION}" "Release version ${FULL_VERSION}"
+dch --release --distribution unstable ""
+
+echo "Versioning Diff:"
+git diff | cat
+
+# Commit changes
+git add maven-settings.xml
+git commit -a -m "build: Setting project version ${MAVEN_ARTIFACT_VERSION} and parent version ${UPSTREAM_VERSION}."
+
+# We run things through fakeroot, which causes issues with finding an .m2/repository location to write. We set the homedir where it does
+# have write access too to get around that.
+export MAVEN_OPTS="${MAVEN_OPTS-} -Duser.home=/home/jenkins"
+
+# Build Debian Package
+git clean -fd
+echo "Building Debian Packages"
+git-buildpackage -us -uc --git-debian-branch="${work_branch}" --git-upstream-tree="${work_branch}" --git-builder="debuild --preserve-envvar=MAVEN_OPTS -d -i -I"
+
+# Build RPM
+echo "Building RPM packages"
+fakeroot make PACKAGE_TYPE=rpm "VERSION=${MAVEN_ARTIFACT_VERSION}" "RPM_VERSION=${VERSION}" "REVISION=${RELEASE}" -f debian/Makefile rpm
+
+# Build Archive
+echo "Building Archive packages"
+fakeroot make PACKAGE_TYPE=archive "VERSION=${MAVEN_ARTIFACT_VERSION}" -f debian/Makefile archive
+
+# Collect output
+mkdir -p "${WORKSPACE}/output"
+cp ../*.{changes,debian.tar.xz,dsc,build,orig.tar.gz,deb} "${WORKSPACE}/output/"
+cp ./*.{tar.gz,zip,rpm} "${WORKSPACE}/output/"
+echo "Output Contents:"
+ls -la "${WORKSPACE}/output/"
+
+if "${BUILD_JAR}"; then
+    if "${MAVEN_SKIP_TESTS}"; then
+        MAVEN_TEST_ARG="-DskipTests"
+    else
+        MAVEN_TEST_ARG="integration-test"
+    fi
+
+    # Build Jar
+    mvn --batch-mode -Pjenkins clean package dependency:analyze site validate -U \
+        "${MAVEN_TEST_ARG}" \
+        "-Dspotbugs.skip" \
+        "-Dcheckstyle.skip" \
+        "-Ddocker.tag=${MAVEN_ARTIFACT_VERSION}" \
+        "-Ddocker.registry=${DOCKER_REGISTRY}" \
+        "-Ddocker.upstream-tag=${UPSTREAM_VERSION}-latest" \
+        "-Dskip.docker.build=false"
+fi
--- release_stabilization.py
+++ release_stabilization.py
@@ -1,9 +1,67 @@
+import logging
+import os
+import subprocess
+import shlex
+from collections import OrderedDict
+
+DOCKER_REGISTRY = "confluent-docker-internal-stabilization.jfrog.io/"
+DOCKER_UPSTREAM_REGISTRY = "confluent-docker-internal.jfrog.io/"
+DOCKER_UPSTREAM_TAG = "v6.5.0"
+PACKAGES_MAVEN_URL = r'${env.ORG_GRADLE_PROJECT_mavenUrl}'
+CC_SPEC_KSQL_BRANCH = "master"
+CCLOUD_DOCKER_REPO = 'confluentinc/cc-ksql'
+CCLOUD_DOCKER_HOTFIX_REPO = 'confluentinc/cc-ksql-hotfix'
+# need to update below automatically?
+CP_VERSION = "7.1.0-cc-docker-ksql.11-88-ccs-rc1"
+MAVEN_SKIP_TESTS = False
 
 class Callbacks:
     def __init__(self, working_dir, leaf, dry_run):
         self.leaf = leaf
+        self.working_dir = working_dir
 
     """This is a callback to Confluent's cloud release tooling,
     and allows us to have consistent versioning"""
     def version_as_leaf(self):
         return self.leaf == 'cc-docker-ksql'
+
+    def maven_docker_build(self):
+        mvn_docker_args = OrderedDict()
+        mvn_docker_args["docker.registry"] = DOCKER_REGISTRY
+        mvn_docker_args["docker.test-registry"] = DOCKER_REGISTRY
+        mvn_docker_args["docker.upstream-registry"] = DOCKER_UPSTREAM_REGISTRY
+        mvn_docker_args["docker.upstream-tag"] = DOCKER_UPSTREAM_TAG
+        mvn_docker_args["skip.docker.build"] = "false"
+        return mvn_docker_args
+
+    def after_build(self, version: str) -> bool:
+        v_version = "v" + version
+        try:
+            # Install utilities required for building. XXX: Add to base image
+            subprocess.run(shlex.split("sudo apt update"))
+            subprocess.run(shlex.split("sudo apt install -y devscripts git-buildpackage dh-systemd javahelper xmlstarlet"))
+
+            os.environ["MAVEN_OPTS"] = "-XX:MaxPermSize=128M"
+            settings_file_path = os.path.join(self.working_dir, "maven-settings.xml")
+
+            subprocess.run(shlex.split(f"cp {settings_file_path} ~/.m2/settings.xml"))
+            subprocess.run(shlex.split(f"cp {settings_file_path} {self.working_dir}"))
+
+            build_packages_path = os.path.join(self.working_dir, "build_packages.sh")
+            subprocess.run(shlex.split(f"{build_packages_path} --workspace {self.working_dir} "
+                f"--docker-registry {DOCKER_REGISTRY} " +
+                f"--project-version {version} "
+                f"--upstream-version ${CP_VERSION} --jar " +
+                f"{'--skip-maven-tests' if MAVEN_SKIP_TESTS else ''}"))
+
+            # is a final build rc release
+            if "rc" not in version:
+                # Publish Artifacts
+                subprocess.run(shlex.split(f"mvn --batch-mode -Pjenkins deploy -DskipTests -Ddocker.skip-build=true -Ddocker.skip-test=true "
+                                           f"-DaltDeploymentRepository=confluent-artifactory-central::default::s3://staging-ksqldb-maven/maven " +
+                                           f"-DrepositoryId=confluent-artifactory-central "
+                                           f"-DnexusUrl=s3://staging-ksqldb-maven/maven "))
+
+            return True
+        except:
+            return False

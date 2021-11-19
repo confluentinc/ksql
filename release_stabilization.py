3f619edc84a92ebfd6243f2c5f6a237f751767f3
Natea Eshetu Beshada <nbeshada+git@confluent.io>
2021-11-19 12:40:20 -0500

    feat: build on prem images

--- release_stabilization.py
+++ release_stabilization.py
@@ -14,6 +14,8 @@ CCLOUD_DOCKER_HOTFIX_REPO = 'confluentinc/cc-ksql-hotfix'
 # need to update below automatically?
 CP_VERSION = "7.1.0-cc-docker-ksql.11-88-ccs-rc1"
 MAVEN_SKIP_TESTS = False
+DOCKER_REPOS = ['confluentinc/ksqldb-cli', 'confluentinc/ksqldb-server']
+DOCKER_ARTIFACT = 'confluentinc/ksqldb-docker'
 
 class Callbacks:
     def __init__(self, working_dir, leaf, dry_run):
@@ -25,42 +27,36 @@ class Callbacks:
     def version_as_leaf(self):
         return self.leaf == 'cc-docker-ksql'
 
+    def maven_build_args(self):
+        build_args = ["integration-test", "-DversionFilter=true", "-U", "-Dspotbugs.skip", "-Dcheckstyle.skip"]
+        return build_args
+
+    def maven_deploy_args(self):
+        deploy_args = ["-DversionFilter=true", "-U", "-DskipTests", "-DaltDeploymentRepository=confluent-artifactory-central::default::s3://staging-ksqldb-maven/maven",
+                       "-DrepositoryId=confluent-artifactory-central", "-DnexusUrl=s3://staging-ksqldb-maven/maven"]
+        return deploy_args
+
     def maven_docker_build(self):
         mvn_docker_args = OrderedDict()
-        mvn_docker_args["docker.registry"] = DOCKER_REGISTRY
-        mvn_docker_args["docker.test-registry"] = DOCKER_REGISTRY
-        mvn_docker_args["docker.upstream-registry"] = DOCKER_UPSTREAM_REGISTRY
-        mvn_docker_args["docker.upstream-tag"] = DOCKER_UPSTREAM_TAG
-        mvn_docker_args["skip.docker.build"] = "false"
+
+        # defaults docker.tag to be version created by stabilization
+        # mvn_docker_args["docker.tag"] =
+
+        mvn_docker_args["docker.registry"] = DOCKER_UPSTREAM_REGISTRY
+        mvn_docker_args["docker.test-registry"] = DOCKER_UPSTREAM_REGISTRY
+        mvn_docker_args["docker.upstream-tag"] = CP_VERSION + "-latest"
+        mvn_docker_args["skip.docker.build"] = "true"
+        mvn_docker_args["skip.docker.test"] = "true"
         return mvn_docker_args
 
-    def after_build(self, version: str) -> bool:
+    def after_publish(self, version: str) -> bool:
         v_version = "v" + version
         try:
-            # Install utilities required for building. XXX: Add to base image
-            subprocess.run(shlex.split("sudo apt update"))
-            subprocess.run(shlex.split("sudo apt install -y devscripts git-buildpackage dh-systemd javahelper xmlstarlet"))
 
-            os.environ["MAVEN_OPTS"] = "-XX:MaxPermSize=128M"
-            settings_file_path = os.path.join(self.working_dir, "maven-settings.xml")
-
-            subprocess.run(shlex.split(f"cp {settings_file_path} ~/.m2/settings.xml"))
-            subprocess.run(shlex.split(f"cp {settings_file_path} {self.working_dir}"))
-
-            build_packages_path = os.path.join(self.working_dir, "build_packages.sh")
-            subprocess.run(shlex.split(f"{build_packages_path} --workspace {self.working_dir} "
-                f"--docker-registry {DOCKER_REGISTRY} " +
-                f"--project-version {version} "
-                f"--upstream-version ${CP_VERSION} --jar " +
-                f"{'--skip-maven-tests' if MAVEN_SKIP_TESTS else ''}"))
-
-            # is a final build rc release
-            if "rc" not in version:
-                # Publish Artifacts
-                subprocess.run(shlex.split(f"mvn --batch-mode -Pjenkins deploy -DskipTests -Ddocker.skip-build=true -Ddocker.skip-test=true "
-                                           f"-DaltDeploymentRepository=confluent-artifactory-central::default::s3://staging-ksqldb-maven/maven " +
-                                           f"-DrepositoryId=confluent-artifactory-central "
-                                           f"-DnexusUrl=s3://staging-ksqldb-maven/maven "))
+            # pull, tag, and push latest docker images
+            for docker_repo in DOCKER_REPOS:
+                subprocess.run(shlex.split(f"docker tag {DOCKER_UPSTREAM_REGISTRY}{DOCKER_ARTIFACT}:{v_version} {DOCKER_UPSTREAM_REGISTRY}{CCLOUD_DOCKER_REPO}:{v_version}"))
+                subprocess.run(shlex.split(f"docker push {DOCKER_UPSTREAM_REGISTRY}{CCLOUD_DOCKER_REPO}:{v_version}"))
 
             return True
         except:

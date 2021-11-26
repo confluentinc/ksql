789bf8b7a0a639918e651175255b93fa4acc63a2
Zara Lim <zlim@confluent.io>
2021-11-26 12:06:47 -0800

    checkstyle

--- ksqldb-streams/src/main/java/io/confluent/ksql/execution/streams/SourceBuilder.java
+++ ksqldb-streams/src/main/java/io/confluent/ksql/execution/streams/SourceBuilder.java
@@ -36,7 +36,6 @@ import io.confluent.ksql.schema.ksql.PhysicalSchema;
 import io.confluent.ksql.schema.ksql.SystemColumns;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;

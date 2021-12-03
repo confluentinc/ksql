0a5ec973d8856e11aafef41b865605451c10a417
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2021-12-03 16:35:05 +0100

    feat: Validate connector config before creating it

--- ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/server/execution/ConnectExecutorTest.java
+++ ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/server/execution/ConnectExecutorTest.java
@@ -18,6 +18,7 @@ package io.confluent.ksql.rest.server.execution;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
@@ -47,6 +48,10 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
 import org.apache.hc.core5.http.HttpStatus;
+import org.apache.kafka.connect.runtime.rest.entities.ConfigInfo;
+import org.apache.kafka.connect.runtime.rest.entities.ConfigInfos;
+import org.apache.kafka.connect.runtime.rest.entities.ConfigKeyInfo;
+import org.apache.kafka.connect.runtime.rest.entities.ConfigValueInfo;
 import org.apache.kafka.connect.runtime.rest.entities.ConnectorInfo;
 import org.apache.kafka.connect.runtime.rest.entities.ConnectorType;
 import org.junit.Before;
@@ -55,27 +60,30 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+@SuppressWarnings("checkstyle:ClassDataAbstractionCoupling")
 @RunWith(MockitoJUnitRunner.class)
 public class ConnectExecutorTest {
 
   private static final KsqlConfig CONFIG = new KsqlConfig(ImmutableMap.of());
 
   private static final CreateConnector CREATE_CONNECTOR = new CreateConnector(
-      "foo", ImmutableMap.of("foo", new StringLiteral("bar")), Type.SOURCE,
-      false);
+      "foo", ImmutableMap.of("connector.class",
+        new StringLiteral("FileStreamSource")), Type.SOURCE, false);
 
   private static final ConfiguredStatement<CreateConnector> CREATE_CONNECTOR_CONFIGURED =
       ConfiguredStatement.of(PreparedStatement.of(
-          "CREATE SOURCE CONNECTOR foo WITH ('foo'='bar');",
+          "CREATE SOURCE CONNECTOR foo WITH ('connector.class'='FileStreamSource');",
           CREATE_CONNECTOR), SessionConfig.of(CONFIG, ImmutableMap.of()));
 
   private static final CreateConnector CREATE_DUPLICATE_CONNECTOR = new CreateConnector(
-      "foo", ImmutableMap.of("foo", new StringLiteral("bar")), Type.SOURCE,
+      "foo", ImmutableMap.of("connector.class",
+        new StringLiteral("FileStreamSource")), Type.SOURCE,
       true);
 
   private static final ConfiguredStatement<CreateConnector> CREATE_DUPLICATE_CONNECTOR_CONFIGURED =
       ConfiguredStatement.of(PreparedStatement.of(
-          "CREATE SOURCE CONNECTOR IF NOT EXISTS foo WITH ('foo'='bar');",
+          "CREATE SOURCE CONNECTOR IF NOT EXISTS foo "
+              + "WITH ('connector.class'='FileStreamSource');",
           CREATE_DUPLICATE_CONNECTOR), SessionConfig.of(CONFIG, ImmutableMap.of())
       );
 
@@ -93,20 +101,23 @@ public class ConnectExecutorTest {
   @Test
   public void shouldPassInCorrectArgsToConnectClient() {
     // Given:
-    givenSuccess();
+    givenValidationSuccess();
+    givenCreationSuccess();
 
     // When:
     ConnectExecutor
         .execute(CREATE_CONNECTOR_CONFIGURED, mock(SessionProperties.class), null, serviceContext);
 
     // Then:
-    verify(connectClient).create(eq("foo"), (Map<String, String>) argThat(hasEntry("foo", "bar")));
+    verify(connectClient).create(eq("foo"),
+        (Map<String, String>) argThat(hasEntry("connector.class", "FileStreamSource")));
   }
 
   @Test
   public void shouldReturnConnectorInfoEntityOnSuccess() {
     // Given:
-    givenSuccess();
+    givenValidationSuccess();
+    givenCreationSuccess();
 
     // When:
     final Optional<KsqlEntity> entity = ConnectExecutor
@@ -118,13 +129,15 @@ public class ConnectExecutorTest {
   }
 
   @Test
-  public void shouldReturnErrorEntityOnError() {
+  public void shouldReturnErrorEntityOnCreationError() {
     // Given:
-    givenError();
+    givenValidationSuccess();
+    givenCreationError();
 
     // When:
     final Optional<KsqlEntity> entity = ConnectExecutor
-        .execute(CREATE_CONNECTOR_CONFIGURED, mock(SessionProperties.class), null, serviceContext).getEntity();
+        .execute(CREATE_CONNECTOR_CONFIGURED, mock(SessionProperties.class), null,
+            serviceContext).getEntity();
 
     // Then:
     assertThat("Expected non-empty response", entity.isPresent());
@@ -132,8 +145,28 @@ public class ConnectExecutorTest {
   }
 
   @Test
+  public void shouldReturnErrorEntityOnValidationError() {
+    // Given:
+    givenValidationError();
+    givenCreationSuccess();
+
+    // When:
+    final Optional<KsqlEntity> entity = ConnectExecutor
+        .execute(CREATE_CONNECTOR_CONFIGURED, mock(SessionProperties.class), null,
+            serviceContext).getEntity();
+
+    // Then:
+    assertThat("Expected non-empty response", entity.isPresent());
+    assertThat(entity.get(), instanceOf(ErrorEntity.class));
+    final String expectedError = "Validation error: name - Name is missing\n"
+        + "hostname - Hostname is required. Some other error";
+    assertThat(((ErrorEntity) entity.get()).getErrorMessage(), is(expectedError));
+  }
+
+  @Test
   public void shouldReturnWarningWhenIfNotExistsSetConnectorExists() {
     //Given:
+    givenValidationSuccess();
     givenConnectorExists();
 
     //When
@@ -148,6 +181,7 @@ public class ConnectExecutorTest {
   @Test
   public void shouldReturnErrorIfConnectorExists() {
     //Given:
+    givenValidationSuccess();
     when(connectClient.create(anyString(), anyMap()))
         .thenReturn(
             ConnectResponse.failure("Connector foo already exists", HttpStatus.SC_CONFLICT));
@@ -161,7 +195,55 @@ public class ConnectExecutorTest {
     assertThat(entity.get(), instanceOf(ErrorEntity.class));
   }
 
-  private void givenSuccess() {
+  @Test
+  public void shouldReturnErrorIfConnectorTypeIsMissing() {
+    // Given:
+    final CreateConnector createConnectorMissingType = new CreateConnector(
+        "connector-name", ImmutableMap.of("foo", new StringLiteral("bar")),
+        Type.SOURCE, false);
+    final ConfiguredStatement<CreateConnector> createConnectorMissingTypeConfigured
+        = ConfiguredStatement.of(PreparedStatement.of(
+        "CREATE SOURCE CONNECTOR foo WITH ('foo'='bar');",
+        createConnectorMissingType), SessionConfig.of(CONFIG, ImmutableMap.of()));
+
+    // When:
+    final Optional<KsqlEntity> entity = ConnectExecutor
+        .execute(createConnectorMissingTypeConfigured, mock(SessionProperties.class),
+            null, serviceContext).getEntity();
+
+    // Then:
+    assertThat("Expected non-empty response", entity.isPresent());
+    assertThat(entity.get(), instanceOf(ErrorEntity.class));
+    final String expectedError = "Validation error: "
+        + "Connector config {name=connector-name, foo=bar} contains no connector type";
+    assertThat(((ErrorEntity) entity.get()).getErrorMessage(), is(expectedError));
+  }
+
+  @Test
+  public void shouldReturnErrorIfConnectorTypeIsEmpty() {
+    // Given:
+    final CreateConnector createConnectorEmptyType = new CreateConnector(
+        "foo", ImmutableMap.of("connector.class", new StringLiteral(" ")),
+        Type.SOURCE, false);
+    final ConfiguredStatement<CreateConnector> createConnectorEmptyTypeConfigured
+        = ConfiguredStatement.of(PreparedStatement.of(
+        "CREATE SOURCE CONNECTOR foo WITH ('connector.class'=' ');",
+        createConnectorEmptyType), SessionConfig.of(CONFIG, ImmutableMap.of()));
+
+
+    // When:
+    final Optional<KsqlEntity> entity = ConnectExecutor
+        .execute(createConnectorEmptyTypeConfigured, mock(SessionProperties.class),
+            null, serviceContext).getEntity();
+
+    // Then:
+    assertThat("Expected non-empty response", entity.isPresent());
+    assertThat(entity.get(), instanceOf(ErrorEntity.class));
+    final String expectedError = "Validation error: Connector type cannot be empty";
+    assertThat(((ErrorEntity) entity.get()).getErrorMessage(), is(expectedError));
+  }
+
+  private void givenCreationSuccess() {
     when(connectClient.create(anyString(), anyMap()))
         .thenReturn(ConnectResponse.success(
             new ConnectorInfo(
@@ -169,10 +251,56 @@ public class ConnectExecutorTest {
                 ImmutableMap.of(),
                 ImmutableList.of(),
                 ConnectorType.SOURCE), HttpStatus.SC_OK));
-
   }
 
-  private void givenError() {
+  private void givenValidationSuccess() {
+    when(connectClient.validate(anyString(), anyMap()))
+        .thenReturn(ConnectResponse.success(
+            new ConfigInfos(
+                "foo",
+                0,
+                ImmutableList.of(),
+                ImmutableList.of()), HttpStatus.SC_OK));
+  }
+
+  private void givenValidationError() {
+    final ConfigInfo configInfo1  = new ConfigInfo(new ConfigKeyInfo("name", "STRING",
+        true, null, "HIGH", "docs",
+            "Common", 1, "MEDIUM", "Connector name",
+        ImmutableList.of()),
+        new ConfigValueInfo("name", null, ImmutableList.of(), ImmutableList.of(
+            "Name is missing"), true));
+    final ConfigInfo configInfo2 = new ConfigInfo(new ConfigKeyInfo("hostname",
+        "STRING", false, null, "HIGH",
+        "docs for hostname",
+        "Common", 2, "MEDIUM", "hostname",
+        ImmutableList.of()),
+        new ConfigValueInfo("hostname", null, ImmutableList.of(),
+            ImmutableList.of("Hostname is required", "Some other error"), true));
+    final ConfigInfo configInfo3 = new ConfigInfo(new ConfigKeyInfo("port",
+        "INT", false, null, "HIGH",
+        "docs for port",
+        "Common", 3, "MEDIUM", "hostname",
+        ImmutableList.of()),
+        new ConfigValueInfo("port", null, ImmutableList.of(), ImmutableList.of(), true));
+    when(connectClient.validate(anyString(), anyMap()))
+        .thenReturn(ConnectResponse.success(
+            new ConfigInfos(
+                "foo",
+                2,
+                ImmutableList.of(),
+                ImmutableList.of(configInfo1, configInfo2, configInfo3)),
+            HttpStatus.SC_OK));
+  }
+
+  private void givenCreationError() {
+    when(connectClient.validate(anyString(), anyMap()))
+        .thenReturn(ConnectResponse.success(
+            new ConfigInfos(
+                "foo",
+                0,
+                ImmutableList.of(),
+                ImmutableList.of()), HttpStatus.SC_OK));
     when(connectClient.create(anyString(), anyMap()))
         .thenReturn(ConnectResponse.failure("error!", HttpStatus.SC_BAD_REQUEST));
   }

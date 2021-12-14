e7925c99d70ec65cf65c76c729a7444a816b1a59
John Roesler <vvcephei@users.noreply.github.com>
2021-12-13 22:48:37 -0600

    chore: Add metrics for push and pull limiters (#8438)

--- ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/util/RateLimiterTest.java
+++ ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/util/RateLimiterTest.java
@@ -16,11 +16,9 @@
 package io.confluent.ksql.rest.util;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThrows;
 
-import io.confluent.ksql.rest.util.ConcurrencyLimiter.Decrementer;
 import io.confluent.ksql.util.KsqlException;
 import java.util.Collections;
 import java.util.Map;
@@ -32,92 +30,54 @@ import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
-public class ConcurrencyLimiterTest {
+public class RateLimiterTest {
+
+  public static final String METRIC_NAMESPACE = "test";
 
   @Test
   public void shouldSucceedUnderLimit() {
     final Metrics metrics = new Metrics();
     final Map<String, String> tags = Collections.emptyMap();
-    ConcurrencyLimiter limiter = new ConcurrencyLimiter(
-        1,
-        "pull",
-        metrics,
-        tags
-    );
+    // It doesn't look like the underlying guava rate limiter has a way to control time, so we're
+    // just going to have to hope that these tests reliably run in under a second.
+    final RateLimiter limiter = new RateLimiter(1, METRIC_NAMESPACE, metrics, tags);
 
     assertThat(getReject(metrics, tags), is(0.0));
-    assertThat(getRemaining(metrics, tags), is(1.0));
 
-    Decrementer decrementer = limiter.increment();
+    limiter.checkLimit();
 
-    assertThat(limiter.getCount(), is(1));
     assertThat(getReject(metrics, tags), is(0.0));
-    assertThat(getRemaining(metrics, tags), is(0.0));
-
-    decrementer.decrementAtMostOnce();
-    decrementer.decrementAtMostOnce();
-
-    assertThat(limiter.getCount(), is(0));
-    assertThat(getReject(metrics, tags), is(0.0));
-    assertThat(getRemaining(metrics, tags), is(1.0));
-
   }
 
   @Test
   public void shouldError_atLimit() {
     final Metrics metrics = new Metrics();
     final Map<String, String> tags = Collections.emptyMap();
-    ConcurrencyLimiter limiter = new ConcurrencyLimiter(
-        1,
-        "pull",
-        metrics,
-        tags
-    );
+    // It doesn't look like the underlying guava rate limiter has a way to control time, so we're
+    // just going to have to hope that these tests reliably run in under a second.
+    final RateLimiter limiter = new RateLimiter(1, METRIC_NAMESPACE, metrics, tags);
 
     assertThat(getReject(metrics, tags), is(0.0));
-    assertThat(getRemaining(metrics, tags), is(1.0));
 
-    Decrementer decrementer = limiter.increment();
+    limiter.checkLimit();
+    final KsqlException ksqlException =
+        assertThrows(KsqlException.class, limiter::checkLimit);
 
-    assertThat(getReject(metrics, tags), is(0.0));
-    assertThat(getRemaining(metrics, tags), is(0.0));
-
-    final Exception e = assertThrows(
-        KsqlException.class,
-        limiter::increment
+    assertThat(
+        ksqlException.getMessage(),
+        is("Host is at rate limit for pull queries. Currently set to 1.0 qps.")
     );
-
-    assertThat(e.getMessage(), containsString("Host is at concurrency limit for pull queries."));
-    assertThat(limiter.getCount(), is(1));
     assertThat(getReject(metrics, tags), is(1.0));
-    assertThat(getRemaining(metrics, tags), is(0.0));
-
-    decrementer.decrementAtMostOnce();
-
-    assertThat(limiter.getCount(), is(0));
-    assertThat(getReject(metrics, tags), is(1.0));
-    assertThat(getRemaining(metrics, tags), is(1.0));
   }
 
   private double getReject(final Metrics metrics, final Map<String, String> tags) {
     final MetricName rejectMetricName = new MetricName(
-        "pull-concurrency-limit-reject-count",
+        METRIC_NAMESPACE + "-rate-limit-reject-count",
         "_confluent-ksql-limits",
         "The number of requests rejected by this limiter",
         tags
     );
     final KafkaMetric rejectMetric = metrics.metrics().get(rejectMetricName);
     return (double) rejectMetric.metricValue();
-  }
-
-  private double getRemaining(final Metrics metrics, final Map<String, String> tags) {
-    final MetricName remainingMetricName = new MetricName(
-        "pull-concurrency-limit-remaining",
-        "_confluent-ksql-limits",
-        "The current value of the concurrency limiter",
-        tags
-    );
-    final KafkaMetric remainingMetric = metrics.metrics().get(remainingMetricName);
-    return (double) remainingMetric.metricValue();
   }
 }

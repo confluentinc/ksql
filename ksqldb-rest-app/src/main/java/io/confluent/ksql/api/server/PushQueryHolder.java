6992a48ce527ac26d9c361dd79977176ffb93263
Alan Sheinberg <asheinberg@confluent.io>
2021-12-17 11:53:45 -0800

    Imports

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/api/server/PushQueryHolder.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/api/server/PushQueryHolder.java
@@ -18,7 +18,6 @@ package io.confluent.ksql.api.server;
 import io.confluent.ksql.api.spi.QueryPublisher;
 import io.confluent.ksql.rest.entity.PushQueryId;
 import java.util.Objects;
-import java.util.UUID;
 import java.util.function.Consumer;
 
 /**

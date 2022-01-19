2bd65f309cffb1bf9214178ddbad720df4f9bb9e
Natea Eshetu Beshada <nbeshada+git@confluent.io>
2022-01-19 14:54:58 -0800

    chore: remove unused code and remove staging

--- scripts/release/mvn-deploy.sh
+++ scripts/release/mvn-deploy.sh
@@ -24,9 +24,9 @@ do
   echo "$gitcmd checkout ${branches[i]}"
 
   deploy_cmd="mvn --batch-mode -Pjenkins deploy -DskipTests -Ddocker.skip-build=true -Ddocker.skip-test=true"
-  deploy_cmd+=" -DaltDeploymentRepository=confluent-artifactory-central::default::s3://staging-ksqldb-maven/maven"
+  deploy_cmd+=" -DaltDeploymentRepository=confluent-artifactory-central::default::s3://ksqldb-maven/maven"
   deploy_cmd+=" -DrepositoryId=confluent-artifactory-central"
-  deploy_cmd+=" -DnexusUrl=s3://staging-ksqldb-maven/maven"
+  deploy_cmd+=" -DnexusUrl=s3://ksqldb-maven/maven"
   echo $deploy_cmd
   eval $deploy_cmd
 
@@ -37,31 +37,3 @@ done
 # checkout CORRECT stabilization branch for each repo
 
 # run mvn deploy on each branch
-
-
-
-
-ksql_ver=$1
-cc_spec_ksql_branch=$2
-working_dir=$3
-gitcmd="git --git-dir=./cc-spec-ksql/.git --work-tree=./cc-spec-ksql"
-cc_spec_ksql_dir="./cc-spec-ksql"
-
-git clone git@github.com:confluentinc/cc-spec-ksql.git $cc_spec_ksql_dir
-$gitcmd checkout $cc_spec_ksql_branch
-
-# get the current version
-devel_ksql_ver=$(grep CurrentImage $cc_spec_ksql_dir/plugins/ksql/ksql_image.go | sed 's/.* = //')
-
-# test if we should update or not
-should_promote=$(python3 $working_dir/scripts/should-promote.py $ksql_ver $devel_ksql_ver)
-
-# update if required
-if [ -n "$FORCE_PROMOTE" ] || [ "$should_promote" == "yes" ]; then
-	echo "Promoting from $devel_ksql_ver to $ksql_ver"
-	bash $working_dir/scripts/generate-ksql-image-go.sh $ksql_ver > $cc_spec_ksql_dir/plugins/ksql/ksql_image.go
-	$gitcmd commit -am "bump ksqldb image to $ksql_ver"
-	$gitcmd push origin $cc_spec_ksql_branch
-else
-	echo "Skip promoting due to version mismatch $ksql_ver $devel_ksql_ver"
-fi

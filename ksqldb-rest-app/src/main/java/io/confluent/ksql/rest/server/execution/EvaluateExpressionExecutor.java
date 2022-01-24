4cf452b1487bb0b9bedbcded81878e6c07464692
Jim Hughes <jhughes@confluent.io>
2022-01-24 14:54:08 -0500

    Sorting out type resolution.

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/execution/EvaluateExpressionExecutor.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/execution/EvaluateExpressionExecutor.java
@@ -18,15 +18,18 @@ package io.confluent.ksql.rest.server.execution;
 import com.google.common.collect.ImmutableMap;
 import io.confluent.ksql.KsqlExecutionContext;
 import io.confluent.ksql.engine.generic.GenericExpressionResolver;
+import io.confluent.ksql.execution.expression.tree.Expression;
+import io.confluent.ksql.execution.util.ExpressionTypeManager;
 import io.confluent.ksql.name.ColumnName;
 import io.confluent.ksql.parser.tree.EvaluateExpression;
 import io.confluent.ksql.rest.SessionProperties;
 import io.confluent.ksql.rest.entity.ExpressionEvaluationEntity;
+import io.confluent.ksql.schema.ksql.LogicalSchema;
 import io.confluent.ksql.schema.ksql.types.SqlType;
-import io.confluent.ksql.schema.ksql.types.SqlTypes;
 import io.confluent.ksql.services.ServiceContext;
 import io.confluent.ksql.statement.ConfiguredStatement;
 import io.confluent.ksql.util.KsqlConfig;
+import java.util.Collections;
 import java.util.Optional;
 
 public final class EvaluateExpressionExecutor {
@@ -39,13 +42,22 @@ public final class EvaluateExpressionExecutor {
       final KsqlExecutionContext executionContext,
       final ServiceContext serviceContext
   ) {
-    final SqlType type = SqlTypes.STRING;
-    final ColumnName fieldName = ColumnName.of("Field");
+    // Calculate the return type
+    final LogicalSchema noColumns = LogicalSchema.builder().build();
+    final Expression expression = statement.getStatement().getExpression();
+    final ExpressionTypeManager expressionTypeManager
+        = new ExpressionTypeManager(noColumns, executionContext.getMetaStore());
+    final SqlType returnType = expressionTypeManager.getExpressionSqlType(expression,
+        Collections.emptyMap());
+
+    // Get the result
+    final ColumnName fieldName = ColumnName.of("Result");
     final KsqlConfig config = new KsqlConfig(ImmutableMap.of());
-    final GenericExpressionResolver resolver = new GenericExpressionResolver(type,
+    final GenericExpressionResolver resolver = new GenericExpressionResolver(returnType,
         fieldName, executionContext.getMetaStore(), config, "insert value", false);
     final Object result =
         resolver.resolve(statement.getStatement().getExpression());
+    
     return StatementExecutorResponse.handled(
         Optional.of(new ExpressionEvaluationEntity("result " + result.toString())));
   }

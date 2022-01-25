097f8ba4a97eeb178fc00b2ef760b6d0229baaee
Walker Carlson <wcarlson@confluent.io>
2022-01-25 15:38:06 -0600

    checkstyle

--- ksqldb-common/src/main/java/io/confluent/ksql/util/QueryApplicationId.java
+++ ksqldb-common/src/main/java/io/confluent/ksql/util/QueryApplicationId.java
@@ -69,7 +69,7 @@ public final class QueryApplicationId {
   public static String buildPrefix(
       final String serviceId,
       final String queryPrefix
-   ) {
+  ) {
     return ReservedInternalTopics.KSQL_INTERNAL_TOPIC_PREFIX
         + serviceId
         + queryPrefix;

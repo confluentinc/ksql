15a4045f40f201b4f530b46a2baf2f9d170b0578
Matthias J. Sax <matthias@confluent.io>
2022-01-28 11:51:22 -0800

    Update ksqldb-engine/src/main/java/io/confluent/ksql/services/SandboxedSchemaRegistryClient.java
    Co-authored-by: Victoria Xia <victoria.f.xia281@gmail.com>

--- ksqldb-engine/src/main/java/io/confluent/ksql/services/SandboxedSchemaRegistryClient.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/services/SandboxedSchemaRegistryClient.java
@@ -51,7 +51,7 @@ final class SandboxedSchemaRegistryClient {
 
   static final class SandboxSchemaRegistryCache implements SchemaRegistryClient {
     // we use `MockSchemaRegistryClient` as a cache inside the sandbox to store
-    // newly registered schema (with polluting the actual SR)
+    // newly registered schemas (without polluting the actual SR)
     // this allowd dependent statement to execute successfully inside the sandbox
     private final MockSchemaRegistryClient sandboxCacheClient = new MockSchemaRegistryClient();
     // client to talk to the actual SR

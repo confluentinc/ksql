6dc5561fcbbb576265b92194b515476e2d679303
Natea Eshetu Beshada <nbeshada+git@confluent.io>
2022-02-02 23:57:18 -0800

    clean up script

--- scripts/release/mvn-deploy.sh
+++ scripts/release/mvn-deploy.sh
@@ -1,91 +1,58 @@
-#!/bin/bash
+#!/bin/bash -x
+
+# Builds all the Java maven artifacts for each ksql dependency and deploys them as a maven
+# repository to a staging S3 bucket.
 
 set -e
 stabilization_unique_identifier=$1
 MY_DIR=`echo $(cd $(dirname $0); pwd)`
 
-# list of cc-docker-ksql dependencies. This will eventually be automatically parsed from the output
-# file of the release stabilization jenkins job
+# list of ksql dependencies
 repos=('kafka' 'common' 'rest-utils' 'schema-registry' 'ksql')
-
 len=${#repos[@]}
 
 for (( i=0; i<$len; i++ ));
 do
-  echo $i
-
-  echo "git clone git@github.com:confluentinc/${repos[i]}.git ./${repos[i]}"
+  # clone each repo and checkout the stabilization branch
   git clone git@github.com:confluentinc/${repos[i]}.git ./${repos[i]}
-
   gitcmd="git --git-dir=./${repos[i]}/.git --work-tree=./${repos[i]}"
-  echo "branch=$($gitcmd branch --list '$stabilization_unique_identifier')"
   branch=$($gitcmd branch --list '$stabilization_unique_identifier')
-  echo "$gitcmd checkout $branch"
   $gitcmd checkout $branch
 
-  echo "cd ${repos[i]}"
   eval cd ${repos[i]}
 
+  # pom file means this is a maven project
   if [[ -e "pom.xml" ]]
   then
-    # pom file means this is a maven project
-    eval pwd
     if [[ "${repos[i]}" == "common" ]]
     then
-      echo "patch -p1 --ignore-whitespace --verbose < ${MY_DIR}/common-deploy.patch"
+      # apply a patch to pom so that we can deploy to s3 with correct permissions
       patch -p1 --ignore-whitespace --verbose < ${MY_DIR}/common-deploy.patch
-      find . -name '*.rej'
     elif [[ "${repos[i]}" == "rest-utils" ]]
     then
-      echo "patch -p1 --ignore-whitespace --verbose < ${MY_DIR}/plugins-deploy.patch"
       patch -p1 --ignore-whitespace --verbose < ${MY_DIR}/plugins-deploy.patch
-      find . -name '*.rej'
     elif [[ "${repos[i]}" == "ksql" || "${repos[i]}" == "schema-registry" ]]
     then
-      echo "patch -p1 --ignore-whitespace --verbose < ${MY_DIR}/pluginManagement-deploy.patch"
       patch -p1 --ignore-whitespace --verbose < ${MY_DIR}/pluginManagement-deploy.patch
-      find . -name '*.rej'
     fi
-    
-#    mvn_cmd="mvn help:effective-pom"
-#    echo $mvn_cmd
-#    eval $mvn_cmd
+
     deploy_cmd="mvn --batch-mode -Pjenkins deploy -DskipTests -Ddocker.skip-build=true -Ddocker.skip-test=true"
     deploy_cmd+=" -DaltDeploymentRepository=confluent-artifactory-central::default::s3://staging-ksqldb-maven/maven"
     deploy_cmd+=" -DrepositoryId=confluent-artifactory-central"
     deploy_cmd+=" -DnexusUrl=s3://staging-ksqldb-maven/maven"
-    echo $deploy_cmd
     eval $deploy_cmd
 
-    echo "cd .."
     eval cd ..
+
+  # gradle file means this is a gradle project
   elif [[ -e 'build.gradle' ]]
   then
-    # gradle file means this is a gradle project
-
-
-    eval pwd
-#    echo "git apply --recount --whitespace=fix ${MY_DIR}/kafka-deploy.patch"
-#    git apply --recount --whitespace=warn ${MY_DIR}/kafka-deploy.patch
-    eval ls
-    echo "patch -p1 --verbose < ${MY_DIR}/kafka-deploy.patch"
     patch -p1 --verbose < ${MY_DIR}/kafka-deploy.patch
-    find . -name '*.rej'
-
-    eval ls
-
     deploy_cmd="./gradlewAll --init-script ${GRADLE_NEXUS_SETTINGS} --no-daemon"
     deploy_cmd+=" -PmavenUrl=s3://staging-ksqldb-maven/maven -PskipSigning=true uploadArchives"
-    echo $deploy_cmd
     eval $deploy_cmd
 
-    echo "cd .."
     eval cd ..
   fi
 done
 
-# git clone each repo in list
-
-# checkout CORRECT stabilization branch for each repo
-
-# run mvn deploy on each branch

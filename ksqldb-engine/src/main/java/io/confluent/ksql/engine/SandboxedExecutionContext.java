30debb4168cd7e72335932dea491ebe089041520
Walker Carlson <18128741+wcarlson5@users.noreply.github.com>
2022-02-15 15:53:43 -0800

    fix: always close validation runtimes even on an exception (#8769)
    * fix: always close validation runtimes even on an exception
    
    * fix: we want to reset offsets on close

--- ksqldb-engine/src/main/java/io/confluent/ksql/engine/SandboxedExecutionContext.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/engine/SandboxedExecutionContext.java
@@ -162,19 +162,23 @@ final class SandboxedExecutionContext implements KsqlExecutionContext {
       final ServiceContext serviceContext,
       final ConfiguredKsqlPlan ksqlPlan
   ) {
-    final ExecuteResult result = EngineExecutor.create(
-        engineContext,
-        serviceContext,
-        ksqlPlan.getConfig()
-    ).execute(ksqlPlan.getPlan());
+    try {
+      final ExecuteResult result = EngineExecutor.create(
+          engineContext,
+          serviceContext,
+          ksqlPlan.getConfig()
+      ).execute(ksqlPlan.getPlan());
 
-    // Having a streams running in a sandboxed environment is not necessary
-    if (!getKsqlConfig().getBoolean(KsqlConfig.KSQL_SHARED_RUNTIME_ENABLED)) {
-      result.getQuery().map(QueryMetadata::getKafkaStreams).ifPresent(streams -> streams.close());
-    } else {
-      engineContext.getQueryRegistry().closeRuntimes();
+      // Having a streams running in a sandboxed environment is not necessary
+      if (!getKsqlConfig().getBoolean(KsqlConfig.KSQL_SHARED_RUNTIME_ENABLED)) {
+        result.getQuery().map(QueryMetadata::getKafkaStreams).ifPresent(streams -> streams.close());
+      }
+      return result;
+    } finally {
+      if (getKsqlConfig().getBoolean(KsqlConfig.KSQL_SHARED_RUNTIME_ENABLED)) {
+        engineContext.getQueryRegistry().closeRuntimes();
+      }
     }
-    return result;
   }
 
   @Override

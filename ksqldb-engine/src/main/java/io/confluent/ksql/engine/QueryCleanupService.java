2cbe6ce1f1d7c3280813560a8cc34ce5a5d3a71f
Walker Carlson <wcarlson@confluent.io>
2022-03-07 14:24:08 -0600

    fix: clean up mistaken topic names

--- ksqldb-engine/src/main/java/io/confluent/ksql/engine/QueryCleanupService.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/engine/QueryCleanupService.java
@@ -94,6 +94,7 @@ public class QueryCleanupService extends AbstractExecutionThreadService {
   public static class QueryCleanupTask implements Runnable {
     private final String appId;
     private final String queryTopicPrefix;
+    private final String altQueryTopicPrefix;
     private final Optional<String> topologyName;
     private final String pathName;
     private final boolean isTransient;
@@ -114,6 +115,11 @@ public class QueryCleanupService extends AbstractExecutionThreadService {
           .map(s -> QueryApplicationId.buildInternalTopicPrefix(
               serviceId,
               persistentQueryPrefix) + s)
+          .orElse(appId);
+      altQueryTopicPrefix = queryId
+          .map(s -> QueryApplicationId.buildInternalTopicPrefix(
+              serviceId,
+              persistentQueryPrefix.split("_")[0] + "-") + s)
           .orElse(appId);
       //generate the prefix depending on if using named topologies
       this.isTransient = isTransient;
@@ -158,6 +164,8 @@ public class QueryCleanupService extends AbstractExecutionThreadService {
           () -> {
             LOG.info("Deleting topics for prefix {}", queryTopicPrefix);
             serviceContext.getTopicClient().deleteInternalTopics(queryTopicPrefix);
+            serviceContext.getTopicClient().deleteInternalTopics(altQueryTopicPrefix);
+
           },
           "internal topics"
       );

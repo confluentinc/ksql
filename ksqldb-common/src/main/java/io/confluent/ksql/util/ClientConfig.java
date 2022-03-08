645cb296367efe39400252bdefc027469edd1462
Vicky Papavasileiou <vpapavasileiou@confluent.io>
2022-03-08 19:44:18 +0000

    add javadoc

--- ksqldb-common/src/main/java/io/confluent/ksql/util/ClientConfig.java
+++ ksqldb-common/src/main/java/io/confluent/ksql/util/ClientConfig.java
@@ -15,6 +15,9 @@
 
 package io.confluent.ksql.util;
 
+/**
+ * Consistency levels a client can specify to be used with pull queries.
+ */
 public class ClientConfig {
 
   public enum ConsistencyLevel {

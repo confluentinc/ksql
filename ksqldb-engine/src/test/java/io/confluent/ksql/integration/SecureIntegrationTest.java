b02a4d875de1b42d442a7bb4b4fd61fa219556d9
Walker Carlson <wcarlson@confluent.io>
2022-03-09 11:22:25 -0600

    fix: checkstyle

--- ksqldb-engine/src/test/java/io/confluent/ksql/integration/SecureIntegrationTest.java
+++ ksqldb-engine/src/test/java/io/confluent/ksql/integration/SecureIntegrationTest.java
@@ -92,7 +92,6 @@ import org.apache.kafka.streams.errors.StreamsException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;

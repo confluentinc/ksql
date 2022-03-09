21f27ed523aed003bd4cee05b573b69256fc0af3
Walker Carlson <wcarlson@confluent.io>
2022-03-09 13:04:54 -0600

    fix: last test fix

--- ksqldb-engine/src/test/java/io/confluent/ksql/integration/DependentStatementsIntegrationTest.java
+++ ksqldb-engine/src/test/java/io/confluent/ksql/integration/DependentStatementsIntegrationTest.java
@@ -29,7 +29,6 @@ import org.apache.kafka.test.IntegrationTest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -132,7 +131,6 @@ public class DependentStatementsIntegrationTest {
   }
 
   @Test
-  @Ignore
   public void shouldRegisterAvroSchemaInSandboxViaCSandSchemaId() throws Exception {
     // Given:
     executeStatement(

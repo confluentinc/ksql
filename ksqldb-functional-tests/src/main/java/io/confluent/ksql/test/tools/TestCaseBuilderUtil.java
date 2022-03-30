d376d79eafb1fa250d0a8a7a2c2dd51151b41fdb
Matthias J. Sax <matthias@confluent.io>
2022-03-29 19:19:52 -0700

    tests: extend QTT to support parameterized configs

--- ksqldb-functional-tests/src/main/java/io/confluent/ksql/test/tools/TestCaseBuilderUtil.java
+++ ksqldb-functional-tests/src/main/java/io/confluent/ksql/test/tools/TestCaseBuilderUtil.java
@@ -72,7 +72,8 @@ public final class TestCaseBuilderUtil {
   public static String buildTestName(
       final Path originalFileName,
       final String testName,
-      final Optional<String> explicitFormat
+      final Optional<String> explicitFormat,
+      final Optional<String> config
   ) {
     final String prefix = filePrefix(originalFileName.toString());
 
@@ -80,7 +81,11 @@ public final class TestCaseBuilderUtil {
         .map(f -> " - " + f)
         .orElse("");
 
-    return prefix + testName + pf;
+    final String pc = config
+        .map(f -> " - " + f)
+        .orElse("");
+
+    return prefix + testName + pf + pc;
   }
 
   public static String extractSimpleTestName(

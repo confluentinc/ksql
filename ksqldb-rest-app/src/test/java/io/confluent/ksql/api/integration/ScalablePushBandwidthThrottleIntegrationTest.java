c719d26e06df0ab0b2c4c352d6bd02c36c908445
nateab <nbeshada+git@confluent.io>
2022-04-04 13:39:45 -0700

    update assertThatEventually to have higher pause

--- ksqldb-rest-app/src/test/java/io/confluent/ksql/api/integration/ScalablePushBandwidthThrottleIntegrationTest.java
+++ ksqldb-rest-app/src/test/java/io/confluent/ksql/api/integration/ScalablePushBandwidthThrottleIntegrationTest.java
@@ -273,7 +273,7 @@ public class ScalablePushBandwidthThrottleIntegrationTest {
   private void assertExpectedScalablePushQueries(
       final int expectedScalablePushQueries
   ) {
-    assertThatEventually(() -> {
+    assertThatEventually("expected scalable push queries", () -> {
       for (final PersistentQueryMetadata metadata : REST_APP.getEngine().getPersistentQueries()) {
         if (metadata.getScalablePushRegistry().get().latestNumRegistered()
             < expectedScalablePushQueries
@@ -287,6 +287,6 @@ public class ScalablePushBandwidthThrottleIntegrationTest {
         }
       }
       return true;
-    }, is(true));
+    }, is(true), 100, 1000);
   }
 }

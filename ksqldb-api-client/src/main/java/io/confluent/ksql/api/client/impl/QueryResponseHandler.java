7efbbb8834a3ac4b4b51c84f482cdf8d0e2d62dd
Almog Gavra <almog@confluent.io>
2022-04-07 12:36:18 -0700

    chore: upgrade vertx to 4.3

--- ksqldb-api-client/src/main/java/io/confluent/ksql/api/client/impl/QueryResponseHandler.java
+++ ksqldb-api-client/src/main/java/io/confluent/ksql/api/client/impl/QueryResponseHandler.java
@@ -17,8 +17,6 @@ package io.confluent.ksql.api.client.impl;
 
 import static io.confluent.ksql.util.BytesUtils.toJsonMsg;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.confluent.ksql.api.client.util.JsonMapper;
 import io.confluent.ksql.api.client.util.RowUtil;
 import io.confluent.ksql.rest.entity.QueryResponseMetadata;
 import io.vertx.core.Context;
@@ -29,8 +27,6 @@ import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 abstract class QueryResponseHandler<T extends CompletableFuture<?>> extends ResponseHandler<T> {
-
-  private static final ObjectMapper JSON_MAPPER = JsonMapper.get();
 
   protected boolean hasReadArguments;
 
@@ -78,6 +74,8 @@ abstract class QueryResponseHandler<T extends CompletableFuture<?>> extends Resp
                       RowUtil.colNamesFromSchema(schema),
                       RowUtil.colTypesFromSchema(schema),
                       null);
+      // queryResponseMetadata = ApiJsonMapper.INSTANCE.get()
+      //    .readValue(buff.getBytes(), QueryResponseMetadata.class);
     } catch (Exception e) {
       cf.completeExceptionally(e);
       return;

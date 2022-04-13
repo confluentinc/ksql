87c0ce1858b79e918f8b6fe316bb7f2759f574d4
Sergio Peña <sergio@confluent.io>
2022-04-13 18:59:18 -0500

    fix: Create stream fails when multiple Protobuf schema definitions exist (#8933)

--- ksqldb-functional-tests/src/main/java/io/confluent/ksql/test/serde/protobuf/ValueSpecProtobufSerdeSupplier.java
+++ ksqldb-functional-tests/src/main/java/io/confluent/ksql/test/serde/protobuf/ValueSpecProtobufSerdeSupplier.java
@@ -15,31 +15,24 @@
 
 package io.confluent.ksql.test.serde.protobuf;
 
-import static io.confluent.connect.protobuf.ProtobufDataConfig.SCHEMAS_CACHE_SIZE_CONFIG;
-import static io.confluent.connect.protobuf.ProtobufDataConfig.WRAPPER_FOR_RAW_PRIMITIVES_CONFIG;
-
-import com.google.common.collect.ImmutableMap;
 import io.confluent.connect.protobuf.ProtobufConverter;
-import io.confluent.connect.protobuf.ProtobufData;
-import io.confluent.connect.protobuf.ProtobufDataConfig;
 import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchema;
+import io.confluent.ksql.serde.protobuf.ProtobufProperties;
+import io.confluent.ksql.serde.protobuf.ProtobufSchemaTranslator;
 import io.confluent.ksql.test.serde.ConnectSerdeSupplier;
 import org.apache.kafka.connect.data.Schema;
 
 public class ValueSpecProtobufSerdeSupplier extends ConnectSerdeSupplier<ProtobufSchema> {
 
-  private final boolean unwrapPrimitives;
+  private final ProtobufSchemaTranslator schemaTranslator;
 
-  public ValueSpecProtobufSerdeSupplier(final boolean unwrapPrimitives) {
+  public ValueSpecProtobufSerdeSupplier(final ProtobufProperties protobufProperties) {
     super(ProtobufConverter::new);
-    this.unwrapPrimitives = unwrapPrimitives;
+    this.schemaTranslator = new ProtobufSchemaTranslator(protobufProperties);
   }
 
   @Override
   protected Schema fromParsedSchema(final ProtobufSchema schema) {
-    return new ProtobufData(new ProtobufDataConfig(ImmutableMap.of(
-        SCHEMAS_CACHE_SIZE_CONFIG, 1,
-        WRAPPER_FOR_RAW_PRIMITIVES_CONFIG, unwrapPrimitives
-    ))).toConnectSchema(schema);
+    return schemaTranslator.toConnectSchema(schema);
   }
 }

9d809dee77f9cc177f4d74184c3eb38e9abcf88a
Matthias J. Sax <matthias@confluent.io>
2022-04-26 21:08:28 -0700

    fix checkstyle

--- ksqldb-functional-tests/src/main/java/io/confluent/ksql/test/model/TopicNode.java
+++ ksqldb-functional-tests/src/main/java/io/confluent/ksql/test/model/TopicNode.java
@@ -15,8 +15,6 @@
 
 package io.confluent.ksql.test.model;
 
-import static java.util.Objects.requireNonNull;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;

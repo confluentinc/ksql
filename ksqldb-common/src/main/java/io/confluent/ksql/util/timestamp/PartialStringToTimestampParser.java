b76cae5e1a8a85925ea4246c8dc867b13d40a8b8
Alieh Saeedi <asaeedi@confluent.io>
2022-06-17 00:54:52 +0200

    fix: preventing multiple trailing Zs

--- ksqldb-common/src/main/java/io/confluent/ksql/util/timestamp/PartialStringToTimestampParser.java
+++ ksqldb-common/src/main/java/io/confluent/ksql/util/timestamp/PartialStringToTimestampParser.java
@@ -55,7 +55,7 @@ public class PartialStringToTimestampParser {
       final String withTimezone = text.substring(text.indexOf('T') + 1);
       timezone = getTimezone(withTimezone);
       time = completeTime(withTimezone.substring(0, withTimezone.length() - timezone.length())
-              .replace("Z",""));
+              .replaceFirst("Z",""));
     } else {
       date = completeDate(text);
       time = completeTime("");

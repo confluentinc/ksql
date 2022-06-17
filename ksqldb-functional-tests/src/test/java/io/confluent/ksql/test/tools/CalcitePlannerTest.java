2e3e308feed2481e82bec8e58f69443d2b5418f2
John Roesler <john@vvcephei.org>
2022-06-17 12:21:55 -0500

    add test cases

--- ksqldb-functional-tests/src/test/java/io/confluent/ksql/test/tools/CalcitePlannerTest.java
+++ ksqldb-functional-tests/src/test/java/io/confluent/ksql/test/tools/CalcitePlannerTest.java
@@ -1,6 +1,8 @@
 package io.confluent.ksql.test.tools;
 
 import com.facebook.presto.spi.SchemaTableName;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
 import com.google.common.collect.ImmutableMap;
 import io.confluent.ksql.execution.ddl.commands.KsqlTopic;
 import io.confluent.ksql.metastore.model.KsqlTable;
@@ -16,12 +18,12 @@ import io.confluent.ksql.serde.SerdeFeatures;
 import io.confluent.ksql.serde.ValueFormat;
 import io.confluent.ksql.structured.SchemaKStream;
 import java.io.PrintWriter;
+import java.util.Collection;
 import java.util.Optional;
 import org.apache.calcite.adapter.java.ReflectiveSchema;
 import org.apache.calcite.rel.RelRoot;
 import org.apache.calcite.rel.externalize.RelWriterImpl;
 import org.apache.calcite.schema.SchemaPlus;
-import org.apache.calcite.schema.Table;
 import org.apache.calcite.sql.SqlExplainLevel;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.parser.SqlParseException;
@@ -33,15 +35,37 @@ import org.apache.calcite.tools.Planner;
 import org.apache.calcite.tools.RelConversionException;
 import org.apache.calcite.tools.ValidationException;
 import org.apache.kafka.streams.Topology;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class CalcitePlannerTest {
+  public static String[] queries = new String[] {
+      "SELECT * FROM Pantalones",
+      "SELECT id, waist FROM Pantalones",
+      "SELECT * FROM Pantalones WHERE ID = '5'",
+      "SELECT * FROM Pantalones WHERE ID IN ('5', 'XYZ')",
+      "SELECT * FROM Pantalones WHERE Waist > 32",
+      "SELECT * FROM Pantalones JOIN Abrigos A ON Pantalones.ID = A.ID",
+      "SELECT Pantalones.ID, Waist, Size, Sleeve FROM Pantalones JOIN Abrigos A ON Pantalones.ID = A.ID",
+      "SELECT * FROM Pantalones JOIN Abrigos A ON Pantalones.ID > A.ID",
+      "CREATE TABLE asdf AS SELECT * FROM Pantalones",
+      "SELECT ID, CONCAT(ID, '+'), Waist * 2 FROM Pantalones",
+      "(SELECT ID, Waist AS Num FROM Pantalones) UNION (SELECT ID, Sleeve as Num FROM Abrigos)",
+      "SELECT ID1, Waist, Size FROM (SELECT ID AS ID1, Waist FROM Pantalones) JOIN (SELECT ID AS ID2, Size FROM Pantalones) ON ID1 = ID2",
+      // algebraic canonicalization
+      "SELECT * FROM (SELECT * FROM Pantalones)",
+      "SELECT id, size FROM (SELECT id, waist, size FROM (SELECT * FROM Pantalones))",
+  };
 
   private String schemaName;
-  private ImmutableMap<SchemaTableName, ? extends KsqlTable<?>> ksqlTables;
   private Planner planner;
+  private ImmutableMap<SchemaTableName, ? extends KsqlTable<?>> ksqlTables;
 
+  private final String sql;
 
   public static class Pantalones {
 
@@ -98,7 +122,7 @@ public class CalcitePlannerTest {
   }
 
   @Before
-  public void beforeClass() {
+  public void before() {
     schemaName = "schema";
 
     final SchemaPlus rootSchema = Frameworks.createRootSchema(true);
@@ -113,7 +137,7 @@ public class CalcitePlannerTest {
         .defaultSchema(schema)
         .build();
 
-    this.planner = Frameworks.getPlanner(config);
+    planner = Frameworks.getPlanner(config);
 
     final LogicalSchema.Builder pantalonesKsqlSchema = LogicalSchema.builder();
     pantalonesKsqlSchema.keyColumn(ColumnName.of("id"), SqlPrimitiveType.of(SqlBaseType.STRING));
@@ -138,41 +162,61 @@ public class CalcitePlannerTest {
         new SchemaTableName(schemaName, "pantalones"), pantalonesKsqlTable
     );
   }
+  @Parameterized.Parameters(name = "{0}")
+  public static Collection<Object[]> data() {
+    final Builder<Object[]> builder = ImmutableList.builder();
+    for (final String query : queries) {
+      builder.add(new Object[]{query});
+    }
+    return builder.build();
+  }
 
-  @Test
-  public void selectStarFrom()
-      throws SqlParseException, ValidationException, RelConversionException {
-    final SqlNode parsed = planner.parse("SELECT * FROM Pantalones");
-    System.out.println(parsed);
-    final SqlNode validated = planner.validate(parsed);
-    System.out.println(validated);
-    final RelRoot logicalPlan = planner.rel(validated);
-    final RelWriterImpl relWriter = new RelWriterImpl(new PrintWriter(System.out),
-        SqlExplainLevel.ALL_ATTRIBUTES, false);
-    logicalPlan.project().explain(relWriter);
-
-//    final PlanBuildContext planBuildContext = PrestoPlanner.getPlanBuildContext();
-//    final SchemaKStream<?> physicalPlan = PrestoPlanner.physicalPlan(prestoPlan, ksqlTables, planBuildContext);
-//    final Topology topology = PrestoPlanner.getTopology(planBuildContext, physicalPlan);
-//    System.out.println(topology.describe());
+  public CalcitePlannerTest(final String sql){
+    this.sql = sql;
   }
 
   @Test
-  public void selectFieldsFrom()
+  public void logical()
       throws SqlParseException, ValidationException, RelConversionException {
-    final SqlNode parsed = planner.parse("SELECT id, waist FROM Pantalones");
+    final RelRoot logicalPlan = getLogicalPlan(sql);
+
+    final RelWriterImpl relWriter = new RelWriterImpl(new PrintWriter(System.out),
+        SqlExplainLevel.ALL_ATTRIBUTES, false);
+
+    System.out.println("{");
+    logicalPlan.project().explain(relWriter);
+    System.out.println("}");
+  }
+
+  @Test
+  public void physical()
+      throws SqlParseException, ValidationException, RelConversionException {
+    final RelRoot logicalPlan = getLogicalPlan(sql);
+
+    final Topology topology = getPhysicalPlan(logicalPlan);
+    System.out.println(topology.describe());
+  }
+
+  private Topology getPhysicalPlan(final RelRoot logicalPlan) {
+    final PlanBuildContext planBuildContext = CalcitePlanner.getPlanBuildContext();
+    final SchemaKStream<?> physicalPlan = CalcitePlanner.physicalPlan(
+        logicalPlan.project(),
+        ksqlTables,
+        planBuildContext
+    );
+    final Topology topology = CalcitePlanner.getTopology(planBuildContext, physicalPlan);
+    return topology;
+  }
+
+  @NotNull
+  private RelRoot getLogicalPlan(final String sql)
+      throws SqlParseException, ValidationException, RelConversionException {
+    final SqlNode parsed = planner.parse(sql);
     System.out.println(parsed);
     final SqlNode validated = planner.validate(parsed);
     System.out.println(validated);
     final RelRoot logicalPlan = planner.rel(validated);
-    final RelWriterImpl relWriter = new RelWriterImpl(new PrintWriter(System.out),
-        SqlExplainLevel.ALL_ATTRIBUTES, false);
-    logicalPlan.project().explain(relWriter);
-
-    final PlanBuildContext planBuildContext = CalcitePlanner.getPlanBuildContext();
-    final SchemaKStream<?> physicalPlan = CalcitePlanner.physicalPlan(logicalPlan.project(), ksqlTables, planBuildContext);
-    final Topology topology = CalcitePlanner.getTopology(planBuildContext, physicalPlan);
-    System.out.println(topology.describe());
+    return logicalPlan;
   }
 
 }

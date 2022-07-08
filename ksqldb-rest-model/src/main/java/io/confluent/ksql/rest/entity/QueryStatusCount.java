77a1cfecad5621bcb4e1ee957914a89755b196e0
Jim Hughes <jhughes@confluent.io>
2022-07-08 11:04:30 -0400

    Fixing checkstyle.

--- ksqldb-rest-model/src/main/java/io/confluent/ksql/rest/entity/QueryStatusCount.java
+++ ksqldb-rest-model/src/main/java/io/confluent/ksql/rest/entity/QueryStatusCount.java
@@ -25,7 +25,6 @@ import java.util.Collections;
 import java.util.EnumMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import org.apache.kafka.streams.KafkaStreams;
 
 /**

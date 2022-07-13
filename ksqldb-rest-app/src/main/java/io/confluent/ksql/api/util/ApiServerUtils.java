aa0f73c8c0b48ac58656c116115ff3e5d1be3ff6
Hao Li <1127478+lihaosky@users.noreply.github.com>
2022-07-13 10:39:53 -0700

    fix: comment and test

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/api/util/ApiServerUtils.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/api/util/ApiServerUtils.java
@@ -15,31 +15,10 @@
 
 package io.confluent.ksql.api.util;
 
-import io.confluent.ksql.properties.PropertiesUtil;
 import io.confluent.ksql.rest.entity.KsqlRequest;
-import io.confluent.ksql.rest.server.KsqlRestConfig;
 import io.confluent.ksql.util.QueryMask;
-import io.confluent.ksql.util.VertxSslOptionsFactory;
-import io.vertx.core.http.HttpHeaders;
-import io.vertx.core.http.HttpServerOptions;
-import io.vertx.core.json.JsonObject;
-import io.vertx.core.net.JksOptions;
-import io.vertx.core.net.PfxOptions;
-import io.vertx.ext.web.RoutingContext;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.channels.ClosedChannelException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import org.apache.kafka.common.config.ConfigException;
-import org.apache.kafka.common.config.SslConfigs;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public final class ApiServerUtils {
-  private static final Logger LOG = LoggerFactory.getLogger(ApiServerUtils.class);
 
   private ApiServerUtils() {
   }
@@ -54,91 +33,5 @@ public final class ApiServerUtils {
 
   public static void setMaskedSql(final KsqlRequest request) {
     request.setMaskedKsql(QueryMask.getMaskedStatement(request.getUnmaskedKsql()));
-  }
-
-  public static void unhandledExceptionHandler(final Throwable t) {
-    if (t instanceof ClosedChannelException) {
-      LOG.debug("Unhandled ClosedChannelException (connection likely closed early)", t);
-    } else {
-      LOG.error("Unhandled exception", t);
-    }
-  }
-
-  public static void chcHandler(final RoutingContext routingContext) {
-    routingContext.response().putHeader(HttpHeaders.CONTENT_TYPE.toString(), "application/json")
-        .end(new JsonObject().toBuffer());
-  }
-
-  public static List<URI> parseListenerStrings(
-      final KsqlRestConfig config,
-      final List<String> stringListeners) {
-    final List<URI> listeners = new ArrayList<>();
-    for (String listenerName : stringListeners) {
-      try {
-        final URI uri = new URI(listenerName);
-        final String scheme = uri.getScheme();
-        if (!"http".equalsIgnoreCase(scheme) && !"https".equalsIgnoreCase(scheme)) {
-          throw new ConfigException("Invalid URI scheme should be http or https: " + listenerName);
-        }
-        if ("https".equalsIgnoreCase(scheme)) {
-          final String keyStoreLocation = config
-              .getString(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG);
-          if (keyStoreLocation == null || keyStoreLocation.isEmpty()) {
-            throw new ConfigException("https listener specified but no keystore provided");
-          }
-        }
-        listeners.add(uri);
-      } catch (URISyntaxException e) {
-        throw new ConfigException("Invalid listener URI: " + listenerName);
-      }
-    }
-    return listeners;
-  }
-
-  public static List<URI> parseListeners(final KsqlRestConfig config) {
-    final List<String> sListeners = config.getList(KsqlRestConfig.LISTENERS_CONFIG);
-    return parseListenerStrings(config, sListeners);
-  }
-
-  private static void configureTlsKeyStore(
-      final KsqlRestConfig ksqlRestConfig,
-      final HttpServerOptions httpServerOptions,
-      final String keyStoreAlias
-  ) {
-    final Map<String, String> props = PropertiesUtil.toMapStrings(ksqlRestConfig.originals());
-    final String keyStoreType = ksqlRestConfig.getString(KsqlRestConfig.SSL_KEYSTORE_TYPE_CONFIG);
-
-    if (keyStoreType.equals(KsqlRestConfig.SSL_STORE_TYPE_JKS)) {
-      final Optional<JksOptions> keyStoreOptions =
-          VertxSslOptionsFactory.buildJksKeyStoreOptions(props, Optional.ofNullable(keyStoreAlias));
-
-      keyStoreOptions.ifPresent(options -> httpServerOptions.setKeyStoreOptions(options));
-    } else if (keyStoreType.equals(KsqlRestConfig.SSL_STORE_TYPE_PKCS12)) {
-      final Optional<PfxOptions> keyStoreOptions =
-          VertxSslOptionsFactory.getPfxKeyStoreOptions(props);
-
-      keyStoreOptions.ifPresent(options -> httpServerOptions.setPfxKeyCertOptions(options));
-    }
-  }
-
-  private static void configureTlsTrustStore(
-      final KsqlRestConfig ksqlRestConfig,
-      final HttpServerOptions httpServerOptions
-  ) {
-    final Map<String, String> props = PropertiesUtil.toMapStrings(ksqlRestConfig.originals());
-    final String trustStoreType =
-        ksqlRestConfig.getString(KsqlRestConfig.SSL_TRUSTSTORE_TYPE_CONFIG);
-
-    if (trustStoreType.equals(KsqlRestConfig.SSL_STORE_TYPE_JKS)) {
-      final Optional<JksOptions> trustStoreOptions =
-          VertxSslOptionsFactory.getJksTrustStoreOptions(props);
-
-      trustStoreOptions.ifPresent(options -> httpServerOptions.setTrustOptions(options));
-    } else if (trustStoreType.equals(KsqlRestConfig.SSL_STORE_TYPE_PKCS12)) {
-      final Optional<PfxOptions> trustStoreOptions =
-          VertxSslOptionsFactory.getPfxTrustStoreOptions(props);
-
-      trustStoreOptions.ifPresent(options -> httpServerOptions.setTrustOptions(options));
-    }
   }
 }

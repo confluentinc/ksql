c418ae19cc142956530e33606f6c1bdf9157e8af
Matthias J. Sax <matthias@confluent.io>
2022-07-14 16:34:07 -0700

    chore: fix logging (#9278)

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/resources/KsqlResource.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/resources/KsqlResource.java
@@ -279,8 +279,6 @@ public class KsqlResource implements KsqlConfigurable {
       final KsqlSecurityContext securityContext,
       final KsqlRequest request
   ) {
-    LOG.info("Received: " + request);
-
     throwIfNotConfigured();
 
     activenessRegistrar.updateLastRequestTime();
@@ -333,7 +331,6 @@ public class KsqlResource implements KsqlConfigurable {
           )
       );
 
-      LOG.info("Processed successfully: " + request);
       addCommandRunnerWarning(
           entities,
           commandRunnerWarning);

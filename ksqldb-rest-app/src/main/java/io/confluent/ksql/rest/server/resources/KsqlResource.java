2b59c5e2e030891c46962d1638aeae2a6a1407f6
Matthias J. Sax <matthias@confluent.io>
2022-07-15 14:17:36 -0700

    Revert "chore: fix logging (#9278)"
    This reverts commit 8d805566c8e049845e233bd1e2a81f8efd2b16b3.

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/resources/KsqlResource.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/resources/KsqlResource.java
@@ -272,6 +272,8 @@ public class KsqlResource implements KsqlConfigurable {
       final KsqlSecurityContext securityContext,
       final KsqlRequest request
   ) {
+    LOG.info("Received: " + request);
+
     throwIfNotConfigured();
 
     activenessRegistrar.updateLastRequestTime();
@@ -324,6 +326,7 @@ public class KsqlResource implements KsqlConfigurable {
           )
       );
 
+      LOG.info("Processed successfully: " + request);
       addCommandRunnerWarning(
           entities,
           commandRunnerWarning);

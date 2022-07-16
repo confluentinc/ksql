81c3480749ccc79bb56a05c3710e0dd5a8debb18
Steven Zhang <35498506+stevenpyzhang@users.noreply.github.com>
2022-07-15 18:48:49 -0700

    chore: make precondition backoff configurable (#9281)
    * chore: make precondition backoff configurable
    
    * update default to 5 seconds

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/KsqlRestConfig.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/KsqlRestConfig.java
@@ -403,6 +403,11 @@ public class KsqlRestConfig extends AbstractConfig {
   private static final String KSQL_COMMAND_TOPIC_RATE_LIMIT_CONFIG_DEFAULT_DOC = 
       "Sets the number of statements that can be executed against the command topic per second";
 
+  public static final String KSQL_PRECONDITION_CHECKER_BACK_OFF_TIME_MS =
+      KSQL_CONFIG_PREFIX + "server.precondition.max.backoff.ms";
+  protected static final String KSQL_PRECONDITION_CHECKER_BACK_OFF_TIME_MS_DOC =
+      "The maximum amount of time to wait before checking the KSQL server preconditions again.";
+
   private static final ConfigDef CONFIG_DEF;
 
   static {
@@ -757,12 +762,18 @@ public class KsqlRestConfig extends AbstractConfig {
             Importance.LOW,
             KSQL_SERVER_SNI_CHECK_ENABLE_DOC
         ).define(
-        KSQL_COMMAND_TOPIC_RATE_LIMIT_CONFIG,
-        Type.DOUBLE,
-        KSQL_COMMAND_TOPIC_RATE_LIMIT_CONFIG_DEFAULT,
-        Importance.LOW,
-        KSQL_COMMAND_TOPIC_RATE_LIMIT_CONFIG_DEFAULT_DOC
-      );
+            KSQL_COMMAND_TOPIC_RATE_LIMIT_CONFIG,
+            Type.DOUBLE,
+            KSQL_COMMAND_TOPIC_RATE_LIMIT_CONFIG_DEFAULT,
+            Importance.LOW,
+            KSQL_COMMAND_TOPIC_RATE_LIMIT_CONFIG_DEFAULT_DOC
+        ).define(
+            KSQL_PRECONDITION_CHECKER_BACK_OFF_TIME_MS,
+            Type.LONG,
+            5000L,
+            Importance.MEDIUM,
+            KSQL_PRECONDITION_CHECKER_BACK_OFF_TIME_MS_DOC
+        );
   }
 
   public KsqlRestConfig(final Map<?, ?> props) {

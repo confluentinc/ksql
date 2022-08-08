600558fa65ff1cc10e430b81c07c219e1b318254
asaeedi <asaeedi@confluent.io>
2022-08-08 12:03:18 +0200

    add more integraion tests

--- ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/server/InsertionIntegrationTest.java
+++ ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/server/InsertionIntegrationTest.java
@@ -43,6 +43,14 @@ public class InsertionIntegrationTest {
   private  static final String EVOLVING_TOPIC = "evolving_topic";
 
   private  static final String SIMPLE_STREAM = "simple_stream";
+  private  static final String DECIMAL_STREAM = "decimal_stream";
+  private  static final String MAP_STREAM = "map_stream";
+  private  static final String ARRAY_STREAM = "array_stream";
+  private  static final String STRUCT_STREAM = "struct_stream";
+
+
+
+
   private  static final String EVOLVING_STREAM = "evolving_stream";
 
   private static final TestKsqlRestApp REST_APP = TestKsqlRestApp
@@ -65,14 +73,34 @@ public class InsertionIntegrationTest {
     ksqlRestClient = REST_APP.buildKsqlClient(Optional.empty());
 
     ksqlRestClient.makeKsqlRequest
-            ("CREATE STREAM " + SIMPLE_STREAM + " (K0 INT KEY, K1 STRING KEY,V0 BOOLEAN, V1 INT) " +
-                    "WITH (KAFKA_TOPIC = '" + SIMPLE_TOPIC + "', VALUE_FORMAT = 'AVRO', " +
-                    "KEY_FORMAT = 'AVRO', PARTITIONS = 1);");
+        ("CREATE STREAM " + SIMPLE_STREAM + " (K0 INT KEY, K1 STRING KEY,V0 BOOLEAN, V1 INT) " +
+            "WITH (KAFKA_TOPIC = '" + SIMPLE_TOPIC + "', VALUE_FORMAT = 'AVRO', " +
+            "KEY_FORMAT = 'AVRO', PARTITIONS = 1);");
 
     ksqlRestClient.makeKsqlRequest
-            ("CREATE STREAM " + EVOLVING_STREAM + " (K0 INT KEY, V0 BOOLEAN) " +
-                    "WITH (KAFKA_TOPIC = '" + EVOLVING_TOPIC + "', VALUE_FORMAT = 'AVRO', " +
-                    "PARTITIONS = 1);");
+        ("CREATE STREAM " + DECIMAL_STREAM + " (K0 INT KEY, V0 DECIMAL(21,19)) " +
+            "WITH (KAFKA_TOPIC = '" + DECIMAL_STREAM + "', VALUE_FORMAT = 'AVRO', " +
+            "KEY_FORMAT = 'AVRO', PARTITIONS = 1);");
+
+    ksqlRestClient.makeKsqlRequest
+        ("CREATE STREAM " + MAP_STREAM + " (K0 INT KEY, V0 MAP<STRING, INT>) " +
+            "WITH (KAFKA_TOPIC = '" + MAP_STREAM + "', VALUE_FORMAT = 'AVRO', " +
+            "KEY_FORMAT = 'AVRO', PARTITIONS = 1);");
+
+    ksqlRestClient.makeKsqlRequest
+        ("CREATE STREAM " + ARRAY_STREAM + " (K0 INT KEY, V0 ARRAY<INT>) " +
+            "WITH (KAFKA_TOPIC = '" + ARRAY_STREAM + "', VALUE_FORMAT = 'AVRO', " +
+            "KEY_FORMAT = 'AVRO', PARTITIONS = 1);");
+
+    ksqlRestClient.makeKsqlRequest
+        ("CREATE STREAM " + STRUCT_STREAM + " (K0 INT KEY, V0 STRUCT<A INT, B STRING, C BOOLEAN>) " +
+            "WITH (KAFKA_TOPIC = '" + STRUCT_STREAM + "', VALUE_FORMAT = 'AVRO', " +
+            "KEY_FORMAT = 'AVRO', PARTITIONS = 1);");
+
+    ksqlRestClient.makeKsqlRequest
+        ("CREATE STREAM " + EVOLVING_STREAM + " (K0 INT KEY, V0 BOOLEAN) " +
+            "WITH (KAFKA_TOPIC = '" + EVOLVING_TOPIC + "', VALUE_FORMAT = 'AVRO', " +
+            "PARTITIONS = 1);");
   }
 
   @After
@@ -80,13 +108,65 @@ public class InsertionIntegrationTest {
   }
 
   @Test
-  public void shouldAvoidNewSchemaRegistrationWithInsertValues() {
+  public void shouldAvoidNewSchemaRegistrationForCompoundKeyWithInsertValues() {
 
     ksqlRestClient.makeKsqlRequest
-            ("INSERT INTO " + SIMPLE_STREAM + " (K0, K1, V0, V1) VALUES (1,'foo', true, 3);");
+        ("INSERT INTO " + SIMPLE_STREAM + " (K0, K1, V0, V1) VALUES (1,'foo', true, 3);");
 
     String key = KsqlConstants.getSRSubject(SIMPLE_TOPIC, true);
     String value = KsqlConstants.getSRSubject(SIMPLE_TOPIC, false);
+
+    assertThat(TEST_HARNESS.getLatestSchemaVersion(key), is(1));
+    assertThat(TEST_HARNESS.getLatestSchemaVersion(value), is(1));
+  }
+
+  @Test
+  public void shouldAvoidNewSchemaRegistrationForDecimalWithInsertValues() {
+
+    ksqlRestClient.makeKsqlRequest
+        ("INSERT INTO " + DECIMAL_STREAM + " (K0, V0) VALUES (1, 14.75);");
+
+    String key = KsqlConstants.getSRSubject(DECIMAL_STREAM, true);
+    String value = KsqlConstants.getSRSubject(DECIMAL_STREAM, false);
+
+    assertThat(TEST_HARNESS.getLatestSchemaVersion(key), is(1));
+    assertThat(TEST_HARNESS.getLatestSchemaVersion(value), is(1));
+  }
+
+  @Test
+  public void shouldAvoidNewSchemaRegistrationForMapWithInsertValues() {
+
+    ksqlRestClient.makeKsqlRequest
+        ("INSERT INTO " + MAP_STREAM + " (K0, V0) VALUES (1, MAP<\"foo\", 10>);");
+
+    String key = KsqlConstants.getSRSubject(MAP_STREAM, true);
+    String value = KsqlConstants.getSRSubject(MAP_STREAM, false);
+
+    assertThat(TEST_HARNESS.getLatestSchemaVersion(key), is(1));
+    assertThat(TEST_HARNESS.getLatestSchemaVersion(value), is(1));
+  }
+
+  @Test
+  public void shouldAvoidNewSchemaRegistrationForArrayWithInsertValues() {
+
+    ksqlRestClient.makeKsqlRequest
+        ("INSERT INTO " + ARRAY_STREAM + " (K0, V0) VALUES (1, Array[10, 20, 30]);");
+
+    String key = KsqlConstants.getSRSubject(ARRAY_STREAM, true);
+    String value = KsqlConstants.getSRSubject(ARRAY_STREAM, false);
+
+    assertThat(TEST_HARNESS.getLatestSchemaVersion(key), is(1));
+    assertThat(TEST_HARNESS.getLatestSchemaVersion(value), is(1));
+  }
+
+  @Test
+  public void shouldAvoidNewSchemaRegistrationForStructWithInsertValues() {
+
+    ksqlRestClient.makeKsqlRequest
+        ("INSERT INTO " + STRUCT_STREAM + " (K0, V0) VALUES (1, STRUCT<10, \"foo\", true>);");
+
+    String key = KsqlConstants.getSRSubject(STRUCT_STREAM, true);
+    String value = KsqlConstants.getSRSubject(STRUCT_STREAM, false);
 
     assertThat(TEST_HARNESS.getLatestSchemaVersion(key), is(1));
     assertThat(TEST_HARNESS.getLatestSchemaVersion(value), is(1));
@@ -95,12 +175,12 @@ public class InsertionIntegrationTest {
   public void shouldSupportEvolvingSchemasOnInsert() {
 
     ksqlRestClient.makeKsqlRequest
-            ("CREATE OR REPLACE STREAM " + EVOLVING_STREAM + " (K0 INT KEY, V0 BOOLEAN, V1 STRING) " +
-                    "WITH (KAFKA_TOPIC = '" + EVOLVING_TOPIC + "', VALUE_FORMAT = 'AVRO', " +
-                    "PARTITIONS = 1);");
+        ("CREATE OR REPLACE STREAM " + EVOLVING_STREAM + " (K0 INT KEY, V0 BOOLEAN, V1 STRING) " +
+            "WITH (KAFKA_TOPIC = '" + EVOLVING_TOPIC + "', VALUE_FORMAT = 'AVRO', " +
+            "PARTITIONS = 1);");
 
     ksqlRestClient.makeKsqlRequest
-            ("INSERT INTO " + EVOLVING_STREAM + " (K0, V0, V1) VALUES (1, true, 'foo');");
+        ("INSERT INTO " + EVOLVING_STREAM + " (K0, V0, V1) VALUES (1, true, 'foo');");
 
     String value = KsqlConstants.getSRSubject(EVOLVING_TOPIC, false);
 

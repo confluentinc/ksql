4a831f84fbe6ab85cdf3c97f672db16d5ddf3078
asaeedi <asaeedi@confluent.io>
2022-08-08 23:23:49 +0200

    add explanatory comments

--- ksqldb-serde/src/main/java/io/confluent/ksql/serde/avro/KsqlAvroSerdeFactory.java
+++ ksqldb-serde/src/main/java/io/confluent/ksql/serde/avro/KsqlAvroSerdeFactory.java
@@ -169,6 +169,9 @@ class KsqlAvroSerdeFactory implements SerdeFactory {
     avroConfig.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG,
         ksqlConfig.getString(KsqlConfig.SCHEMA_REGISTRY_URL_PROPERTY));
 
+    /*  CONNECT_META_DATA_CONFIG is set to true in order to be consistent with the code used to
+        register schema in CREATE TABLE/STREAM.
+    */
     avroConfig.put(AvroDataConfig.CONNECT_META_DATA_CONFIG, true);
 
     if (schemaId.isPresent()) {

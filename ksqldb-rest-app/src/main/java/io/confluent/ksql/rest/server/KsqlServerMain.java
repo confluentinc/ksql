8de48c0b92232293e9cef9222798e647394319cf
Steven Zhang <stevenz@confluent.io>
2022-08-09 13:34:59 -0700

    remove print

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/KsqlServerMain.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/KsqlServerMain.java
@@ -54,7 +54,6 @@ public class KsqlServerMain {
         return;
       }
 
-      System.out.println("steven is MIGRATING UP IN HERE asdfasdf");
       final Supplier<Map<String, String>> propertiesLoader =
           () -> PropertiesUtil.applyOverrides(
             PropertiesUtil.loadProperties(serverOptions.getPropertiesFile()),

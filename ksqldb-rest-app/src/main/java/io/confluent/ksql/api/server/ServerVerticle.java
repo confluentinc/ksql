5f3f237af84c65b91f36ead35bb5aa596a6c1882
Rohan <desai.p.rohan@gmail.com>
2022-08-12 11:31:44 -0700

    fix: cors handling for precondition server
    This patch adds cors handling for the precondition server. It also refactors the
    cors test so that it can be used to test any server, and then uses the refactored
    test to test the precondition server

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/api/server/ServerVerticle.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/api/server/ServerVerticle.java
@@ -118,7 +118,7 @@ public class ServerVerticle extends AbstractVerticle {
       router.route().handler(new SniHandler());
     }
 
-    KsqlCorsHandler.setupCorsHandler(server, router);
+    KsqlCorsHandler.setupCorsHandler(server.getConfig(), router);
 
     // /chc endpoints need to be before server state handler but after CORS handler as they
     // need to be usable from browser with cross origin policy

2c83f44e1b9dcd162193b003af436751fc61a451
Natea Eshetu Beshada <nbeshada+git@confluent.io>
2022-08-16 15:47:40 -0700

    ignore unknown true

--- ksqldb-rest-model/src/main/java/io/confluent/ksql/rest/entity/HealthCheckResponse.java
+++ ksqldb-rest-model/src/main/java/io/confluent/ksql/rest/entity/HealthCheckResponse.java
@@ -27,7 +27,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
-@JsonIgnoreProperties()
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Immutable
 public final class HealthCheckResponse {
 

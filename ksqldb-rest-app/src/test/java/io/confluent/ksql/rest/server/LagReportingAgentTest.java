0691419d6dbde7947da8a73809f5900202cc4cc1
Tomasz Nguyen <tonguyen@confluent.io>
2022-08-17 18:23:02 +0100

    Revert "fix: Ensure lag reporter doesn't crash (#9423)"
    This reverts commit 6527c2d145dddfd9adfaad7b0a8506b2cffeb45e.

--- ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/server/LagReportingAgentTest.java
+++ ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/server/LagReportingAgentTest.java
@@ -3,10 +3,7 @@ package io.confluent.ksql.rest.server;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -28,7 +25,6 @@ import io.confluent.ksql.util.KsqlHostInfo;
 import io.confluent.ksql.util.PersistentQueryMetadata;
 import java.net.URI;
 import java.time.Clock;
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
 import org.apache.kafka.streams.LagInfo;
@@ -240,7 +236,9 @@ public class LagReportingAgentTest {
     assertEquals(M2_B4_LAG, lag.getOffsetLag());
   }
 
-  private void setupLags() {
+  @Test
+  public void shouldSendLags() {
+    // Given:
     when(clock.millis()).thenReturn(TIME_NOW_MS);
     when(lagInfo0.currentOffsetPosition()).thenReturn(M2_A1_CUR);
     when(lagInfo0.endOffsetPosition()).thenReturn(M2_A1_END);
@@ -266,47 +264,7 @@ public class LagReportingAgentTest {
     when(query0.getQueryApplicationId()).thenReturn(QUERY_ID0);
     when(query1.getAllLocalStorePartitionLags()).thenReturn(query1Lag);
     when(query1.getQueryApplicationId()).thenReturn(QUERY_ID1);
-  }
-
-  @Test
-  public void shouldSendLags() {
-    // Given:
-    setupLags();
     SendLagService sendLagService = lagReportingAgent.new SendLagService();
-
-    // When:
-    lagReportingAgent.onHostStatusUpdated(HOSTS_ALIVE);
-    sendLagService.runOneIteration();
-
-    // Then:
-    LagReportingMessage exp = hostLag(LOCALHOST_INFO, LAG_MAP2, TIME_NOW_MS);
-    verify(ksqlClient).makeAsyncLagReportRequest(eq(URI.create("http://host2:1234/")), eq(exp));
-    verify(ksqlClient).makeAsyncLagReportRequest(eq(URI.create("http://host1:1234/")), eq(exp));
-  }
-
-  @Test
-  public void shouldSendLags_generalError() {
-    // Given:
-    ArrayList<PersistentQueryMetadata> queries = new ArrayList<>();
-    queries.add(null);
-    when(ksqlEngine.getPersistentQueries()).thenReturn(queries);
-    SendLagService sendLagService = lagReportingAgent.new SendLagService();
-
-    // When:
-    lagReportingAgent.onHostStatusUpdated(HOSTS_ALIVE);
-    sendLagService.runOneIteration();
-
-    // Then:
-    verify(ksqlClient, never()).makeAsyncLagReportRequest(any(), any());
-  }
-
-  @Test
-  public void shouldSendLags_networkError() {
-    // Given:
-    setupLags();
-    SendLagService sendLagService = lagReportingAgent.new SendLagService();
-    doThrow(new RuntimeException("Error!"))
-        .when(ksqlClient).makeAsyncLagReportRequest(eq(URI.create("http://host1:1234/")), any());
 
     // When:
     lagReportingAgent.onHostStatusUpdated(HOSTS_ALIVE);

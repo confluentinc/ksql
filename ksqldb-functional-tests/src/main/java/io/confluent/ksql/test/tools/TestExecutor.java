360a5e32fe457363d8e4e7d89a86094b58fa3740
Vicky Papavasileiou <vpapavasileiou@confluent.io>
2022-09-26 12:41:27 +0100

    remove print

--- ksqldb-functional-tests/src/main/java/io/confluent/ksql/test/tools/TestExecutor.java
+++ ksqldb-functional-tests/src/main/java/io/confluent/ksql/test/tools/TestExecutor.java
@@ -437,7 +437,6 @@ public class TestExecutor implements Closeable {
       final Collection<ProducerRecord<?, ?>> actual,
       final boolean ranWithInsertStatements
   ) {
-    System.out.println("---> Actual records: " + actual);
     if (actual.size() != expected.size()) {
       throw new KsqlException("Topic " + topicName + ". Expected <" + expected.size()
           + "> records but it was <" + actual.size() + ">\n" + getActualsForErrorMessage(actual));

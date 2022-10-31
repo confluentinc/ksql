dad01201e016e1d87fc5cd941e3b324b14ef48a6
Walker Carlson <wcarlson@confluent.io>
2022-10-31 12:56:09 -0500

    add testing

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/restore/KsqlRestoreCommandTopic.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/restore/KsqlRestoreCommandTopic.java
@@ -424,7 +424,7 @@ public class KsqlRestoreCommandTopic {
         final JSONObject queryPlan = plan.getJSONObject("queryPlan");
         queryId = queryPlan.getString("queryId");
         if (hasKey(queryPlan, "runtimeId")
-            && queryPlan.get("runtimeId") != null
+            && !queryPlan.isNull("runtimeId")
             && ((Optional<String>) queryPlan.get("runtimeId")).isPresent()) {
           streamsProperties.put(
               StreamsConfig.APPLICATION_ID_CONFIG,

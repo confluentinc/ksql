3c9b29525c71f37ae4aac09a479d58bf9e0660c8
Roman <rkhachatryan@confluent.io>
2023-02-14 20:16:19 +0100

    Upgrade netty to 4.1.86 #2 (#9770)
    * Revert "Revert "[KSE-1612] Upgrade netty to 4.1.86 and vertx to 4.3.7 (#9764)" (#9766)"
    
    This reverts commit 1ecd4f7b95165743317b348b876f405827051878.
    
    * Fix KsqlClientTest
    
    Update exception expectations in KsqlClientTest according to the
    upgraded vert.x.
    
    * Update origin in CorsTest as per OWASP
    
    Include schema into the ORIGIN,
    see https://github.com/vert-x3/vertx-web/commit/c667f07d14cba3e59f39a8b4abc7b4017258cf8c

--- ksqldb-rest-client/src/test/java/io/confluent/ksql/rest/client/KsqlClientTest.java
+++ ksqldb-rest-client/src/test/java/io/confluent/ksql/rest/client/KsqlClientTest.java
@@ -484,7 +484,7 @@ public class KsqlClientTest {
     );
 
     // Then:
-    assertThat(e.getMessage(), containsString(
+    assertThat(e.getCause().getMessage(), containsString(
         "java.io.IOException: Keystore was tampered with, or password was incorrect"
     ));
   }
@@ -505,7 +505,7 @@ public class KsqlClientTest {
     );
 
     // Then:
-    assertThat(e.getMessage(), containsString(
+    assertThat(e.getCause().getMessage(), containsString(
         "java.io.IOException: Keystore was tampered with, or password was incorrect"
     ));
   }
@@ -723,6 +723,7 @@ public class KsqlClientTest {
     props.putAll(ClientTrustStore.trustStoreProps());
     props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, password);
     createClient(props);
+    ksqlClient.target(serverUri).getServerInfo().get();
   }
 
   private void startServerWithTls() throws Exception {

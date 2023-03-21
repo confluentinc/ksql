feb8014b4426ed05a7ad38394b31bb78e914bf4d
Matthias J. Sax <matthias@confluent.io>
2023-03-21 16:58:38 -0700

    fix

--- ksqldb-parser/src/test/java/io/confluent/ksql/parser/KsqlParserTest.java
+++ ksqldb-parser/src/test/java/io/confluent/ksql/parser/KsqlParserTest.java
@@ -428,12 +428,6 @@ public class KsqlParserTest {
   }
 
   @Test
-  public void testReservedColumnIdentifers() {
-    assertQuerySucceeds("SELECT ROWTIME as ROWTIME FROM test1 t1;");
-    assertQuerySucceeds("SELECT ROWKEY as ROWKEY FROM test1 t1;");
-  }
-
-  @Test
   public void testReservedRowTimeAlias() {
     // When:
     final ParseFailedException e = assertThrows(

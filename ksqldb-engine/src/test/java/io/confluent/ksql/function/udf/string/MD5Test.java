a7813513e92e2f0bb5f34348a4f9c4bb00a8a155
Derek Nelson <derek.nelson@confluent.io>
2023-04-19 15:41:48 -0700

    Add unit tests

--- ksqldb-engine/src/test/java/io/confluent/ksql/function/udf/string/MD5Test.java
+++ ksqldb-engine/src/test/java/io/confluent/ksql/function/udf/string/MD5Test.java
@@ -14,8 +14,15 @@
 
 package io.confluent.ksql.function.udf.string;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class MD5Test {
 
@@ -28,12 +35,26 @@ public class MD5Test {
 
   @Test
   public void shouldReturnNullForNull() {
-
+    assertThat(udf.md5(null), is(nullValue()));
   }
 
   @Test
   public void shouldReturnHexString() {
+    assertThat(udf.md5("one"), is("f97c5d29941bfb1b2fdab0874906ab82"));
+    assertThat(udf.md5("two"), is("b8a9f715dbb64fd5c56e7783c6820a61"));
+    assertThat(udf.md5("three"), is("35d6d33467aae9a2e3dccb4b6b027878"));
+    assertThat(udf.md5(""), is("d41d8cd98f00b204e9800998ecf8427e"));
+    assertThat(udf.md5(" "), is("7215ee9c7d9dc229d2921a40e899ec5f"));
 
+    // Sanity check some strange Unicode characters
+    assertThat(udf.md5("★☀☺"), is("670884f26d7be1298886c2e9d7c248a4"));
+
+    // Now generate random input strings and compare results to DigestUtils
+    for (int i = 0; i < 1000; i++) {
+      String uuid = UUID.randomUUID().toString();
+      DigestUtils.md5Hex(uuid);
+      assertThat(udf.md5(uuid), is(DigestUtils.md5Hex(uuid)));
+    }
   }
 }
 

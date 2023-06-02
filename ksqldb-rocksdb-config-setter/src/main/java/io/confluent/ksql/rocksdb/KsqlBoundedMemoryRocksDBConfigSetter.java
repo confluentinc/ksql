3527941918b405499dc5468cb6bcf1477a76ca49
Alieh <107070585+aliehsaeedii@users.noreply.github.com>
2023-06-02 08:41:36 +0200

    feat: expose compaction and compression configs for RocksDB (#9853)
    * feat: add compaction and compression configs
    
    * feat: cluster-level config (backup)
    
    * fix: add tests and setter correction
    
    * fix: address reviews
    
    * fix: set back the default value for trival_moves

--- ksqldb-rocksdb-config-setter/src/main/java/io/confluent/ksql/rocksdb/KsqlBoundedMemoryRocksDBConfigSetter.java
+++ ksqldb-rocksdb-config-setter/src/main/java/io/confluent/ksql/rocksdb/KsqlBoundedMemoryRocksDBConfigSetter.java
@@ -22,6 +22,9 @@ import org.apache.kafka.common.Configurable;
 import org.apache.kafka.streams.state.RocksDBConfigSetter;
 import org.rocksdb.BlockBasedTableConfig;
 import org.rocksdb.Cache;
+import org.rocksdb.CompactionOptionsUniversal;
+import org.rocksdb.CompactionStyle;
+import org.rocksdb.CompressionType;
 import org.rocksdb.Options;
 
 /**
@@ -29,6 +32,11 @@ import org.rocksdb.Options;
  * across all RocksDB instances to the number of bytes passed via
  * "ksql.plugins.rocksdb.total.memory", and also configures
  * the shared RocksDB thread pool to use "ksql.plugins.rocksdb.num.background.threads" threads.
+ * Moreover, it sets the RocksDB compaction and compression algorithms as well as maximum
+ * background jobs for compaction and memtable flushes and the trivial move as they are configured
+ * through "ksql.plugins.rocksdb.compaction.style", "ksql.plugins.rocksdb.compression.type",
+ * "ksql.plugins.rocksdb.max.background.jobs", and "ksql.plugins.rocksdb.compaction.trivial.move"
+ * respectively.
  * </p>
  * See https://docs.confluent.io/5.3.0/streams/developer-guide/memory-mgmt.html#rocksdb.
  */
@@ -37,6 +45,11 @@ public class KsqlBoundedMemoryRocksDBConfigSetter implements RocksDBConfigSetter
   private static org.rocksdb.Cache cache;
   private static org.rocksdb.WriteBufferManager writeBufferManager;
   private static final AtomicBoolean configured = new AtomicBoolean(false);
+  private static CompactionStyle compactionStyle;
+  private static CompressionType compressionType;
+  private static int maxNumConcurrentJobs;
+  private static boolean allowTrivialMove;
+
 
   @Override
   public void configure(final Map<String, ?> config) {
@@ -65,10 +78,35 @@ public class KsqlBoundedMemoryRocksDBConfigSetter implements RocksDBConfigSetter
 
       limitTotalMemory(pluginConfig, cacheFactory, bufferManagerFactory);
       configureNumThreads(pluginConfig, options);
+      setCompactionStyle(pluginConfig);
+      setCompressionType(pluginConfig);
+      setCompactionTrivialMove(pluginConfig);
+      setMaximumConcurrentBackgroundJobs(pluginConfig);
     } catch (final IllegalArgumentException e) {
       reset();
       throw e;
     }
+  }
+
+  private static void setCompactionStyle(final KsqlBoundedMemoryRocksDBConfig config) {
+    compactionStyle =
+        CompactionStyle.valueOf(config.getString(KsqlBoundedMemoryRocksDBConfig.COMPACTION_STYLE));
+  }
+
+  private static void setCompressionType(final KsqlBoundedMemoryRocksDBConfig config) {
+    compressionType =
+        CompressionType.valueOf(config.getString(KsqlBoundedMemoryRocksDBConfig.COMPRESSION_TYPE));
+  }
+
+  private static void setCompactionTrivialMove(final KsqlBoundedMemoryRocksDBConfig config) {
+    allowTrivialMove = config.getBoolean(
+        KsqlBoundedMemoryRocksDBConfig.COMPACTION_TRIVIAL_MOVE);
+  }
+
+  private static void setMaximumConcurrentBackgroundJobs(
+      final KsqlBoundedMemoryRocksDBConfig config) {
+    maxNumConcurrentJobs = config.getInt(
+        KsqlBoundedMemoryRocksDBConfig.MAX_BACKGROUND_JOBS);
   }
 
   @VisibleForTesting
@@ -135,6 +173,21 @@ public class KsqlBoundedMemoryRocksDBConfigSetter implements RocksDBConfigSetter
     tableConfig.setBlockCache(cache);
     tableConfig.setCacheIndexAndFilterBlocks(true);
     options.setWriteBufferManager(writeBufferManager);
+    options.setCompactionStyle(compactionStyle);
+    options.setCompressionType(compressionType);
+    if (maxNumConcurrentJobs != -1) {
+      options.setMaxBackgroundJobs(maxNumConcurrentJobs);
+    }
+    if (compactionStyle.equals(CompactionStyle.UNIVERSAL)) {
+      if (options.compactionOptionsUniversal() == null) {
+        final CompactionOptionsUniversal compactionOptionsUniversal =
+            new CompactionOptionsUniversal();
+        compactionOptionsUniversal.setAllowTrivialMove(allowTrivialMove);
+        options.setCompactionOptionsUniversal(compactionOptionsUniversal);
+      } else {
+        options.compactionOptionsUniversal().setAllowTrivialMove(allowTrivialMove);
+      }
+    }
 
     tableConfig.setCacheIndexAndFilterBlocksWithHighPriority(true);
     tableConfig.setPinTopLevelIndexAndFilter(true);

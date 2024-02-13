ae4a60f3a43f9700495e067d4cec81085880e236
Parag Badani <pbadani@confluent.io>
2024-02-13 20:00:53 +0530

    Ignore PreconditionCheckerIntegrationTest

--- ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/integration/PreconditionCheckerIntegrationTest.java
+++ ksqldb-rest-app/src/test/java/io/confluent/ksql/rest/integration/PreconditionCheckerIntegrationTest.java
@@ -45,13 +45,12 @@ import java.util.Optional;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
 
 @Category({IntegrationTest.class})
-@TestMethodOrder(MethodOrderer.MethodName.class)
+@Ignore
 public class PreconditionCheckerIntegrationTest {
   private final ServerState serverState = new ServerState();
   private final CorsTest corsTest = new CorsTest(this::init, 503);

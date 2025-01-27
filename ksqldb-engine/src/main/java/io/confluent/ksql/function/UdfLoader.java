e7356a6a7ef48e3367f53496ebdeddc419310382
Hrishabh Gupta <hgupta@confluent.io>
2025-01-27 17:31:18 +0530

    refactor: converted udf functions logging into debug

--- ksqldb-engine/src/main/java/io/confluent/ksql/function/UdfLoader.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/function/UdfLoader.java
@@ -144,7 +144,7 @@ public class UdfLoader {
     final FunctionInvoker invoker = FunctionLoaderUtils.createFunctionInvoker(method);
     final String functionName = udfDescriptionAnnotation.name();
 
-    LOGGER.info("Adding function " + functionName + " for method " + method);
+    LOGGER.debug("Adding function {} for method {}", functionName, method);
 
     final List<ParameterInfo> parameters = FunctionLoaderUtils
         .createParameters(method, functionName, typeParser);

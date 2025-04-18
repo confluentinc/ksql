f92bcf8a12c58747297c33bc4d6fe294bfb9def1
Robert Yokota <rayokota@gmail.com>
2025-04-18 09:08:37 -0700

    Fix method

--- ksqldb-engine/src/main/java/io/confluent/ksql/services/SandboxedSchemaRegistryClient.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/services/SandboxedSchemaRegistryClient.java
@@ -290,8 +290,15 @@ final class SandboxedSchemaRegistryClient {
     public RegisterSchemaResponse getIdWithResponse(
         String subject, ParsedSchema schema, boolean normalize)
         throws IOException, RestClientException {
-      int id = getId(subject, schema, normalize);
-      return new RegisterSchemaResponse(id);
+      try {
+        return srClient.getIdWithResponse(subject, schema,  normalize);
+      } catch (final RestClientException e) {
+        // if we don't find the schema in SR, we try to get it from the sandbox cache
+        if (e.getStatus() == HttpStatus.SC_NOT_FOUND) {
+          return sandboxCacheClient.getIdWithResponse(subject, schema, normalize);
+        }
+        throw e;
+      }
     }
 
     @Override

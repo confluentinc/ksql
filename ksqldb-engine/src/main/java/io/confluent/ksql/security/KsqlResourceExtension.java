0ededa655e6585bb2552e675594577c18977538c
unicorn-io <vtayal@confluent.io>
2025-08-14 14:58:44 +0530

    address review comments

--- ksqldb-engine/src/main/java/io/confluent/ksql/security/KsqlResourceExtension.java
+++ ksqldb-engine/src/main/java/io/confluent/ksql/security/KsqlResourceExtension.java
@@ -7,7 +7,7 @@ package io.confluent.ksql.security;
 import io.confluent.ksql.util.KsqlConfig;
 
 public interface KsqlResourceExtension {
-  void register(KsqlConfig var1);
+  void register(KsqlConfig ksqlConfig);
 
   void clean();
 }

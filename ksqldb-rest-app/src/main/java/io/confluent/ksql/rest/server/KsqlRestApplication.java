c2bb80a81e5a1f6cb44fc4237f6de132ba348a73
unicorn-io <vtayal@confluent.io>
2025-08-21 16:15:56 +0530

    KSQL-13744 | Added check for a compliant resource extension class name

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/KsqlRestApplication.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/server/KsqlRestApplication.java
@@ -153,6 +153,8 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
@@ -1172,7 +1174,10 @@ public final class KsqlRestApplication implements Executable {
     final String extensionClassName =
         ksqlConfig.getString(KsqlConfig.KSQL_RESOURCE_EXTENSION_CLASS);
 
-    if (extensionClassName == null || extensionClassName.trim().isEmpty()) {
+    if (StringUtils.isBlank(extensionClassName) || !extensionClassName.toLowerCase().matches(
+        "io\\.confluent\\.ksql\\.security\\.license\\."
+            + "(dummyksqllicensevalidatorextension|ksqllicensevalidatorextension)")
+    ) {
       log.warn(KsqlConstants.KSQL_RESOURCE_EXTENSION_MISCONFIGURED_LOG_MESSAGE);
       return Optional.empty();
     }

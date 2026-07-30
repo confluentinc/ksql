c0ae09ff77b1afd1eaafa801eb75a9aec60cba5c
Pragati Gupta <121372716+pragatiguptaaa@users.noreply.github.com>
2026-07-30 20:52:30 +0530

    Merge pull request #11055 from confluentinc/KSQL-14872 (#11128)
    Add ConfigOverrideLogger to log property overrides on KSQL endpoints that allow property overrides

--- ksqldb-engine/src/test/java/io/confluent/ksql/logging/query/QueryLoggerTest.java
+++ ksqldb-engine/src/test/java/io/confluent/ksql/logging/query/QueryLoggerTest.java
@@ -13,6 +13,7 @@
 package io.confluent.ksql.logging.query;
 
 import io.confluent.ksql.engine.rewrite.QueryAnonymizer;
+import io.confluent.ksql.test.util.TestAppender;
 import io.confluent.ksql.util.KsqlConfig;
 import io.confluent.ksql.util.QueryGuid;
 import java.util.List;
